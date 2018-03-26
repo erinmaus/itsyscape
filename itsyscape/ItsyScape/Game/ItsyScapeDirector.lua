6c7ab23a69139186ba925a6b0a5c538d765dc269
Aaron Bolyard <adb@aaronbolyard.com>
2018-03-25 20:48:22 -0400

    Added ItsyScapeDirector.

---
+++ itsyscape/ItsyScape/Game/ItsyScapeDirector.lua
@@ -0,0 +1,26 @@
+--------------------------------------------------------------------------------
+-- ItsyScape/Game/ItsyScapeDirector.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+-------------------------------------------------------------------------------
+
+local Class = require "ItsyScape.Common.Class"
+local Director = require "ItsyScape.Peep.Director"
+
+local ItsyScapeDirector = Class(Director)
+
+function ItsyScapeDirector:new(game)
+	Director.new(self)
+
+	self.game = game
+end
+
+function ItsyScapeDirector:getGameInstance()
+	return self.game
+end
+
+return ItsyScapeDirector
