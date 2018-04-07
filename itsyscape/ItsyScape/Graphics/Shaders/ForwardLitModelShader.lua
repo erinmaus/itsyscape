35ca2a38159e09158592289b5ce6d1dd57085df8
Erin Maus <squeak@erinmaus.com>
2018-04-07 08:57:41 -0400

    Fixed tiny problem with source code header.
    Most files were missing a single dash.

--- itsyscape/ItsyScape/Graphics/Shaders/ForwardLitModelShader.lua
+++ itsyscape/ItsyScape/Graphics/Shaders/ForwardLitModelShader.lua
@@ -6,8 +6,7 @@
 -- This Source Code Form is subject to the terms of the Mozilla Public
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
--------------------------------------------------------------------------------
-
+--------------------------------------------------------------------------------
 local Class = require "ItsyScape.Common.Class"
 
 -- https://www.tomdalling.com/blog/modern-opengl/08-even-more-lighting-directional-lights-spotlights-multiple-lights/
