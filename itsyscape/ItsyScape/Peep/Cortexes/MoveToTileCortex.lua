b38a4ed67ce942cbbaf3c9b19c9414694342a5c8
Erin Maus <squeak@erinmaus.com>
2018-04-08 07:41:35 -0400

    Woops, update the Cortexes, too.

--- itsyscape/ItsyScape/Peep/Cortexes/MoveToTileCortex.lua
+++ itsyscape/ItsyScape/Peep/Cortexes/MoveToTileCortex.lua
@@ -32,26 +32,31 @@ function MoveToTileCortex:update(delta)
 	for peep in self:iterate() do
 		local position = peep:getBehavior(PositionBehavior).position
 		local targetTile = peep:getBehavior(TargetTileBehavior)
+		local movement = peep:getBehavior(MovementBehavior)
+		movement.isStopping = false
 
 		local currentTile, currentTileI, currentTileJ = map:getTileAt(position.x, position.z)
 		local nextTile, nextTileI, nextTileJ = map:getTile(targetTile.pathNode.i, targetTile.pathNode.j)
 
 		local targetPosition = map:getTileCenter(nextTileI, nextTileJ)
 		local positionDifference = targetPosition - position
+		positionDifference.y = 0
+
 		local distance = positionDifference:getLength()
 		local direction = positionDifference:getNormal()
 
 		if distance > 0.5 then
 			-- If we're too far from the tile, steer towards it.
-			local movement = peep:getBehavior(MovementBehavior)
-			movement.acceleration = movement.acceleration + positionDifference
-			movement.velocity = movement.velocity + positionDifference * 0.5
+			movement.acceleration = movement.acceleration + positionDifference / delta
+			movement.velocity = movement.velocity
 		else
 			peep:removeBehavior(TargetTileBehavior)
 
 			-- Otherwise, activate next node (if possible).
 			if targetTile.nextPathNode then
 				targetTile.nextPathNode:activate(peep)
+			else
+				movement.isStopping = true
 			end
 		end
 	end
