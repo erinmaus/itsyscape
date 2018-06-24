2e3aab82e767819b07e182f2ea700ae0792a4243
Erin Maus <squeak@erinmaus.com>
2018-06-24 00:23:47 -0400

    Made MapMotion extendable.

--- itsyscape/ItsyScape/World/MapMotion.lua
+++ itsyscape/ItsyScape/World/MapMotion.lua
@@ -9,6 +9,8 @@
 --------------------------------------------------------------------------------
 
 local Class = require "ItsyScape.Common.Class"
+local Ray = require "ItsyScape.Common.Math.Ray"
+local Vector = require "ItsyScape.Common.Math.Vector"
 local Map = require "ItsyScape.World.Map"
 local Tile = require "ItsyScape.World.Tile"
 
@@ -17,6 +19,20 @@ MapMotion = Class()
 function MapMotion:new(map)
 	self.map = map
 	self.isDragging = false
+end
+
+function MapMotion:getReferenceY(e)
+	local y = e.y / e.zoom
+
+	return -y
+end
+
+function MapMotion:beginPerform(e)
+	-- Nothing.
+end
+
+function MapMotion:endPerform(e)
+	-- Nothing.
 end
 
 -- Called when the mouse is pressed.
@@ -29,28 +45,63 @@ function MapMotion:onMousePressed(e)
 	--   1. Allow button customization (instead of left/primary mouse button)
 	--   2. Raise entire tile if position is close to center (< cellSize / 2?)
 	local tiles = self.map:testRay(e.ray)
-	table.sort(tiles, function(a, b) return a[2].z < b[2].z end)
+	table.sort(tiles, function(a, b) return a[2].z > b[2].z end)
 
 	if #tiles >= 1 and e.button == 1 then
+		local v = e.ray.origin:cross(e.forward)
 		self.isDragging = true
-		self.referenceY = e.ray:project(-(e.ray.origin.z / e.ray.direction.z)).y
+		self.referenceY = math.floor(self:getReferenceY(e))
 		self.tile = tiles[1][Map.RAY_TEST_RESULT_TILE]
-		self.corners = {}
+		self.tileI = tiles[1][Map.RAY_TEST_RESULT_I]
+		self.tileJ = tiles[1][Map.RAY_TEST_RESULT_J]
 
-		local _, _, corners = self.tile:findNearestCorner(
-			tiles[1][Map.RAY_TEST_RESULT_POSITION],
-			tiles[1][Map.RAY_TEST_RESULT_I], tiles[1][Map.RAY_TEST_RESULT_J],
-			self.map.cellSize)
-		for i = 1, #corners do
-			local c = corners[i]
-			local distance = c[Tile.NEAREST_CORNER_RESULT_DISTANCE]
-			if distance < self.map.cellSize * (2 / 3) then
-				table.insert(
-					self.corners,
-					corners[i][Tile.NEAREST_CORNER_RESULT_CORNER])
+		local tileCenter = self.map:getTileCenter(
+			tiles[1][Map.RAY_TEST_RESULT_I],
+			tiles[1][Map.RAY_TEST_RESULT_J])
+		local distanceFromTileCenter
+		do
+			local difference = tileCenter - tiles[1][Map.RAY_TEST_RESULT_POSITION]
+			distanceFromTileCenter = difference:getLength()
+		end
+
+		if distanceFromTileCenter < self.map:getCellSize() / 4 then
+			self.corners ={
+				"topLeft",
+				"topRight",
+				"bottomLeft",
+				"bottomRight"
+			}
+		else
+			self.corners = {}
+			local _, _, corners = self.tile:findNearestCorner(
+				tiles[1][Map.RAY_TEST_RESULT_POSITION],
+				tiles[1][Map.RAY_TEST_RESULT_I], tiles[1][Map.RAY_TEST_RESULT_J],
+				self.map.cellSize)
+			for i = 1, #corners do
+				local c = corners[i]
+				local distance = c[Tile.NEAREST_CORNER_RESULT_DISTANCE]
+				if distance < self.map.cellSize * (2 / 3) then
+					table.insert(
+						self.corners,
+						corners[i][Tile.NEAREST_CORNER_RESULT_CORNER])
+				end
 			end
 		end
+		
+		self:beginPerform(e)
 	end
+end
+
+function MapMotion:getCorners()
+	return unpack(self.corners or {})
+end
+
+function MapMotion:getTile()
+	return self.tile, self.tileI or 1, self.tileJ or 1
+end
+
+function MapMotion:getMap()
+	return self.map
 end
 
 -- Called when the mouse is released.
@@ -59,7 +110,10 @@ end
 --
 -- e is a MouseEvent-like structure.
 function MapMotion:onMouseReleased(e)
-	self.isDragging = false
+	if self.isDragging then
+		self.isDragging = false
+		self:endPerform()
+	end
 end
 
 -- Called when the mouse is moved.
@@ -73,21 +127,11 @@ function MapMotion:onMouseMoved(e)
 	if self.isDragging then
 		-- Using the Z-axis as the reference point, compute how far along
 		-- the Y-axis the ray has moved and adjust corners by amount.
-		local p = e.ray:project(-(e.ray.origin.z / e.ray.direction.z))
-		local y = math.floor(p.y)
+		local y = math.floor(self:getReferenceY(e))
 		local distance = y - self.referenceY
 
 		if math.abs(distance) >= 1 then
-			for i = 1, #self.corners do
-				self.tile[self.corners[i]] = self.tile[self.corners[i]] + distance
-			end
-
-			if distance < 0 then
-				self.tile:snapCorners('min')
-			else
-				self.tile:snapCorners('max')
-			end
-
+			self:perform(e, distance)
 			self.referenceY = y
 
 			return true
@@ -95,6 +139,19 @@ function MapMotion:onMouseMoved(e)
 	end
 
 	return false
+end
+
+-- Raises the corner of a tile. Default action.
+function MapMotion:perform(e, distance)
+	for i = 1, #self.corners do
+		self.tile[self.corners[i]] = self.tile[self.corners[i]] + distance
+	end
+
+	if distance < 0 then
+		self.tile:snapCorners('min')
+	else
+		self.tile:snapCorners('max')
+	end
 end
 
 return MapMotion
