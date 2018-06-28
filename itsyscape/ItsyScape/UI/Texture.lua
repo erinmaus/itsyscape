8325096086fb14158cb798cd4315f7f2e61d3962
Aaron Bolyard <adb@aaronbolyard.com>
2018-06-28 05:57:50 -0400

    Added Texture widget.

---
+++ itsyscape/ItsyScape/UI/Texture.lua
@@ -0,0 +1,55 @@
+--------------------------------------------------------------------------------
+-- ItsyScape/UI/Texture.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
+local Callback = require "ItsyScape.Common.Callback"
+local Widget = require "ItsyScape.UI.Widget"
+
+local Texture = Class(Widget)
+
+function Texture:new()
+	Widget.new(self)
+
+	self.texture = false
+	self.left = 0
+	self.right = 1
+	self.top = 0
+	self.bottom = 1
+	self.keepAspect = true
+end
+
+-- Sets the texture 'resource' (a Love2D image).
+function Texture:setTexture(resource, l, r, t, b)
+	self.texture = resource
+	self.left = l or 0
+	self.right = r or 1
+	self.top = t or 0
+	self.bottom = b or 1
+end
+
+-- Gets the texture.
+function Texture:getTexture()
+	return self.texture
+end
+
+-- Gets the bounds (left, right, top, bottom).
+function Texture:getBounds()
+	return self.left, self.right, self.top, self.bottom
+end
+
+-- Gets whether the texture keeps the aspect ratio.
+function Texture:getKeepAspect()
+	return self.keepAspect
+end
+
+function Texture:setKeepAspect(value)
+	self.keepAspect = value or false
+end
+
+return Texture
---
+++ itsyscape/ItsyScape/UI/TextureRenderer.lua
@@ -0,0 +1,70 @@
+--------------------------------------------------------------------------------
+-- ItsyScape/UI/WidgetRenderer.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
+local WidgetRenderer = require "ItsyScape.UI.WidgetRenderer"
+
+local TextureRenderer = Class(WidgetRenderer)
+function TextureRenderer:new(resources)
+	WidgetRenderer.new(self, resources)
+
+	self.quads = {}
+end
+
+function TextureRenderer:drop(widget)
+	WidgetRenderer.drop(self, widget)
+	self.quads[widget] = nil
+	print("dropped", widget)
+end
+
+function TextureRenderer:draw(widget, state)
+	self:visit(widget)
+
+	local texture = widget:getTexture()
+	if texture and texture:getIsReady() then
+		texture = texture:getResource()
+
+		local l, r, t, b = widget:getBounds()
+		l = l * texture:getWidth()
+		r = r * texture:getWidth()
+		t = t * texture:getHeight()
+		b = b * texture:getHeight()
+		local w, h = r - l, b - t
+		local quad = self.quads[widget]
+		if not quad or quad.texture ~= texture then
+			quad = {}
+			quad.texture = texture
+			quad.q = love.graphics.newQuad(l, t, w, h, texture:getWidth(), texture:getHeight())
+			self.quads[widget] = quad
+		else
+			quad.q:setViewport(l, t, w, h)
+		end
+
+		local width, height = widget:getSize()
+		local scaleX, scaleY
+		if widget:getKeepAspect() then
+			if w > h then
+				local aspect = h / w
+				scaleX = width / w
+				scaleY = scaleX * aspect
+			elseif h > w then
+				local aspect = w / h
+				scaleY = height	/ h
+				scaleX = scaleY * aspect
+			else
+				scaleX = width / w
+				scaleY = height / h
+			end
+		end
+
+		love.graphics.draw(texture, quad.q, 0, 0, 0, scaleX, scaleY)
+	end
+end
+
+return TextureRenderer
