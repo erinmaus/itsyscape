aef9a59289d89cd214a9ee67b6898dc9428451ae
Erin Maus <squeak@erinmaus.com>
2018-07-25 06:52:34 -0400

    Added Rumbridge caverns tile set.

---
+++ itsyscape/Resources/Game/TileSets/RumbridgeUnderground/Layout.lua
@@ -0,0 +1,186 @@
+{
+	flags = {
+		['invert-y'] = true
+	},
+
+	texture = "Resources/Game/TileSets/RumbridgeUnderground/Texture.png",
+
+	{
+		x = 128,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt"
+	},
+
+	{
+		x = 128,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dark_dirt",
+		colorRed = 128,
+		colorGreen = 128,
+		colorBlue = 128
+	},
+
+	{
+		x = 0,
+		y = 768,
+		width = 128,
+		height = 256,
+		name = "cliff"
+	},
+
+	{
+		x = 128,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "stone.floor"
+	},
+
+	{
+		x = 0,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "stone.wall"
+	},
+
+	{
+		x = 256,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.tl"
+	},
+
+	{
+		x = 384,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.tm"
+	},
+
+	{
+		x = 512,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.tr"
+	},
+
+	{
+		x = 256,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.ml"
+	},
+
+	{
+		x = 384,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.mm"
+	},
+
+	{
+		x = 512,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.mr"
+	},
+
+	{
+		x = 256,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.bl"
+	},
+
+	{
+		x = 384,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.bm"
+	},
+
+	{
+		x = 512,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.br"
+	},
+
+	{
+		x = 640,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.otl"
+	},
+
+	{
+		x = 768,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.otm"
+	},
+
+	{
+		x = 896,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.otr"
+	},
+
+	{
+		x = 640,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.oml"
+	},
+
+	{
+		x = 896,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.omr"
+	},
+
+	{
+		x = 640,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.obl"
+	},
+
+	{
+		x = 768,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.obm"
+	},
+
+	{
+		x = 896,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt.edge.obr"
+	}
+}
