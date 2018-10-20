1ee0f9318c7638ebd8313f7ce17e65f429f92522
Erin Maus <squeak@erinmaus.com>
2018-10-20 08:13:47 -0400

    Small tweak to bank dialog.

--- itsyscape/Resources/Game/Peeps/Player/One.lua
+++ itsyscape/Resources/Game/Peeps/Player/One.lua
@@ -128,6 +128,7 @@ function One:assign(director, key, ...)
 		t:spawn(inventory.inventory, "ErrinTheHeathensGloves", 1)
 		t:spawn(inventory.inventory, "ErrinTheHeathensCoat", 1)
 		t:spawn(inventory.inventory, "ErrinTheHeathensStaff", 1)
+		t:spawn(inventory.inventory, "IsabelleIsland_AbandonedMine_ReinforcedBronzeKey", 1)
 		t:commit()
 	end
 
