bf394794e9253af8ab46987a9db8ed396f5e603d
Erin Maus <squeak@erinmaus.com>
2018-10-20 08:14:24 -0400

    Finished Isabelle's initial dialog.

--- itsyscape/Resources/Game/Maps/IsabelleIsland_Tower/Dialog/IsabellePostStart_en-US.lua
+++ itsyscape/Resources/Game/Maps/IsabelleIsland_Tower/Dialog/IsabellePostStart_en-US.lua
@@ -1,5 +1,10 @@
 speaker "Isabelle"
 
-message "bla bla bla"
+message {
+	"Please speak to Advisor Grimm just outside this room.",
+	"He has my full faith and acts in my stead."
+}
+
+message "Thank you and I look forward to hearing your progress!"
 
 _TARGET:getState():give("KeyItem", "CalmBeforeTheStorm_TalkedToIsabelle1")
