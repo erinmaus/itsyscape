79f014a38d34c1e6de82aab94d007f7b4dfddc6c
Aaron Bolyard <adb@aaronbolyard.com>
2018-11-12 17:36:29 -0500

    Fudged positioning of weapons. They always faces the player now.

--- itsyscape/Resources/Game/DB/Creeps/Nymph.lua
+++ itsyscape/Resources/Game/DB/Creeps/Nymph.lua
@@ -13,7 +13,7 @@ ItsyScape.Resource.Peep "Nymph_Base" {
 }
 
 ItsyScape.Meta.ResourceDescription {
-	Value = "The revenge of felled trees."
+	Value = "The revenge of felled trees.",
 	Resource = ItsyScape.Resource.Peep "Nymph_Base"
 }
 
@@ -55,7 +55,15 @@ ItsyScape.Meta.PeepStat {
 	Skill = ItsyScape.Resource.Skill "Constitution",
 	Value = ItsyScape.Utility.xpForLevel(10),
 	Resource = ItsyScape.Resource.Peep "Nymph_Base"
-}ItsyScape.Resource.Peep "Nymph_Attackable_Wand" {
+}
+
+ItsyScape.Meta.PeepEquipmentItem {
+	Item = ItsyScape.Resource.Item "WoodlandRobe",
+	Count = 1,
+	Resource = ItsyScape.Resource.Peep "Nymph_Base"
+}
+
+ItsyScape.Resource.Peep "Nymph_Attackable_Wand" {
 	ItsyScape.Action.Attack(),
 }
 
@@ -76,7 +84,7 @@ ItsyScape.Meta.ResourceName {
 }
 
 ItsyScape.Meta.ResourceDescription {
-	Value = "Revenging felled trees since the dawn of humanity."
+	Value = "Revenging felled trees since the dawn of humanity.",
 	Resource = ItsyScape.Resource.Peep "Nymph_Attackable_Wand"
 }
 
@@ -114,4 +122,51 @@ ItsyScape.Meta.PeepEquipmentItem {
 	Item = ItsyScape.Resource.Item "WoodlandRobe",
 	Count = 1,
 	Resource = ItsyScape.Resource.Peep "Nymph_Attackable_Wand"
+}
+
+ItsyScape.Resource.Item "WoodlandRobe" {
+	ItsyScape.Action.Equip() {
+		Requirement {
+			Resource = ItsyScape.Resource.Skill "Magic",
+			Count = ItsyScape.Utility.xpForLevel(5)
+		},
+
+		Requirement {
+			Resource = ItsyScape.Resource.Skill "Defense",
+			Count = ItsyScape.Utility.xpForLevel(5)
+		}
+	},
+
+	ItsyScape.Action.Dequip()
+}
+
+ItsyScape.Meta.ResourceName {
+	Value = "Woodland robe",
+	Language = "en-US",
+	Resource = ItsyScape.Resource.Item "WoodlandRobe"
+}
+
+ItsyScape.Meta.ResourceDescription {
+	Value = "Feels like nothing at all.",
+	Language = "en-US",
+	Resource = ItsyScape.Resource.Item "WoodlandRobe"
+}
+
+local ARMOR_BODY_WEIGHT = 1
+ItsyScape.Meta.Equipment {
+	AccuracyMagic = ItsyScape.Utility.styleBonusForItem(5, 0.2),
+	DefenseStab = ItsyScape.Utility.styleBonusForItem(5, ARMOR_BODY_WEIGHT),
+	DefenseSlash = ItsyScape.Utility.styleBonusForItem(4, ARMOR_BODY_WEIGHT),
+	DefenseCrush = ItsyScape.Utility.styleBonusForItem(6, ARMOR_BODY_WEIGHT),
+	DefenseRanged = -ItsyScape.Utility.styleBonusForItem(2, ARMOR_BODY_WEIGHT),
+	StrengthMagic = ItsyScape.Utility.strengthBonusForWeapon(5, 0.5),
+	Prayer = 3,
+	EquipSlot = ItsyScape.Utility.Equipment.PLAYER_SLOT_BODY,
+	Resource = ItsyScape.Resource.Item "WoodlandRobe"
+}
+
+ItsyScape.Meta.EquipmentModel {
+	Type = "ItsyScape.Game.Skin.ModelSkin",
+	Filename = "Resources/Game/Skins/WoodlandRobe/WoodlandRobe.lua",
+	Resource = ItsyScape.Resource.Item "WoodlandRobe"
 }
