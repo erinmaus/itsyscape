7ff0aba7599752a7bce464ee0071a9ff427063ca
Aaron Bolyard <adb@aaronbolyard.com>
2018-11-19 19:41:17 -0500

    Nymphs have drops; bound nymph.

---
+++ itsyscape/Resources/Game/Skins/AncientDriftwoodMask/AncientDriftwoodMask.lua
@@ -0,0 +1,5 @@
+{
+	model = "Resources/Game/Skins/AncientDriftwoodMask/AncientDriftwoodMask.lmesh",
+	texture = "Resources/Game/Skins/AncientDriftwoodMask/AncientDriftwoodMask.png",
+	isBlocking = false
+}
