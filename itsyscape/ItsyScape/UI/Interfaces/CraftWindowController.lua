0371b6df09af6ffc3c33068b2ef059e920b51db8
Erin Maus <squeak@erinmaus.com>
2018-11-30 07:15:40 -0500

    Remove debug print.

--- itsyscape/ItsyScape/UI/Interfaces/CraftWindowController.lua
+++ itsyscape/ItsyScape/UI/Interfaces/CraftWindowController.lua
@@ -43,8 +43,6 @@ function CraftWindowController:new(peep, director, prop, categoryKey, categoryVa
 					table.insert(self.state.actions, a)
 
 					self.actionsByID[a.id] = actionInstance
-				else
-					print 'failed'
 				end
 			end
 		end
