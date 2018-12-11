82c7571ed6216e07125cb5f31accfeaf39adaa2f
Aaron Bolyard <adb@aaronbolyard.com>
2018-12-11 09:55:07 -0500

    Added Rosalind.

---
+++ itsyscape/Resources/Game/Skins/PlayerKit1/Shirts/RedDress.lua
@@ -0,0 +1,4 @@
+{
+	model = "Resources/Game/Skins/Common/PlayerKit1/Dress.lmesh",
+	texture = "Resources/Game/Skins/PlayerKit1/Shirts/RedDress.png"
+}
---
+++ itsyscape/Resources/Game/Skins/PlayerKit1/Shirts/WinterTunic.lua
@@ -0,0 +1,4 @@
+{
+	model = "Resources/Game/Skins/PlayerKit1/Shirts/WinterTunic.lmesh",
+	texture = "Resources/Game/Skins/PlayerKit1/Shirts/WinterTunic.png"
+}
