82842776a7b68417b439823f2cb4f38a8846dad2
Aaron Bolyard <adb@aaronbolyard.com>
2018-12-17 22:27:57 -0500

    Mn'thro, undead squid.

--- itsyscape/Resources/Game/Skins/GhostlyMinerForeman/GhostlyMinerForeman.lua
+++ itsyscape/Resources/Game/Skins/GhostlyMinerForeman/GhostlyMinerForeman.lua
@@ -1,8 +1,4 @@
 {
 	model = "Resources/Game/Skins/GhostlyMinerForeman/GhostlyMinerForeman.lmesh",
-	texture = "Resources/Game/Skins/GhostlyMinerForeman/Texture.png",
-	isTranslucent = true,
-	position = { 0, 1, 0 },
-	scale = { 1.5, 1.5, 1 },
-	fullLit = true
+	texture = "Resources/Game/Skins/GhostlyMinerForeman/Texture.png"
 }
---
+++ itsyscape/Resources/Game/Skins/UndeadSquid/UndeadSquid.lua
@@ -0,0 +1,6 @@
+{
+	model = "Resources/Game/Skins/UndeadSquid/UndeadSquid.lmesh",
+	texture = "Resources/Game/Skins/UndeadSquid/UndeadSquid.png",
+	scale = { 2, 2, 2 },
+	position = { 0, -13, 0 }
+}
