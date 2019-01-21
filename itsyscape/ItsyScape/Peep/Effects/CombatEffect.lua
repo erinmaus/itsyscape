e8d4f1683249bb8d9d7ac05c99c1817fb2d758d8
Erin Maus <squeak@erinmaus.com>
2019-01-21 05:42:38 -0500

    Collapse Damage/Accuracy effects into single 'CombatEffect'.

--- itsyscape/ItsyScape/Peep/Effects/CombatEffect.lua
+++ itsyscape/ItsyScape/Peep/Effects/CombatEffect.lua
@@ -1,5 +1,5 @@
 --------------------------------------------------------------------------------
--- ItsyScape/Peep/Effects/AccuracyEffect.lua
+-- ItsyScape/Peep/Effects/CombatEffect.lua
 --
 -- This file is a part of ItsyScape.
 --
@@ -10,21 +10,40 @@
 local Class = require "ItsyScape.Common.Class"
 local Effect = require "ItsyScape.Peep.Effect"
 
-local AccuracyEffect = Class(Effect)
+local CombatEffect = Class(Effect)
+
+-- Modifies an damage roll, assuming 'damageRoll' is from self damaging the
+-- target.
+--
+-- damageRoll is a ItsyScape.Game.Weapon.CombatRoll
+--
+-- purpose corresponds to a Weapon PURPOSE_* enum.
+function CombatEffect:applySelfToDamage(damageRoll, purpose)
+	-- Nothing.
+end
+
+-- Modifies an damage roll, assuming 'damageRoll' is from target damaging self.
+--
+-- damageRoll is a ItsyScape.Game.Weapon.CombatRoll
+--
+-- purpose corresponds to a Weapon PURPOSE_* enum.
+function CombatEffect:applyTargetToDamage(damageRoll)
+	-- Nothing.
+end
 
 -- Modifies an attack roll, assuming 'attackRoll' is from self attacking the
 -- target.
 --
 -- attackRoll is a ItsyScape.Game.Weapon.AttackRoll
-function AccuracyEffect:applySelf(attackRoll)
+function CombatEffect:applySelfToAttack(attackRoll)
 	-- Nothing.
 end
 
 -- Modifies an attack roll, assuming 'attackRoll' is from target attacking self.
 --
 -- attackRoll is a ItsyScape.Game.Weapon.AttackRoll
-function AccuracyEffect:applyTarget(attackRoll)
+function CombatEffect:applyTargetToAttack(attackRoll)
 	-- Nothing.
 end
 
-return AccuracyEffect
+return CombatEffect
