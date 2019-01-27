da06dc1d05e42dbb9de6b3124d29c25ff1809e96
Erin Maus <squeak@erinmaus.com>
2019-01-27 05:44:49 -0500

    Defer settings to settings.cfg.

--- itsyscape/conf.lua
+++ itsyscape/conf.lua
@@ -1,9 +1,33 @@
+local serpent = require "serpent"
+
 function love.conf(t)
+	local s
+	do
+		love.filesystem.setIdentity("ItsyRealm")
+		local file = love.filesystem.read("settings.cfg")
+		if file then
+			local r, e = loadstring('return ' .. file)
+			if not r then
+				io.stderr:write(string.format("Failed to parse settings: '%s'\n", e))
+			else
+				r, e = pcall(r)
+				if not r then
+					io.stderr:write(string.format("Failed to load settings: '%s'\n", e))
+				else
+					s = e
+				end
+			end
+		end
+
+		s = s or {}
+	end
+
 	t.identity = "ItsyRealm"
 	t.title = "ItsyRealm"
-	t.window.width = 1280
-	t.window.height = 720
+	t.window.width = s.width or 1280
+	t.window.height = s.height or 720
 	t.window.depth = 24
-	t.window.fullscreen = false
-	t.window.vsync = 0
+	t.window.fullscreen = s.fullscreen or false
+	t.window.vsync = s.vsync or 0
+	t.window.display = s.display or t.window.display
 end
