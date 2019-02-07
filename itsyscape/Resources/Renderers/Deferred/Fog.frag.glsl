f65ceba9018a9c09762929c4b11aedf01a7ee8cf
Erin Maus <squeak@erinmaus.com>
2019-02-07 06:55:22 -0500

    Mix fog with alpha.

--- itsyscape/Resources/Renderers/Deferred/Fog.frag.glsl
+++ itsyscape/Resources/Renderers/Deferred/Fog.frag.glsl
@@ -11,6 +11,7 @@
 ///////////////////////////////////////////////////////////////////////////////
 
 uniform Image scape_PositionTexture;
+uniform Image scape_ColorTexture;
 
 uniform vec2 scape_FogParameters;
 uniform vec3 scape_FogColor;
@@ -22,9 +23,10 @@ vec4 effect(
 	vec2 textureCoordinate,
 	vec2 screenCoordinate)
 {
+	float alpha = Texel(scape_ColorTexture, textureCoordinate).a;
 	vec3 position = Texel(scape_PositionTexture, textureCoordinate).xyz - scape_CameraEye;
 	float length = length(position);
 	float factor = 1.0 - clamp((scape_FogParameters.y - length) / (scape_FogParameters.y - scape_FogParameters.x), 0.0, 1.0);
 
-	return vec4(scape_FogColor * factor, factor);
+	return vec4(scape_FogColor * factor * alpha, factor * alpha);
 }
