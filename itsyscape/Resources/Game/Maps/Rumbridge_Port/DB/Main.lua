2abdd37699a4276cd95b4076014c7793cf5f127f
Aaron Bolyard <adb@aaronbolyard.com>
2020-01-28 07:23:49 -0500

    Added Rumbridge Port (map only.)

---
+++ itsyscape/Resources/Game/Maps/Rumbridge_Port/DB/Main.lua
@@ -0,0 +1,116 @@
+local M = include "Resources/Game/Maps/Rumbridge_Port/DB/Default.lua"
+
+ItsyScape.Meta.PeepID {
+	Value = "Resources.Game.Maps.Rumbridge_Port.Peep",
+	Resource = M._MAP
+}
+
+ItsyScape.Meta.ResourceName {
+	Value = "Rumbridge Port",
+	Language = "en-US",
+	Resource = M._MAP
+}
+
+ItsyScape.Meta.ResourceDescription {
+	Value = "Home of the most drunken, pathetic sailors you've ever seen.",
+	Language = "en-US",
+	Resource = M._MAP
+}
+
+M["Light_Ambient"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 0,
+		PositionY = 0,
+		PositionZ = 0,
+		Name = "Light_Ambient",
+		Map = M._MAP,
+		Resource = M["Light_Ambient"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "AmbientLight_Default",
+		MapObject = M["Light_Ambient"]
+	}
+
+	ItsyScape.Meta.Light {
+		ColorRed = 33,
+		ColorGreen = 162,
+		ColorBlue = 234,
+		Resource = M["Light_Ambient"]
+	}
+
+	ItsyScape.Meta.AmbientLight {
+		Ambience = 0.6,
+		Resource = M["Light_Ambient"]
+	}
+end
+
+M["Light_Sun"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 0,
+		PositionY = 0,
+		PositionZ = 0,
+		Name = "Light_Sun",
+		Map = M._MAP,
+		Resource = M["Light_Sun"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "DirectionalLight_Default",
+		MapObject = M["Light_Sun"]
+	}
+
+	ItsyScape.Meta.Light {
+		ColorRed = 234,
+		ColorGreen = 162,
+		ColorBlue = 33,
+		Resource = M["Light_Sun"]
+	}
+
+	ItsyScape.Meta.DirectionalLight {
+		DirectionX = 4,
+		DirectionY = 5,
+		DirectionZ = 4,
+		Resource = M["Light_Sun"]
+	}
+end
+
+M["Light_Fog"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 0,
+		PositionY = 0,
+		PositionZ = 0,
+		Name = "Light_Fog",
+		Map = M._MAP,
+		Resource = M["Light_Fog"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Fog_Default",
+		MapObject = M["Light_Fog"]
+	}
+
+	ItsyScape.Meta.Fog {
+		ColorRed = 33,
+		ColorGreen = 162,
+		ColorBlue = 234,
+		NearDistance = 50,
+		FarDistance = 200,
+		Resource = M["Light_Fog"]
+	}
+end
+
+M["Anchor_FromTown"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 37,
+		PositionY = 5,
+		PositionZ = 63,
+		Name = "Anchor_FromTown",
+		Map = M._MAP,
+		Resource = M["Anchor_FromTown"]
+	}
+end
