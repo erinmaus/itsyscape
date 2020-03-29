b08d822ef810b2f79de279c9e55562d35f14b9d2
Aaron Bolyard <adb@aaronbolyard.com>
2020-03-29 18:14:31 -0400

    Map table!

---
+++ itsyscape/Resources/Game/TileSets/MapTable/Layout.lua
@@ -0,0 +1,49 @@
+{
+	flags = {
+		['invert-y'] = true
+	},
+
+	texture = "Resources/Game/TileSets/MapTable/Texture.png",
+	textureWidth = 512,
+	textureHeight = 512,
+
+	{
+		x = 64,
+		y = 448,
+		width = 64,
+		height = 64,
+		name = "grass"
+	},
+
+	{
+		x = 0,
+		y = 384,
+		width = 64,
+		height = 128,
+		name = "cliff"
+	},
+
+	{
+		x = 128,
+		y = 448,
+		width = 64,
+		height = 64,
+		name = "town"
+	},
+
+	{
+		x = 128,
+		y = 384,
+		width = 64,
+		height = 64,
+		name = "trees"
+	},
+
+	{
+		x = 128,
+		y = 320,
+		width = 64,
+		height = 64,
+		name = "coconut"
+	},
+}
