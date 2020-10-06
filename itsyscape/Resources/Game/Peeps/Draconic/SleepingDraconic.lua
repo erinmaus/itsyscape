b747b5026980a22ea08381c0d9e820b6ac091b8b
Erin Maus <squeak@erinmaus.com>
2020-10-06 11:41:20 -0400

    Specify time offset when spawning.

--- itsyscape/Resources/Game/Peeps/Draconic/SleepingDraconic.lua
+++ itsyscape/Resources/Game/Peeps/Draconic/SleepingDraconic.lua
@@ -13,10 +13,12 @@ local CacheRef = require "ItsyScape.Game.CacheRef"
 local BaseDraconic = require "Resources.Game.Peeps.Draconic.BaseDraconic"
 local ActorReferenceBehavior = require "ItsyScape.Peep.Behaviors.ActorReferenceBehavior"
 local DisabledBehavior = require "ItsyScape.Peep.Behaviors.DisabledBehavior"
+local HumanoidActorAnimatorCortex = require "ItsyScape.Peep.Cortexes.HumanoidActorAnimatorCortex"
 
 local SleepingDraconic = Class(BaseDraconic)
 SleepingDraconic.SPAWN_BUBBLES_MAX_TICK = 2
-SleepingDraconic.OFFSET = Vector(1, 1, 1)
+SleepingDraconic.POSITION_OFFSET = Vector(1, 1, 1)
+SleepingDraconic.MAX_ANIMATION_TIME_OFFSET = 10
 
 function SleepingDraconic:new(resource, name, ...)
 	BaseDraconic.new(self, resource, name or 'Draconic', ...)
@@ -30,6 +32,14 @@ function SleepingDraconic:ready(director, game)
 		"Resources/Game/Animations/Human_Idle_SleepingInVat/Script.lua")
 	self:addResource("animation-idle", idleAnimation)
 
+	local actor = self:getBehavior(ActorReferenceBehavior).actor
+	actor:playAnimation(
+		'main',
+		HumanoidActorAnimatorCortex.WALK_PRIORITY,
+		idleAnimation,
+		true,
+		math.random() * SleepingDraconic.MAX_ANIMATION_TIME_OFFSET)
+
 	self:poke('breathe')
 
 	BaseDraconic.ready(self, director, game)
@@ -37,7 +47,7 @@ end
 
 function SleepingDraconic:onBreathe()
 	local stage = self:getDirector():getGameInstance():getStage()
-	local offset = SleepingDraconic.OFFSET * Vector(
+	local offset = SleepingDraconic.POSITION_OFFSET * Vector(
 		math.random() * 2 - 1,
 		math.random(),
 		math.random() * 2 - 1) + Vector.UNIT_Y
