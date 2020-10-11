4b46c7a6c5471b6ee1f6c579b08f3af14765bf40
Erin Maus <squeak@erinmaus.com>
2020-10-10 23:41:48 -0400

    Added Hex Labs tile set.

--- itsyscape/Resources/Game/Maps/Rumbridge_HexLabs_Floor1/DB/Default.lua
+++ itsyscape/Resources/Game/Maps/Rumbridge_HexLabs_Floor1/DB/Default.lua
@@ -2,12 +2,12 @@ local M = {}
 
 M._MAP = ItsyScape.Resource.Map "Rumbridge_HexLabs_Floor1"
 
-M["HexLabs_Vat7"] = ItsyScape.Resource.MapObject.Unique()
+M["HexLabs_Vat6"] = ItsyScape.Resource.MapObject.Unique()
 do
 	ItsyScape.Meta.MapObjectLocation {
 		PositionX = 24.000000,
 		PositionY = 3.000000,
-		PositionZ = 26.125000,
+		PositionZ = 22.250000,
 		RotationX = 0.000000,
 		RotationY = 0.000000,
 		RotationZ = 0.000000,
@@ -15,38 +15,38 @@ do
 		ScaleX = 1.000000,
 		ScaleY = 1.000000,
 		ScaleZ = 1.000000,
-		Name = "HexLabs_Vat7",
+		Name = "HexLabs_Vat6",
 		Map = M._MAP,
-		Resource = M["HexLabs_Vat7"]
+		Resource = M["HexLabs_Vat6"]
 	}
 
 	ItsyScape.Meta.PropMapObject {
 		Prop = ItsyScape.Resource.Prop "HexLabs_Vat",
-		MapObject = M["HexLabs_Vat7"]
+		MapObject = M["HexLabs_Vat6"]
 	}
 end
 
-M["HexLabs_Vat1"] = ItsyScape.Resource.MapObject.Unique()
+M["HexLabs_JakkenstoneShardAnalyzer_Pillar12"] = ItsyScape.Resource.MapObject.Unique()
 do
 	ItsyScape.Meta.MapObjectLocation {
-		PositionX = 38.000000,
-		PositionY = 3.000000,
-		PositionZ = 18.000000,
+		PositionX = 43.000000,
+		PositionY = 2.000000,
+		PositionZ = 67.000000,
 		RotationX = 0.000000,
-		RotationY = 0.000000,
+		RotationY = 1.000000,
 		RotationZ = 0.000000,
-		RotationW = 1.000000,
+		RotationW = 0.000000,
 		ScaleX = 1.000000,
 		ScaleY = 1.000000,
 		ScaleZ = 1.000000,
-		Name = "HexLabs_Vat1",
+		Name = "HexLabs_JakkenstoneShardAnalyzer_Pillar12",
 		Map = M._MAP,
-		Resource = M["HexLabs_Vat1"]
+		Resource = M["HexLabs_JakkenstoneShardAnalyzer_Pillar12"]
 	}
 
 	ItsyScape.Meta.PropMapObject {
-		Prop = ItsyScape.Resource.Prop "HexLabs_Vat",
-		MapObject = M["HexLabs_Vat1"]
+		Prop = ItsyScape.Resource.Prop "HexLabs_JakkenstoneShardAnalyzer_Pillar",
+		MapObject = M["HexLabs_JakkenstoneShardAnalyzer_Pillar12"]
 	}
 end
 
@@ -98,27 +98,27 @@ do
 	}
 end
 
-M["HexLabs_JakkenstoneShardAnalyzer_Pillar12"] = ItsyScape.Resource.MapObject.Unique()
+M["HexLabs_JakkenstoneShardAnalyzer1"] = ItsyScape.Resource.MapObject.Unique()
 do
 	ItsyScape.Meta.MapObjectLocation {
-		PositionX = 43.000000,
+		PositionX = 48.000000,
 		PositionY = 2.000000,
-		PositionZ = 67.000000,
+		PositionZ = 62.500000,
 		RotationX = 0.000000,
-		RotationY = 1.000000,
+		RotationY = 0.000000,
 		RotationZ = 0.000000,
-		RotationW = 0.000000,
+		RotationW = 1.000000,
 		ScaleX = 1.000000,
 		ScaleY = 1.000000,
 		ScaleZ = 1.000000,
-		Name = "HexLabs_JakkenstoneShardAnalyzer_Pillar12",
+		Name = "HexLabs_JakkenstoneShardAnalyzer1",
 		Map = M._MAP,
-		Resource = M["HexLabs_JakkenstoneShardAnalyzer_Pillar12"]
+		Resource = M["HexLabs_JakkenstoneShardAnalyzer1"]
 	}
 
 	ItsyScape.Meta.PropMapObject {
-		Prop = ItsyScape.Resource.Prop "HexLabs_JakkenstoneShardAnalyzer_Pillar",
-		MapObject = M["HexLabs_JakkenstoneShardAnalyzer_Pillar12"]
+		Prop = ItsyScape.Resource.Prop "HexLabs_JakkenstoneShardAnalyzer",
+		MapObject = M["HexLabs_JakkenstoneShardAnalyzer1"]
 	}
 end
 
@@ -146,84 +146,12 @@ do
 	}
 end
 
-M["HexLabs_Vat6"] = ItsyScape.Resource.MapObject.Unique()
-do
-	ItsyScape.Meta.MapObjectLocation {
-		PositionX = 24.000000,
-		PositionY = 3.000000,
-		PositionZ = 22.250000,
-		RotationX = 0.000000,
-		RotationY = 0.000000,
-		RotationZ = 0.000000,
-		RotationW = 1.000000,
-		ScaleX = 1.000000,
-		ScaleY = 1.000000,
-		ScaleZ = 1.000000,
-		Name = "HexLabs_Vat6",
-		Map = M._MAP,
-		Resource = M["HexLabs_Vat6"]
-	}
-
-	ItsyScape.Meta.PropMapObject {
-		Prop = ItsyScape.Resource.Prop "HexLabs_Vat",
-		MapObject = M["HexLabs_Vat6"]
-	}
-end
-
-M["HexLabs_JakkenstoneShardAnalyzer_Pillar11"] = ItsyScape.Resource.MapObject.Unique()
-do
-	ItsyScape.Meta.MapObjectLocation {
-		PositionX = 43.000000,
-		PositionY = 2.000000,
-		PositionZ = 57.000000,
-		RotationX = 0.000000,
-		RotationY = 0.707107,
-		RotationZ = 0.000000,
-		RotationW = 0.707107,
-		ScaleX = 1.000000,
-		ScaleY = 1.000000,
-		ScaleZ = 1.000000,
-		Name = "HexLabs_JakkenstoneShardAnalyzer_Pillar11",
-		Map = M._MAP,
-		Resource = M["HexLabs_JakkenstoneShardAnalyzer_Pillar11"]
-	}
-
-	ItsyScape.Meta.PropMapObject {
-		Prop = ItsyScape.Resource.Prop "HexLabs_JakkenstoneShardAnalyzer_Pillar",
-		MapObject = M["HexLabs_JakkenstoneShardAnalyzer_Pillar11"]
-	}
-end
-
-M["HexLabs_Vat8"] = ItsyScape.Resource.MapObject.Unique()
-do
-	ItsyScape.Meta.MapObjectLocation {
-		PositionX = 24.000000,
-		PositionY = 3.000000,
-		PositionZ = 30.125000,
-		RotationX = 0.000000,
-		RotationY = 0.000000,
-		RotationZ = 0.000000,
-		RotationW = 1.000000,
-		ScaleX = 1.000000,
-		ScaleY = 1.000000,
-		ScaleZ = 1.000000,
-		Name = "HexLabs_Vat8",
-		Map = M._MAP,
-		Resource = M["HexLabs_Vat8"]
-	}
-
-	ItsyScape.Meta.PropMapObject {
-		Prop = ItsyScape.Resource.Prop "HexLabs_Vat",
-		MapObject = M["HexLabs_Vat8"]
-	}
-end
-
-M["HexLabs_Vat3"] = ItsyScape.Resource.MapObject.Unique()
+M["HexLabs_Vat1"] = ItsyScape.Resource.MapObject.Unique()
 do
 	ItsyScape.Meta.MapObjectLocation {
 		PositionX = 38.000000,
 		PositionY = 3.000000,
-		PositionZ = 26.125000,
+		PositionZ = 18.000000,
 		RotationX = 0.000000,
 		RotationY = 0.000000,
 		RotationZ = 0.000000,
@@ -231,38 +159,14 @@ do
 		ScaleX = 1.000000,
 		ScaleY = 1.000000,
 		ScaleZ = 1.000000,
-		Name = "HexLabs_Vat3",
+		Name = "HexLabs_Vat1",
 		Map = M._MAP,
-		Resource = M["HexLabs_Vat3"]
+		Resource = M["HexLabs_Vat1"]
 	}
 
 	ItsyScape.Meta.PropMapObject {
 		Prop = ItsyScape.Resource.Prop "HexLabs_Vat",
-		MapObject = M["HexLabs_Vat3"]
-	}
-end
-
-M["HexLabs_JakkenstoneShardAnalyzer_Pillar10"] = ItsyScape.Resource.MapObject.Unique()
-do
-	ItsyScape.Meta.MapObjectLocation {
-		PositionX = 53.000000,
-		PositionY = 2.000000,
-		PositionZ = 57.000000,
-		RotationX = 0.000000,
-		RotationY = 0.000000,
-		RotationZ = 0.000000,
-		RotationW = 1.000000,
-		ScaleX = 1.000000,
-		ScaleY = 1.000000,
-		ScaleZ = 1.000000,
-		Name = "HexLabs_JakkenstoneShardAnalyzer_Pillar10",
-		Map = M._MAP,
-		Resource = M["HexLabs_JakkenstoneShardAnalyzer_Pillar10"]
-	}
-
-	ItsyScape.Meta.PropMapObject {
-		Prop = ItsyScape.Resource.Prop "HexLabs_JakkenstoneShardAnalyzer_Pillar",
-		MapObject = M["HexLabs_JakkenstoneShardAnalyzer_Pillar10"]
+		MapObject = M["HexLabs_Vat1"]
 	}
 end
 
@@ -290,12 +194,12 @@ do
 	}
 end
 
-M["HexLabs_JakkenstoneShardAnalyzer1"] = ItsyScape.Resource.MapObject.Unique()
+M["HexLabs_Vat8"] = ItsyScape.Resource.MapObject.Unique()
 do
 	ItsyScape.Meta.MapObjectLocation {
-		PositionX = 48.000000,
-		PositionY = 2.000000,
-		PositionZ = 62.500000,
+		PositionX = 24.000000,
+		PositionY = 3.000000,
+		PositionZ = 30.125000,
 		RotationX = 0.000000,
 		RotationY = 0.000000,
 		RotationZ = 0.000000,
@@ -303,14 +207,110 @@ do
 		ScaleX = 1.000000,
 		ScaleY = 1.000000,
 		ScaleZ = 1.000000,
-		Name = "HexLabs_JakkenstoneShardAnalyzer1",
+		Name = "HexLabs_Vat8",
 		Map = M._MAP,
-		Resource = M["HexLabs_JakkenstoneShardAnalyzer1"]
+		Resource = M["HexLabs_Vat8"]
 	}
 
 	ItsyScape.Meta.PropMapObject {
-		Prop = ItsyScape.Resource.Prop "HexLabs_JakkenstoneShardAnalyzer",
-		MapObject = M["HexLabs_JakkenstoneShardAnalyzer1"]
+		Prop = ItsyScape.Resource.Prop "HexLabs_Vat",
+		MapObject = M["HexLabs_Vat8"]
+	}
+end
+
+M["HexLabs_Vat7"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 24.000000,
+		PositionY = 3.000000,
+		PositionZ = 26.125000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "HexLabs_Vat7",
+		Map = M._MAP,
+		Resource = M["HexLabs_Vat7"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "HexLabs_Vat",
+		MapObject = M["HexLabs_Vat7"]
+	}
+end
+
+M["HexLabs_JakkenstoneShardAnalyzer_Pillar10"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 53.000000,
+		PositionY = 2.000000,
+		PositionZ = 57.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "HexLabs_JakkenstoneShardAnalyzer_Pillar10",
+		Map = M._MAP,
+		Resource = M["HexLabs_JakkenstoneShardAnalyzer_Pillar10"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "HexLabs_JakkenstoneShardAnalyzer_Pillar",
+		MapObject = M["HexLabs_JakkenstoneShardAnalyzer_Pillar10"]
+	}
+end
+
+M["HexLabs_Vat3"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 38.000000,
+		PositionY = 3.000000,
+		PositionZ = 26.125000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "HexLabs_Vat3",
+		Map = M._MAP,
+		Resource = M["HexLabs_Vat3"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "HexLabs_Vat",
+		MapObject = M["HexLabs_Vat3"]
+	}
+end
+
+M["HexLabs_JakkenstoneShardAnalyzer_Pillar11"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 43.000000,
+		PositionY = 2.000000,
+		PositionZ = 57.000000,
+		RotationX = 0.000000,
+		RotationY = 0.707107,
+		RotationZ = 0.000000,
+		RotationW = 0.707107,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "HexLabs_JakkenstoneShardAnalyzer_Pillar11",
+		Map = M._MAP,
+		Resource = M["HexLabs_JakkenstoneShardAnalyzer_Pillar11"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "HexLabs_JakkenstoneShardAnalyzer_Pillar",
+		MapObject = M["HexLabs_JakkenstoneShardAnalyzer_Pillar11"]
 	}
 end
 
