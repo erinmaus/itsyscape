fa97c970dc8ef9fa395f287cbcdb32742e599087
Erin Maus <squeak@erinmaus.com>
2020-10-10 23:41:48 -0400

    Added Hex Labs tile set.

---
+++ itsyscape/Resources/Game/TileSets/HexLabs/Layout.lua
@@ -0,0 +1,257 @@
+{
+	flags = {
+		['invert-y'] = true
+	},
+
+	texture = "Resources/Game/TileSets/HexLabs/Texture.png",
+	textureWidth = 1024,
+	textureHeight = 1024,
+
+	{
+		x = 128,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "plate"
+	},
+
+	{
+		x = 0,
+		y = 768,
+		width = 128,
+		height = 256,
+		name = "wall"
+	},
+
+	{
+		x = 0,
+		y = 256,
+		width = 128,
+		height = 256,
+		name = "wall2"
+	},
+
+	{
+		x = 0,
+		y = 512,
+		width = 128,
+		height = 256,
+		name = "cave_wall"
+	},
+
+	{
+		x = 128,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "dark-plate"
+	},
+
+	{
+		x = 128,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt1"
+	},
+
+	{
+		x = 128,
+		y = 512,
+		width = 128,
+		height = 128,
+		name = "dirt2"
+	},
+
+	{
+		x = 640,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt1.edge.tl"
+	},
+
+	{
+		x = 768,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt1.edge.tm"
+	},
+
+	{
+		x = 896,
+		y = 896,
+		width = 128,
+		height = 128,
+		name = "dirt1.edge.tr"
+	},
+
+	{
+		x = 640,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "dirt1.edge.ml"
+	},
+
+	{
+		x = 896,
+		y = 768,
+		width = 128,
+		height = 128,
+		name = "dirt1.edge.mr"
+	},
+
+	{
+		x = 640,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt1.edge.bl"
+	},
+
+	{
+		x = 768,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt1.edge.bm"
+	},
+
+	{
+		x = 896,
+		y = 640,
+		width = 128,
+		height = 128,
+		name = "dirt1.edge.br"
+	},
+
+	{
+		x = 640,
+		y = 512,
+		width = 128,
+		height = 128,
+		name = "plate.edge.tl"
+	},
+
+	{
+		x = 768,
+		y = 512,
+		width = 128,
+		height = 128,
+		name = "plate.edge.tm"
+	},
+
+	{
+		x = 896,
+		y = 512,
+		width = 128,
+		height = 128,
+		name = "plate.edge.tr"
+	},
+
+	{
+		x = 640,
+		y = 384,
+		width = 128,
+		height = 128,
+		name = "plate.edge.ml"
+	},
+
+	{
+		x = 896,
+		y = 384,
+		width = 128,
+		height = 128,
+		name = "plate.edge.mr"
+	},
+
+	{
+		x = 640,
+		y = 256,
+		width = 128,
+		height = 128,
+		name = "plate.edge.bl"
+	},
+
+	{
+		x = 768,
+		y = 256,
+		width = 128,
+		height = 128,
+		name = "plate.edge.bm"
+	},
+
+	{
+		x = 896,
+		y = 256,
+		width = 128,
+		height = 128,
+		name = "plate.edge.br"
+	},
+
+	{
+		x = 256,
+		y = 512,
+		width = 128,
+		height = 128,
+		name = "dirt2.edge.tl"
+	},
+
+	{
+		x = 384,
+		y = 512,
+		width = 128,
+		height = 128,
+		name = "dirt2.edge.tm"
+	},
+
+	{
+		x = 512,
+		y = 512,
+		width = 128,
+		height = 128,
+		name = "dirt2.edge.tr"
+	},
+
+	{
+		x = 256,
+		y = 384,
+		width = 128,
+		height = 128,
+		name = "dirt2.edge.ml"
+	},
+
+	{
+		x = 512,
+		y = 384,
+		width = 128,
+		height = 128,
+		name = "dirt2.edge.mr"
+	},
+
+	{
+		x = 256,
+		y = 256,
+		width = 128,
+		height = 128,
+		name = "dirt2.edge.bl"
+	},
+
+	{
+		x = 384,
+		y = 256,
+		width = 128,
+		height = 128,
+		name = "dirt2.edge.bm"
+	},
+
+	{
+		x = 512,
+		y = 256,
+		width = 128,
+		height = 128,
+		name = "dirt2.edge.br"
+	}
+}
