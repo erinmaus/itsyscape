bfefc51c4783c036f58143a9d4887d4b23354536
Erin Maus <squeak@erinmaus.com>
2020-12-04 15:57:16 -0500

    Enable drop tables for foraging.

--- itsyscape/Resources/Game/Actions/Gather.lua
+++ itsyscape/Resources/Game/Actions/Gather.lua
@@ -11,12 +11,14 @@ local Class = require "ItsyScape.Common.Class"
 local Action = require "ItsyScape.Peep.Action"
 
 local Gather = Class(Action)
-Gather.SCOPES = {}
-Gather.FLAGS = {}
+Gather.SCOPES = { ['x-shake'] = true }
+Gather.FLAGS = { ['item-inventory'] = true, ['item-drop-excess'] = true }
 
 function Gather:perform(state, player, target)
-	Log.warn("NYI")
-	return false
+	local success = self:transfer(state, player)
+	Action.perform(self, state, player)
+
+	return success
 end
 
 return Gather
