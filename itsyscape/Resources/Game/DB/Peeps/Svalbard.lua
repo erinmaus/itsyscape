d2740f6fbfb5f6cd6b9628180d76ac7feb7c41c2
Erin Maus <adb@aaronbolyard.com>
2020-12-22 06:45:53 -0500

    Make Svalbard attackable.

--- itsyscape/Resources/Game/DB/Peeps/Svalbard.lua
+++ itsyscape/Resources/Game/DB/Peeps/Svalbard.lua
@@ -9,7 +9,7 @@
 --------------------------------------------------------------------------------
 
 ItsyScape.Resource.Peep "Svalbard" {
-	-- Nothing.
+	ItsyScape.Action.Attack()
 }
 
 ItsyScape.Meta.PeepID {
@@ -25,6 +25,138 @@ ItsyScape.Meta.ResourceName {
 
 ItsyScape.Meta.ResourceDescription {
 	Value = "A dragon raised from the dead, thirsty for blood.",
+	Language = "en-US",
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Attack",
+	Value = ItsyScape.Utility.xpForLevel(150),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Strength",
+	Value = ItsyScape.Utility.xpForLevel(150),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Defense",
+	Value = ItsyScape.Utility.xpForLevel(100),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Archery",
+	Value = ItsyScape.Utility.xpForLevel(100),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Dexterity",
+	Value = ItsyScape.Utility.xpForLevel(100),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Magic",
+	Value = ItsyScape.Utility.xpForLevel(100),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Wisdom",
+	Value = ItsyScape.Utility.xpForLevel(100),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Base",
+	DefenseStab = ItsyScape.Utility.styleBonusForItem(100, 1.1),
+	DefenseSlash = ItsyScape.Utility.styleBonusForItem(100, 1.1),
+	DefenseCrush = ItsyScape.Utility.styleBonusForItem(100, 1.1),
+	DefenseMagic = ItsyScape.Utility.styleBonusForItem(120, 1.2),
+	DefenseRanged = ItsyScape.Utility.styleBonusForItem(90, 0.9),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Weakened",
+	DefenseStab = ItsyScape.Utility.styleBonusForItem(80),
+	DefenseSlash = ItsyScape.Utility.styleBonusForItem(80),
+	DefenseCrush = ItsyScape.Utility.styleBonusForItem(80),
+	DefenseMagic = ItsyScape.Utility.styleBonusForItem(100),
+	DefenseRanged = ItsyScape.Utility.styleBonusForItem(60),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Attack (Melee)",
+	AccuracySlash = ItsyScape.Utility.styleBonusForWeapon(85),
+	StrengthMelee = ItsyScape.Utility.strengthBonusForWeapon(80),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Attack (Magic)",
+	AccuracyMagic = ItsyScape.Utility.styleBonusForWeapon(85),
+	StrengthMagic = ItsyScape.Utility.strengthBonusForWeapon(80),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Attack (Archery)",
+	AccuracyMagic = ItsyScape.Utility.styleBonusForWeapon(85),
+	StrengthMagic = ItsyScape.Utility.strengthBonusForWeapon(80),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Special Attack (Melee)",
+	AccuracyMagic = ItsyScape.Utility.styleBonusForWeapon(100),
+	StrengthMelee = ItsyScape.Utility.strengthBonusForWeapon(100),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Special Attack (Magic)",
+	AccuracyMagic = ItsyScape.Utility.styleBonusForWeapon(90),
+	StrengthMagic = ItsyScape.Utility.strengthBonusForWeapon(90),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Special Attack (Archery)",
+	AccuracyMagic = ItsyScape.Utility.styleBonusForWeapon(100),
+	StrengthMagic = ItsyScape.Utility.strengthBonusForWeapon(100),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.Equipment {
+	Name = "Special Attack (Dragonfyre)",
+	AccuracyMagic = ItsyScape.Utility.styleBonusForWeapon(150),
+	StrengthMagic = ItsyScape.Utility.strengthBonusForWeapon(150),
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Resource.Peep "Svalbard's organs" {
+	-- Nothing.
+}
+
+ItsyScape.Meta.PeepID {
+	Value = "Resources.Game.Peeps.Svalbard.SvalbardOrgans",
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.ResourceName {
+	Value = "Svalbard's organs",
+	Language = "en-US",
+	Resource = ItsyScape.Resource.Peep "Svalbard"
+}
+
+ItsyScape.Meta.ResourceDescription {
+	Value = "Svalbard's organs, protected by his ribs and scales.",
 	Language = "en-US",
 	Resource = ItsyScape.Resource.Peep "Svalbard"
 }
