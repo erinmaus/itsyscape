d7faad2e51ae5135c21b855dff7630befc752c95
Erin Maus <adb@aaronbolyard.com>
2021-01-05 22:04:05 -0500

    Update map.

--- itsyscape/Resources/Game/Maps/Trailer_Svalbard/DB/Default.lua
+++ itsyscape/Resources/Game/Maps/Trailer_Svalbard/DB/Default.lua
@@ -2,4 +2,532 @@ local M = {}
 
 M._MAP = ItsyScape.Resource.Map "Trailer_Svalbard"
 
+M["Bones_PileOfSkulls1"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 37.000000,
+		PositionY = 8.000000,
+		PositionZ = 35.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Bones_PileOfSkulls1",
+		Map = M._MAP,
+		Resource = M["Bones_PileOfSkulls1"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Bones_PileOfSkulls",
+		MapObject = M["Bones_PileOfSkulls1"]
+	}
+end
+
+M["RockClump_Variant_11"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 53.000000,
+		PositionY = 4.000000,
+		PositionZ = 37.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "RockClump_Variant_11",
+		Map = M._MAP,
+		Resource = M["RockClump_Variant_11"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "RockClump_Variant_1",
+		MapObject = M["RockClump_Variant_11"]
+	}
+end
+
+M["Bones_JustBones5"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 37.000000,
+		PositionY = 8.000000,
+		PositionZ = 27.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Bones_JustBones5",
+		Map = M._MAP,
+		Resource = M["Bones_JustBones5"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Bones_JustBones",
+		MapObject = M["Bones_JustBones5"]
+	}
+end
+
+M["Bones_JustBones3"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 29.000000,
+		PositionY = 4.000000,
+		PositionZ = 29.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Bones_JustBones3",
+		Map = M._MAP,
+		Resource = M["Bones_JustBones3"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Bones_JustBones",
+		MapObject = M["Bones_JustBones3"]
+	}
+end
+
+M["Rock_Variant_15"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 51.000000,
+		PositionY = 4.000000,
+		PositionZ = 11.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Rock_Variant_15",
+		Map = M._MAP,
+		Resource = M["Rock_Variant_15"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Rock_Variant_1",
+		MapObject = M["Rock_Variant_15"]
+	}
+end
+
+M["Rock_Variant_14"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 41.000000,
+		PositionY = 4.000000,
+		PositionZ = 15.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Rock_Variant_14",
+		Map = M._MAP,
+		Resource = M["Rock_Variant_14"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Rock_Variant_1",
+		MapObject = M["Rock_Variant_14"]
+	}
+end
+
+M["Rock_Variant_12"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 17.000000,
+		PositionY = 4.000000,
+		PositionZ = 39.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Rock_Variant_12",
+		Map = M._MAP,
+		Resource = M["Rock_Variant_12"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Rock_Variant_1",
+		MapObject = M["Rock_Variant_12"]
+	}
+end
+
+M["Rock_Variant_11"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 29.000000,
+		PositionY = 4.000000,
+		PositionZ = 49.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Rock_Variant_11",
+		Map = M._MAP,
+		Resource = M["Rock_Variant_11"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Rock_Variant_1",
+		MapObject = M["Rock_Variant_11"]
+	}
+end
+
+M["Torch_Default1"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 37.000000,
+		PositionY = 4.000000,
+		PositionZ = 37.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Torch_Default1",
+		Map = M._MAP,
+		Resource = M["Torch_Default1"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Torch_Default",
+		MapObject = M["Torch_Default1"]
+	}
+end
+
+M["Bones_JustBones2"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 31.000000,
+		PositionY = 4.000000,
+		PositionZ = 29.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Bones_JustBones2",
+		Map = M._MAP,
+		Resource = M["Bones_JustBones2"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Bones_JustBones",
+		MapObject = M["Bones_JustBones2"]
+	}
+end
+
+M["Bones_JustBones4"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 29.000000,
+		PositionY = 8.000000,
+		PositionZ = 35.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Bones_JustBones4",
+		Map = M._MAP,
+		Resource = M["Bones_JustBones4"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Bones_JustBones",
+		MapObject = M["Bones_JustBones4"]
+	}
+end
+
+M["Bones_JustBones1"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 31.000000,
+		PositionY = 4.000000,
+		PositionZ = 27.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Bones_JustBones1",
+		Map = M._MAP,
+		Resource = M["Bones_JustBones1"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Bones_JustBones",
+		MapObject = M["Bones_JustBones1"]
+	}
+end
+
+M["Rock_Variant_13"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 47.000000,
+		PositionY = 4.000000,
+		PositionZ = 27.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Rock_Variant_13",
+		Map = M._MAP,
+		Resource = M["Rock_Variant_13"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Rock_Variant_1",
+		MapObject = M["Rock_Variant_13"]
+	}
+end
+
+M["IsabelleIsland_AbandonedMine_Pillar2"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 31.000000,
+		PositionY = 4.000000,
+		PositionZ = 9.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "IsabelleIsland_AbandonedMine_Pillar2",
+		Map = M._MAP,
+		Resource = M["IsabelleIsland_AbandonedMine_Pillar2"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "IsabelleIsland_AbandonedMine_Pillar",
+		MapObject = M["IsabelleIsland_AbandonedMine_Pillar2"]
+	}
+end
+
+M["RockClump_Variant_12"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 19.000000,
+		PositionY = 6.000000,
+		PositionZ = 47.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "RockClump_Variant_12",
+		Map = M._MAP,
+		Resource = M["RockClump_Variant_12"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "RockClump_Variant_1",
+		MapObject = M["RockClump_Variant_12"]
+	}
+end
+
+M["IsabelleIsland_AbandonedMine_Pillar3"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 53.000000,
+		PositionY = 4.000000,
+		PositionZ = 19.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "IsabelleIsland_AbandonedMine_Pillar3",
+		Map = M._MAP,
+		Resource = M["IsabelleIsland_AbandonedMine_Pillar3"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "IsabelleIsland_AbandonedMine_Pillar",
+		MapObject = M["IsabelleIsland_AbandonedMine_Pillar3"]
+	}
+end
+
+M["Bones_Skull1"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 37.000000,
+		PositionY = 4.000000,
+		PositionZ = 29.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Bones_Skull1",
+		Map = M._MAP,
+		Resource = M["Bones_Skull1"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Bones_Skull",
+		MapObject = M["Bones_Skull1"]
+	}
+end
+
+M["Torch_Default4"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 33.000000,
+		PositionY = 4.000000,
+		PositionZ = 9.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Torch_Default4",
+		Map = M._MAP,
+		Resource = M["Torch_Default4"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Torch_Default",
+		MapObject = M["Torch_Default4"]
+	}
+end
+
+M["Torch_Default3"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 51.000000,
+		PositionY = 4.000000,
+		PositionZ = 19.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Torch_Default3",
+		Map = M._MAP,
+		Resource = M["Torch_Default3"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Torch_Default",
+		MapObject = M["Torch_Default3"]
+	}
+end
+
+M["Bones_Skull2"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 29.000000,
+		PositionY = 8.000000,
+		PositionZ = 27.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Bones_Skull2",
+		Map = M._MAP,
+		Resource = M["Bones_Skull2"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Bones_Skull",
+		MapObject = M["Bones_Skull2"]
+	}
+end
+
+M["Torch_Default2"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 29.000000,
+		PositionY = 4.000000,
+		PositionZ = 37.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "Torch_Default2",
+		Map = M._MAP,
+		Resource = M["Torch_Default2"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Torch_Default",
+		MapObject = M["Torch_Default2"]
+	}
+end
+
+M["ItsyRock_Default1"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 15.000000,
+		PositionY = 5.000000,
+		PositionZ = 15.000000,
+		RotationX = 0.000000,
+		RotationY = 0.000000,
+		RotationZ = 0.000000,
+		RotationW = 1.000000,
+		ScaleX = 1.000000,
+		ScaleY = 1.000000,
+		ScaleZ = 1.000000,
+		Name = "ItsyRock_Default1",
+		Map = M._MAP,
+		Resource = M["ItsyRock_Default1"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "ItsyRock_Default",
+		MapObject = M["ItsyRock_Default1"]
+	}
+end
+
 return M
