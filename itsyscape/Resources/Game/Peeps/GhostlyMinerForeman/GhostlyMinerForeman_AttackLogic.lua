15eb224fa2c834195faa1b014f74c44800e34375
Erin Maus <squeak@erinmaus.com>
2021-02-15 08:54:55 -0500

    Improve aggression switching mechanic.

--- itsyscape/Resources/Game/Peeps/GhostlyMinerForeman/GhostlyMinerForeman_AttackLogic.lua
+++ itsyscape/Resources/Game/Peeps/GhostlyMinerForeman/GhostlyMinerForeman_AttackLogic.lua
@@ -15,6 +15,14 @@ local BTreeBuilder = require "B.TreeBuilder"
 local Mashina = require "ItsyScape.Mashina"
 
 local AGGRESSOR = B.Reference("GhostlyMinerForeman", "AGGRESSOR")
+
+local TALK_DURATION = 2.5
+
+-- 10 ticks per second, ~5 seconds to target player
+local TARGET_PLAYER_CHANCE    = 1 / 50
+local AGGRESSOR_SWITCH_CHANCE = 1 / 2
+local AGGRESSOR_SWITCH_WAIT   = 10
+
 local Tree = BTreeBuilder.Node() {
 	Mashina.Step {
 		Mashina.Peep.Talk {
@@ -22,23 +30,63 @@ local Tree = BTreeBuilder.Node() {
 		},
 
 		Mashina.Repeat {
-			Mashina.Success {
-				Mashina.Sequence {
-					Mashina.Peep.WasAttacked {
-						took_damage = false,
-						[AGGRESSOR] = B.Output.aggressor
-					},
+			Mashina.Step {
+				Mashina.Success {
+					Mashina.ParallelTry {
+						Mashina.Step {
+							Mashina.Peep.WasAttacked {
+								took_damage = false,
+								[AGGRESSOR] = B.Output.aggressor
+							},
 
-					Mashina.RandomCheck {
-						chance = 1 / 2
-					},
+							Mashina.RandomCheck {
+								chance = AGGRESSOR_SWITCH_CHANCE
+							},
 
-					Mashina.Peep.EngageCombatTarget {
-						peep = AGGRESSOR
-					},
+							Mashina.Peep.EngageCombatTarget {
+								peep = AGGRESSOR
+							},
 
-					Mashina.Peep.Talk {
-						message = "Maggot!"
+							Mashina.Peep.Talk {
+								message = "Maggot!",
+								duration = TALK_DURATION
+							}
+						},
+
+						Mashina.Step {
+							Mashina.RandomCheck {
+								chance = TARGET_PLAYER_CHANCE
+							},
+
+							Mashina.Peep.GetPlayer {
+								[AGGRESSOR] = B.Output.player
+							},
+
+							Mashina.Peep.EngageCombatTarget {
+								peep = AGGRESSOR
+							},
+
+							Mashina.RandomSequence {
+								Mashina.Peep.Talk {
+									message = "You're unworthy of the Empty King's attention!",
+									duration = TALK_DURATION
+								},
+
+								Mashina.Peep.Talk {
+									message = "I will rip you apart!",
+									duration = TALK_DURATION
+								},
+
+								Mashina.Peep.Talk {
+									message = "The Empty King's plans must proceed!",
+									duration = TALK_DURATION
+								}
+							},
+
+							Mashina.Peep.TimeOut {
+								duration = AGGRESSOR_SWITCH_WAIT
+							}
+						}
 					}
 				}
 			}
