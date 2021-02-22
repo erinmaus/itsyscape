6fe90a0ad677ca3a65ff554da542cf0f293e99d0
Erin Maus <squeak@erinmaus.com>
2021-02-21 19:58:50 -0500

    Add ship dream.

---
+++ itsyscape/Resources/Game/Maps/Dream_CalmBeforeTheStorm_Ship/DB/Main.lua
@@ -0,0 +1,145 @@
+local M = include "Resources/Game/Maps/Dream_CalmBeforeTheStorm_Ship/DB/Default.lua"
+
+ItsyScape.Meta.PeepID {
+	Value = "Resources.Game.Maps.Dream_CalmBeforeTheStorm_Ship.Peep",
+	Resource = M._MAP
+}
+
+ItsyScape.Meta.ResourceName {
+	Value = "Dream Sequence",
+	Language = "en-US",
+	Resource = M._MAP
+}
+
+ItsyScape.Meta.ResourceDescription {
+	Value = "This dream takes place under the ocean.",
+	Language = "en-US",
+	Resource = M._MAP
+}
+
+M["Light_Ambient"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 0,
+		PositionY = 0,
+		PositionZ = 0,
+		Name = "Light_Ambient",
+		Map = M._MAP,
+		Resource = M["Light_Ambient"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "AmbientLight_Default",
+		MapObject = M["Light_Ambient"]
+	}
+
+	ItsyScape.Meta.Light {
+		ColorRed = 255,
+		ColorGreen = 255,
+		ColorBlue = 255,
+		Resource = M["Light_Ambient"]
+	}
+
+	ItsyScape.Meta.AmbientLight {
+		Ambience = 0.8,
+		Resource = M["Light_Ambient"]
+	}
+end
+
+M["Light_Sun"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 0,
+		PositionY = 0,
+		PositionZ = 0,
+		Name = "Light_Sun",
+		Map = M._MAP,
+		Resource = M["Light_Sun"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "DirectionalLight_Default",
+		MapObject = M["Light_Sun"]
+	}
+
+	ItsyScape.Meta.Light {
+		ColorRed = 255,
+		ColorGreen = 255,
+		ColorBlue = 255,
+		Resource = M["Light_Sun"]
+	}
+
+	ItsyScape.Meta.DirectionalLight {
+		DirectionX = 4,
+		DirectionY = 3,
+		DirectionZ = 4,
+		Resource = M["Light_Sun"]
+	}
+end
+
+M["Light_Fog"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 0,
+		PositionY = 0,
+		PositionZ = 0,
+		Name = "Light_Fog",
+		Map = M._MAP,
+		Resource = M["Light_Fog"]
+	}
+
+	ItsyScape.Meta.PropMapObject {
+		Prop = ItsyScape.Resource.Prop "Fog_Default",
+		MapObject = M["Light_Fog"]
+	}
+
+	ItsyScape.Meta.Fog {
+		ColorRed = 255,
+		ColorGreen = 255,
+		ColorBlue = 255,
+		NearDistance = 5,
+		FarDistance = 15,
+		FollowTarget = 1,
+		Resource = M["Light_Fog"]
+	}
+end
+
+M["Anchor_Spawn"] = ItsyScape.Resource.MapObject.Unique()
+do
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 33,
+		PositionY = 1,
+		PositionZ = 61,
+		Name = "Anchor_Spawn",
+		Map = M._MAP,
+		Resource = M["Anchor_Spawn"]
+	}
+end
+
+M["Anchor_Ship"] = ItsyScape.Resource.MapObject.Unique()
+do
+	local rotation1 = ItsyScape.Utility.Quaternion.fromAxisAngle(
+		ItsyScape.Utility.Vector.UNIT_X,
+		math.pi / 6)
+	local rotation2 = ItsyScape.Utility.Quaternion.fromAxisAngle(
+		ItsyScape.Utility.Vector.UNIT_Z,
+		math.pi / 6)
+	local rotation = rotation1 * rotation2
+
+	ItsyScape.Meta.MapObjectLocation {
+		PositionX = 33,
+		PositionY = 8,
+		PositionZ = 33,
+		RotationX = rotation.x,
+		RotationY = rotation.y,
+		RotationZ = rotation.z,
+		RotationW = rotation.w,
+		Name = "Anchor_Ship",
+		Map = M._MAP,
+		Resource = M["Anchor_Ship"]
+	}
+
+	M["Anchor_Ship"] {
+		TalkAction
+	}
+end
