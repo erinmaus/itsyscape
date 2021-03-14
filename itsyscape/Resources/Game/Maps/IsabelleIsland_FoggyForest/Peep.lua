6eb3e169151480dd70a21f1e846fb95e4926a4ff
Erin Maus <squeak@erinmaus.com>
2021-03-14 18:34:19 -0400

    Tweak mechanics of fight.

--- itsyscape/Resources/Game/Maps/IsabelleIsland_FoggyForest/Peep.lua
+++ itsyscape/Resources/Game/Maps/IsabelleIsland_FoggyForest/Peep.lua
@@ -8,8 +8,11 @@
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --------------------------------------------------------------------------------
 local Class = require "ItsyScape.Common.Class"
+local Vector = require "ItsyScape.Common.Math.Vector"
+local BossStat = require "ItsyScape.Game.BossStat"
 local Utility = require "ItsyScape.Game.Utility"
 local Map = require "ItsyScape.Peep.Peeps.Map"
+local BossStatsBehavior = require "ItsyScape.Peep.Behaviors.BossStatsBehavior"
 local CombatTargetBehavior = require "ItsyScape.Peep.Behaviors.CombatTargetBehavior"
 local CombatStatusBehavior = require "ItsyScape.Peep.Behaviors.CombatStatusBehavior"
 local ActorReferenceBehavior = require "ItsyScape.Peep.Behaviors.ActorReferenceBehavior"
@@ -26,7 +29,11 @@ FoggyForest.ANCHORS = {
 	"Anchor_SpawnClearing"
 }
 
-FoggyForest.BOSS = "IsabelleIsland_FoggyForest_BossyNymph"
+FoggyForest.NUM_FOES = 3
+FoggyForest.NUM_TICKS = 5
+
+FoggyForest.BOSS_UNATTACKABLE = "IsabelleIsland_FoggyForest_BossyNymph_Unattackable"
+FoggyForest.BOSS_ATTACKABLE = "IsabelleIsland_FoggyForest_BossyNymph"
 FoggyForest.BOSS_ANCHOR = "Anchor_SpawnClearing"
 
 function FoggyForest:new(resource, name, ...)
@@ -34,12 +41,48 @@ function FoggyForest:new(resource, name, ...)
 
 	self:addPoke('ancientDriftwoodTreeHit')
 	self:addPoke('ancientDriftwoodTreeFelled')
+
+	self.nymph = false
+
+	self:addBehavior(BossStatsBehavior)
+
+	self.numFoesStat = BossStat({
+		icon = "Resources/Game/UI/Icons/Concepts/Rage.png",
+		text = "Foes remaining",
+		inColor = { 0.78, 0.21, 0.21, 1.0 },
+		outColor = { 0.21, 0.67, 0.78, 1.0 },
+		current = 0,
+		max = 0
+	})
+
+	self.treeHealthStat = BossStat({
+		icon = "Resources/Game/Items/CopperHatchet/Icon.png",
+		text = "Tree health",
+		inColor = { 0.78, 0.21, 0.21, 1.0 },
+		outColor = { 0.21, 0.67, 0.78, 1.0 },
+		current = FoggyForest.NUM_TICKS,
+		max = FoggyForest.NUM_TICKS
+	})
+
+	local stats = self:getBehavior(BossStatsBehavior)
+	table.insert(stats.stats, self.numFoesStat)
+	table.insert(stats.stats, self.treeHealthStat)
 end
 
-function FoggyForest:onAncientDriftwoodTreeHit(e)
-	Log.info('Ancient driftwood tree hit; at %d ticks.', e.ticks)
+function FoggyForest:spawnBossyNymph()
+	local player = Utility.Peep.getPlayer(self)
+	local adjacent = Utility.Peep.getPosition(player) - Vector.UNIT_X * 2
+	local actor = Utility.spawnActorAtPosition(self, FoggyForest.BOSS_UNATTACKABLE, adjacent.x, adjacent.y, adjacent.z)
 
-	for i = 1, 3 do
+	self.nymph = actor:getPeep()
+
+	self.nymph:listen('finalize', function()
+		Utility.UI.openInterface(player, "BossHUD", false, self.nymph)
+	end)
+end
+
+function FoggyForest:spawnFoes(e)
+	for i = 1, FoggyForest.NUM_FOES do
 		local peep
 		do
 			local index = math.random(1, #FoggyForest.PEEPS)
@@ -56,19 +99,40 @@ function FoggyForest:onAncientDriftwoodTreeHit(e)
 		if actor then
 			Log.info("Spawned %s.", actor:getName())
 			actor:getPeep():listen('finalize', Utility.Peep.attack, actor:getPeep(), e.peep, math.huge)
+			actor:getPeep():listen('die', function()
+				self.numFoesStat.currentValue = self.numFoesStat.currentValue - 1
+			end)
 		else
 			Log.warn("Failed to spawn '%s' @ '%s'.", peep, anchor)
 		end
+
 	end
+
+	self.numFoesStat.currentValue = self.numFoesStat.currentValue + FoggyForest.NUM_FOES
+	self.numFoesStat.maxValue = self.numFoesStat.currentValue + FoggyForest.NUM_FOES
+end
+
+function FoggyForest:onAncientDriftwoodTreeHit(e)
+	Log.info('Ancient driftwood tree hit; at %d ticks.', e.ticks)
+
+	if not self.nymph then
+		Log.info("Spawning bossy nymph...")
+		self:spawnBossyNymph()
+	else
+		self:spawnFoes(e)
+	end
+
+	self.treeHealthStat.currentValue = self.treeHealthStat.currentValue - 1
 end
 
 function FoggyForest:onAncientDriftwoodTreeFelled(e)
 	Log.info('Ancient driftwood tree felled after %d ticks.', e.ticks)
 
-	local actor = Utility.spawnActorAtAnchor(self, FoggyForest.BOSS, FoggyForest.BOSS_ANCHOR)
-	if actor then
-		actor:getPeep():listen('finalize', Utility.Peep.attack, actor:getPeep(), e.peep, math.huge)
-	end
+	local gameDB = self:getDirector():getGameDB()
+	Utility.Peep.setResource(self.nymph, gameDB:getResource(FoggyForest.BOSS_ATTACKABLE, "Peep"))
+	Utility.Peep.attack(self.nymph, e.peep or Utility.Peep.getPlayer(self))
+
+	self.treeHealthStat.currentValue = self.treeHealthStat.currentValue - 1
 end
 
 return FoggyForest
