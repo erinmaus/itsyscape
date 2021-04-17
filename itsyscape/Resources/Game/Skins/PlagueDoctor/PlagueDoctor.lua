53bba0797c51d5f3b481072f07ad6de0b44b2527
Erin Maus <adb@aaronbolyard.com>
2021-04-17 10:53:12 -0400

    Add plague doctor hat & mask.

---
+++ itsyscape/Resources/Game/Skins/PlagueDoctor/PlagueDoctor.lua
@@ -0,0 +1,5 @@
+{
+	model = "Resources/Game/Skins/PlagueDoctor/PlagueDoctor.lmesh",
+	texture = "Resources/Game/Skins/PlagueDoctor/PlagueDoctor.png",
+	isOccluded = true
+}
---
+++ itsyscape/Resources/Game/Skins/PlagueDoctor/PlagueDoctorJustHat.lua
@@ -0,0 +1,6 @@
+{
+	model = "Resources/Game/Skins/PlagueDoctor/PlagueDoctorJustHat.lmesh",
+	texture = "Resources/Game/Skins/PlagueDoctor/PlagueDoctor.png",
+	isBlocking = false,
+	isGhosty = true
+}
