a36db66c53d275d2220dd94d3dd6a8f06cff04e5
Erin Maus <squeak@erinmaus.com>
2021-04-17 14:44:21 -0400

    Hex labs redesign WIP.

--- itsyscape/Resources/Game/Props/Door_HexLabs/View.lua
+++ itsyscape/Resources/Game/Props/Door_HexLabs/View.lua
@@ -8,12 +8,23 @@
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --------------------------------------------------------------------------------
 local Class = require "ItsyScape.Common.Class"
+local Vector = require "ItsyScape.Common.Math.Vector"
 local DoorView = require "Resources.Game.Props.Common.DoorView"
 
-local IronDoor = Class(DoorView)
+local Door = Class(DoorView)
 
-function IronDoor:getBaseFilename()
+function Door:getBaseFilename()
 	return "Resources/Game/Props/Door_HexLabs"
 end
 
-return IronDoor
+function Door:load()
+	DoorView.load(self)
+
+	local resources = self:getResources()
+	resources:queueEvent(
+		function()
+			self.node:getTransform():setLocalScale(Vector.ONE * 1 + Vector(1/2))
+		end)
+end
+
+return Door
