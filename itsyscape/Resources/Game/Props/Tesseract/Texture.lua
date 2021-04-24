50726d7306b6566810537ff5cd184d22fa09c823
Erin Maus <squeak@erinmaus.com>
2021-04-24 14:41:49 -0400

    More work on Tesseract.

--- itsyscape/Resources/Game/Props/Tesseract/Texture.lua
+++ itsyscape/Resources/Game/Props/Tesseract/Texture.lua
@@ -2,27 +2,27 @@
 	type = 'cube',
 
 	{
-		plusX  = "Inside.png",
-		plusY  = "Inside.png",
-		plusZ  = "Inside.png",
-		minusX = "Inside.png",
-		minusY = "Inside.png",
-		minusZ = "Inside.png"
+		all = "Nucleus.png"
 	},
 	{
-		plusX  = "Inside.png",
-		plusY  = "Inside.png",
-		plusZ  = "Inside.png",
-		minusX = "Inside.png",
-		minusY = "Inside.png",
-		minusZ = "Inside.png"
+		all = "Nucleus.png"
 	},
 	{
-		plusX  = "Outside.png",
-		plusY  = "Outside.png",
-		plusZ  = "Outside.png",
-		minusX = "Outside.png",
-		minusY = "Outside.png",
-		minusZ = "Outside.png"
+		all = "Nucleus.png"
+	},
+	{
+		all = "Nucleus.png"
+	},
+	{
+		all = "Inside.png"
+	},
+	{
+		all = "Inside.png"
+	},
+	{
+		all = "Outside.png"
+	},
+	{
+		all = "Outside.png"
 	}
 }
