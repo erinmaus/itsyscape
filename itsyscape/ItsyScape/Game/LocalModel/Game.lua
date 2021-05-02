291929b4c579f68329731b1361c6a598786af065
Erin Maus <squeak@erinmaus.com>
2021-05-02 16:00:02 -0400

    Save on quit.

--- itsyscape/ItsyScape/Game/LocalModel/Game.lua
+++ itsyscape/ItsyScape/Game/LocalModel/Game.lua
@@ -87,9 +87,54 @@ function LocalGame:update(delta)
 	self.stage:update(delta)
 end
 
+function LocalGame:saveGame()
+	local playerActor = self.player:getActor()
+	local playerPeep = playerActor and playerActor:getPeep()
+	if playerPeep then
+		local storage = self.director:getPlayerStorage(playerPeep)
+		local root = storage:getRoot()
+
+		local hasLocation = root:hasSection("Location")
+		if not hasLocation then
+			local finishedQuest = playerPeep:getState():has('Quest', "PreTutorial")
+			local startedQuest = playerPeep:getState():has('KeyItem', "PreTutorial_Start")
+
+			local map, anchor
+			if finishedQuest then
+				map = "IsabelleIsland_Tower_Floor5"
+				anchor = "Anchor_StartGame"
+			elseif startedQuest then
+				map = "PreTutorial_MansionFloor1"
+				anchor = "Anchor_Spawn"
+			end
+
+			if map and anchor then
+				local x, y, z = Utility.Map.getAnchorPosition(self, map, anchor)
+				local locationSection = root:getSection("Location")
+				locationSection:set({
+					name = map,
+					x = x,
+					y = y,
+					z = z,
+					layer = 1
+				})
+			end
+		end
+
+		hasLocation = root:hasSection("Location")
+		if hasLocation then
+			Utility.save(playerPeep, false)
+		end
+	end
+end
+
 function LocalGame:quit()
+	self.stage:collectItems()
+	self:saveGame()
+
 	self.stage:unloadAll()
 	self:tick()
+
 	self.player:poof()
 	self:tick()
 
