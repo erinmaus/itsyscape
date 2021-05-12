ec1fbd5764c46a1be9a27656a8a45ea88f417dd3
Erin Maus <squeak@erinmaus.com>
2021-05-12 13:39:57 -0400

    Add antenna.

---
+++ itsyscape/Resources/Game/Props/MysteriousMachinations_MysteriousRuins_Antenna/Texture_Active.lua
@@ -0,0 +1,19 @@
+{
+	type = 'cube',
+
+	{
+		all = "Nucleus.png"
+	},
+	{
+		all = "Nucleus.png"
+	},
+	{
+		all = "Nucleus.png"
+	},
+	{
+		all = "Nucleus.png"
+	},
+	{
+		all = "Inside.png"
+	}
+}
