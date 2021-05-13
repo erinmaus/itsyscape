cf60d36b2bb87aaf701c23686893b5eff2b1280e
Erin Maus <squeak@erinmaus.com>
2021-05-13 10:43:56 -0400

    Remove comment.

--- itsyscape/ItsyScape/Peep/Cortexes/MoveToTileCortex.lua
+++ itsyscape/ItsyScape/Peep/Cortexes/MoveToTileCortex.lua
@@ -82,51 +82,6 @@ function MoveToTileCortex:update(delta)
 			end
 		end
 	end
-
-	-- for peep in self:iterate() do
-	-- 	local position = peep:getBehavior(PositionBehavior).position
-	-- 	local targetTile = peep:getBehavior(TargetTileBehavior)
-	-- 	local movement = peep:getBehavior(MovementBehavior)
-	-- 	if movement.maxSpeed == 0 or movement.maxAcceleration == 0 or
-	-- 	   movement.velocityMultiplier == 0 or movement.accelerationMultiplier == 0
-	-- 	then
-	-- 		peep:removeBehavior(TargetTileBehavior)
-	-- 	elseif targetTile and movement and position then
-	-- 		local map = game:getDirector():getMap(peep:getBehavior(PositionBehavior).layer or 1)
-	-- 		if map then
-	-- 			movement.isStopping = false
-
-	-- 			local currentTile, currentTileI, currentTileJ = map:getTileAt(position.x, position.z)
-	-- 			local nextTile, nextTileI, nextTileJ = map:getTile(targetTile.pathNode.i, targetTile.pathNode.j)
-
-	-- 			local targetPosition = map:getTileCenter(nextTileI, nextTileJ)
-	-- 			local positionDifference = targetPosition - position
-	-- 			positionDifference.y = 0
-
-	-- 			local distance = positionDifference:getLength()
-	-- 			local direction = positionDifference:getNormal()
-
-	-- 			if distance > targetTile.distance then
-	-- 				-- If we're too far from the tile, steer towards it.
-	-- 				local currentVelocityMagnitude = movement.velocity:getLength()
-	-- 				local desiredSpeed = movement.maxSpeed * movement.velocityMultiplier
-	-- 				local desiredVelocity = desiredSpeed * direction
-	-- 				local velocityDifference = desiredVelocity - movement.velocity
-	-- 				local d = 1 - (currentVelocityMagnitude / desiredVelocity:getLength())
-	-- 				movement.acceleration = movement.acceleration * d + velocityDifference
-	-- 			else
-	-- 				peep:removeBehavior(TargetTileBehavior)
-
-	-- 				-- Otherwise, activate next node (if possible).
-	-- 				if targetTile.nextPathNode then
-	-- 					targetTile.nextPathNode:activate(peep)
-	-- 				else
-	-- 					movement.isStopping = true
-	-- 				end
-	-- 			end
-	-- 		end
-	-- 	end
-	-- end
 end
 
 return MoveToTileCortex
