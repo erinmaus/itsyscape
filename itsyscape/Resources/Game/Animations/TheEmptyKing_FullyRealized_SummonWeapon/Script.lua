cf1f4e0b3c019d9f85f9d9584a7e90b4f45f8d19
Erin Maus <squeak@erinmaus.com>
2021-06-17 20:20:59 -0400

    Initial draft of The Empty King, fully realized.

---
+++ itsyscape/Resources/Game/Animations/TheEmptyKing_FullyRealized_SummonWeapon/Script.lua
@@ -0,0 +1,7 @@
+Animation "The Empty King (Fully Realized) Summon Weapon" {
+	Target {
+		PlayAnimation "Resources/Game/Animations/TheEmptyKing_FullyRealized_SummonWeapon/Animation.lanim" {
+			repeatAnimation = true
+		}
+	}
+}
