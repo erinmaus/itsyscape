8825686d0ecb03bc01b805621003521215d626ad
Erin Maus <squeak@erinmaus.com>
2021-07-07 10:28:34 -0400

    Add mummy & ancient skeleton peeps.

--- itsyscape/Resources/Game/DB/Creeps/Skeleton.lua
+++ itsyscape/Resources/Game/DB/Creeps/Skeleton.lua
@@ -107,3 +107,138 @@ ItsyScape.Meta.ResourceDescription {
 	Language = "en-US",
 	Resource = ItsyScape.Resource.Peep "Skeleton_Base_Attackable"
 }
+
+ItsyScape.Resource.Peep "AncientSkeleton" {
+	ItsyScape.Action.Attack()
+}
+
+ItsyScape.Meta.ResourceTag {
+	Value = "Undead",
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Meta.PeepID {
+	Value = "Resources.Game.Peeps.Skeleton.BaseAncientSkeleton",
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Meta.ResourceName {
+	Value = "Ancient skeleton",
+	Language = "en-US",
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Meta.ResourceDescription {
+	Value = "Spooky!",
+	Language = "en-US",
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Attack",
+	Value = ItsyScape.Utility.xpForLevel(40),
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Strength",
+	Value = ItsyScape.Utility.xpForLevel(40),
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Defense",
+	Value = ItsyScape.Utility.xpForLevel(30),
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Constitution",
+	Value = ItsyScape.Utility.xpForLevel(150),
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Meta.Equipment {
+	AccuracyCrush = ItsyScape.Utility.styleBonusForWeapon(55, 0),
+	DefenseStab   = ItsyScape.Utility.styleBonusForItem(50, 1.3),
+	DefenseSlash  = ItsyScape.Utility.styleBonusForItem(45, 1.3),
+	DefenseCrush  = ItsyScape.Utility.styleBonusForItem(40, 1.3),
+	DefenseMagic  = ItsyScape.Utility.styleBonusForItem(30, 0.5),
+	DefenseRanged = ItsyScape.Utility.styleBonusForItem(35, 1.0),
+	StrengthMelee = ItsyScape.Utility.strengthBonusForWeapon(55),
+	Resource = ItsyScape.Resource.Peep "AncientSkeleton"
+}
+
+ItsyScape.Resource.Peep "PrestigiousAncientSkeleton" {
+	ItsyScape.Action.Attack()
+}
+
+ItsyScape.Meta.ResourceTag {
+	Value = "Undead",
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepID {
+	Value = "Resources.Game.Peeps.Skeleton.BaseAncientSkeleton",
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.ResourceName {
+	Value = "Prestigious ancient skeleton",
+	Language = "en-US",
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.ResourceDescription {
+	Value = "A li'l empty headed.",
+	Language = "en-US",
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Attack",
+	Value = ItsyScape.Utility.xpForLevel(45),
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Strength",
+	Value = ItsyScape.Utility.xpForLevel(45),
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Defense",
+	Value = ItsyScape.Utility.xpForLevel(30),
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepStat {
+	Skill = ItsyScape.Resource.Skill "Constitution",
+	Value = ItsyScape.Utility.xpForLevel(150),
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepEquipmentItem {
+	Item = ItsyScape.Resource.Item "AncientCeremonialRobe",
+	Count = 1,
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepEquipmentItem {
+	Item = ItsyScape.Resource.Item "AncientCeremonialBoots",
+	Count = 1,
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepEquipmentItem {
+	Item = ItsyScape.Resource.Item "AncientCeremonialGloves",
+	Count = 1,
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
+
+ItsyScape.Meta.PeepEquipmentItem {
+	Item = ItsyScape.Resource.Item "AncientZweihander",
+	Count = 1,
+	Resource = ItsyScape.Resource.Peep "PrestigiousAncientSkeleton"
+}
