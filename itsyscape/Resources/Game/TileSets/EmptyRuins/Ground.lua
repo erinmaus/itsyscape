0ee13b3bd27d132693092b4a14cd71ec1941a4aa
Erin Maus <squeak@erinmaus.com>
2021-08-11 08:51:43 -0400

    Add more ground decos.

--- itsyscape/Resources/Game/TileSets/EmptyRuins/Ground.lua
+++ itsyscape/Resources/Game/TileSets/EmptyRuins/Ground.lua
@@ -17,11 +17,70 @@ local EmptyRuinsGround = Class(GroundDecorations)
 EmptyRuinsGround.GRASS_SATURATION = 4
 EmptyRuinsGround.GRASS_THRESHOLD  = 0.45
 EmptyRuinsGround.GRASS_FUDGE = 0.83
+EmptyRuinsGround.GRASS_COLOR = Color(0.4, 0.6, 0.5, 1.0)
+
+EmptyRuinsGround.STONE_THRESHOLD = 0.45
+EmptyRuinsGround.STONE_TYPE_FUDGE = 7.934058
+EmptyRuinsGround.STONE_PURPLE_COLOR = Color(0.3, 0.3, 0.5, 1.0)
+EmptyRuinsGround.STONE_GREEN_COLOR = Color(0.3, 0.5, 0.3, 1.0)
+EmptyRuinsGround.STONE_SKULL_DECO = 0.35
+EmptyRuinsGround.STONE_CRACK_DECO = 0.4
+EmptyRuinsGround.STONE_BRICK_DECO = 1.0
+EmptyRuinsGround.STONE_NUM_BRICK = 3
 
 function EmptyRuinsGround:new()
 	GroundDecorations.new(self, "EmptyRuins")
 
 	self:registerTile("grass", self.emitGrass)
+	self:registerTile("purple-stone", self.emitStone)
+	self:registerTile("green-stone", self.emitStone)
+end
+
+function EmptyRuinsGround:placeSkull(tileSet, map, i, j, tileSetTile, mapTile)
+	local center = map:getTileCenter(i, j)
+	local noise = self:noise(center.x / 4, center.y / 4, center.z / 4)
+	local rotation = Quaternion.fromAxisAngle(Vector.UNIT_Y, noise * math.pi * 2)
+	self:addFeature("skull", center, rotation, Vector((noise + 1) * 2))
+end
+
+function EmptyRuinsGround:placeCrack(tileSet, map, i, j, tileSetTile, mapTile)
+	local center = map:getTileCenter(i, j)
+	local texture = math.floor(self:noise(center.x, center.y, center.z, 0.25) * EmptyRuinsGround.STONE_NUM_CRACK + 0.5)
+	local noise = self:noise(center.x, center.y, center.z)
+	local rotation = Quaternion.fromAxisAngle(Vector.UNIT_Y, noise * math.pi * 2)
+	self:addFeature("crack" .. texture, center, rotation)
+end
+
+function EmptyRuinsGround:placeBricks(tileSet, map, i, j, tileSetTile, mapTile)
+	local center = map:getTileCenter(i, j)
+	local texture = math.floor(self:noise(center.x, center.y, center.z, 0.25) * EmptyRuinsGround.STONE_NUM_BRICK + 0.5)
+	local colorScale = (self:noise(center.x, center.y, center.z) + 1) / 2
+	local noise = self:noise(center.x, center.y, center.z)
+
+	local featureColor
+	if tileSetTile.name == "purple-stone" then
+		featureColor = EmptyRuinsGround.STONE_PURPLE_COLOR
+	elseif tileSetTile.name == "green-stone" then
+		featureColor = EmptyRuinsGround.STONE_GREEN_COLOR
+	else
+		Log.warn("Unhandled stone type: '%s'.", tileSetTile.name)
+		featureColor = Color(1, 0, 0, 1)
+	end
+
+	self:addFeature(
+		"brick" .. texture,
+		center,
+		Quaternion.IDENTITY,
+		Vector.ONE,
+		featureColor * Color(colorScale, colorScale, colorScale, 1))
+end
+
+function EmptyRuinsGround:emitStone(tileSet, map, i, j, tileSetTile, mapTile)
+	local center = map:getTileCenter(i, j)
+	local noise = self:noise(center.x, center.y, center.z)
+	if noise < EmptyRuinsGround.STONE_THRESHOLD then
+		self:placeBricks(tileSet, map, i, j, tileSetTile, mapTile)
+	end
 end
 
 function EmptyRuinsGround:emitGrass(tileSet, map, i, j, tileSetTile, mapTile)
@@ -57,7 +116,7 @@ function EmptyRuinsGround:emitGrass(tileSet, map, i, j, tileSetTile, mapTile)
 					Vector(offsetX, offsetY, offsetZ),
 					Quaternion.IDENTITY,
 					Vector(scale),
-					Color(0.4 * color, 0.6 * color, 0.5 * color, 1.0))
+					EmptyRuinsGround.GRASS_COLOR * Color(color, color, color, 1))
 			end
 		end
 	end
