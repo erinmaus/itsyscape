35fa632d014b2a70877f53c6387e7d6d1e157948
Erin Maus <squeak@erinmaus.com>
2021-09-23 11:25:08 -0400

    Initial work on TV mimic.

---
+++ itsyscape/Resources/Game/Peeps/ChestMimic/TVMimic.lua
@@ -0,0 +1,82 @@
+--------------------------------------------------------------------------------
+-- Resources/Peeps/ChestMimic/TVMimic.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
+local Vector = require "ItsyScape.Common.Math.Vector"
+local Quaternion = require "ItsyScape.Common.Math.Quaternion"
+local CacheRef = require "ItsyScape.Game.CacheRef"
+local Utility = require "ItsyScape.Game.Utility"
+local Equipment = require "ItsyScape.Game.Equipment"
+local Creep = require "ItsyScape.Peep.Peeps.Creep"
+local ActorReferenceBehavior = require "ItsyScape.Peep.Behaviors.ActorReferenceBehavior"
+local MovementBehavior = require "ItsyScape.Peep.Behaviors.MovementBehavior"
+local RotationBehavior = require "ItsyScape.Peep.Behaviors.RotationBehavior"
+local SizeBehavior = require "ItsyScape.Peep.Behaviors.SizeBehavior"
+
+local TVMimic = Class(Creep)
+
+function TVMimic:new(resource, name, ...)
+	Creep.new(self, resource, name or 'TVMimic', ...)
+
+	local movement = self:getBehavior(MovementBehavior)
+	movement.stoppingForce = 0.5
+	movement.maxSpeed = 5
+	movement.decay = 0.6
+	movement.maxAcceleration = 18
+	movement.velocityMultiplier = 1.1
+	movement.accelerationMultiplier = 1.1
+
+	self:addBehavior(RotationBehavior)
+
+	local size = self:getBehavior(SizeBehavior)
+	size.size = Vector(2, 1, 2)
+	size.zoom = 2.5
+	size.yPan = Vector(0, 0.5, 0)
+end
+
+function TVMimic:ready(director, game)
+	local actor = self:getBehavior(ActorReferenceBehavior)
+	if actor and actor.actor then
+		actor = actor.actor
+	end
+
+	local body = CacheRef(
+		"ItsyScape.Game.Body",
+		"Resources/Game/Bodies/TVMimic.lskel")
+	actor:setBody(body)
+
+	local idleAnimation = CacheRef(
+		"ItsyScape.Graphics.AnimationResource",
+		"Resources/Game/Animations/TVMimic_Idle/Script.lua")
+	self:addResource("animation-idle", idleAnimation)
+
+	-- local walkAnimation = CacheRef(
+	-- 	"ItsyScape.Graphics.AnimationResource",
+	-- 	"Resources/Game/Animations/TVMimic_Walk/Script.lua")
+	-- self:addResource("animation-walk", walkAnimation)
+
+	-- local dieAnimation = CacheRef(
+	-- 	"ItsyScape.Graphics.AnimationResource",
+	-- 	"Resources/Game/Animations/TVMimic_Die/Script.lua")
+	-- self:addResource("animation-die", dieAnimation)
+
+	-- local attackAnimation = CacheRef(
+	-- 	"ItsyScape.Graphics.AnimationResource",
+	-- 	"Resources/Game/Animations/TVMimic_Attack/Script.lua")
+	-- self:addResource("animation-attack", attackAnimation)
+
+	Creep.ready(self, director, game)
+end
+
+function TVMimic:update(...)
+	Creep.update(self, ...)
+	Utility.Peep.face3D(self)
+end
+
+return TVMimic
---
+++ itsyscape/Resources/Game/Props/TV_Mimic/View.lua
@@ -0,0 +1,128 @@
+--------------------------------------------------------------------------------
+-- Resources/Game/Props/TV_HexLabs/View.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
+local Vector = require "ItsyScape.Common.Math.Vector"
+local DecorationSceneNode = require "ItsyScape.Graphics.DecorationSceneNode"
+local Color = require "ItsyScape.Graphics.Color"
+local PropView = require "ItsyScape.Graphics.PropView"
+local StaticMeshResource = require "ItsyScape.Graphics.StaticMeshResource"
+local TextureResource = require "ItsyScape.Graphics.TextureResource"
+local VideoResource = require "ItsyScape.Graphics.VideoResource"
+
+local TVMimic = Class(PropView)
+TVMimic.WIDTH = 256
+TVMimic.HEIGHT = 128
+
+function TVMimic:new(prop, gameView)
+	PropView.new(self, prop, gameView)
+
+	self.spawned = false
+	self.transform = love.math.newTransform()
+end
+
+function TVMimic:load()
+	PropView.load(self)
+
+	local resources = self:getResources()
+	local root = self:getRoot()
+
+	resources:queue(
+		StaticMeshResource,
+		"Resources/Game/Props/TV/Model.lstatic",
+		function(mesh)
+			self.mesh = mesh
+		end)
+	resources:queue(
+		TextureResource,
+		"Resources/Game/Props/TV/Texture.png",
+		function(texture)
+			self.texture = texture
+		end)
+	resources:queue(
+		VideoResource,
+		"Resources/Game/Videos/Tutorial/Static.ogv",
+		function(video)
+			self.video = video:getResource()
+			self.video:play()
+			self.video.onVideoFinished:register(function()
+				self.video:rewind()
+			end)
+		end)
+	resources:queueEvent(function()
+		self.tv = DecorationSceneNode()
+		self.tv:fromGroup(self.mesh:getResource(), "TV")
+		self.tv:getMaterial():setTextures(self.texture)
+		self.tv:setParent(root)
+		self.tv:onWillRender(self:getApplyTransformCallback())
+
+		self.screen = DecorationSceneNode()
+		self.screen:fromGroup(self.mesh:getResource(), "TVScreen")
+		self.screen:getMaterial():setTextures(self.texture)
+		self.screen:setParent(root)
+		self.screen:onWillRender(self:getApplyTransformCallback())
+
+		self.screen:onWillRender(function(_, delta)
+			local texture = self.video:getSnapshot()
+			if not self.screenTexture or self.screenTexture:getResource() ~= texture then
+				self.screenTexture = TextureResource(texture)
+			end
+
+			self.screen:getMaterial():setTextures(self.screenTexture)
+		end)
+	end)
+end
+
+function PropView:getIsStatic()
+	return not self.spawned
+end
+
+function TVMimic:tick()
+	PropView.tick(self)
+
+	if self.tv then
+		local state = self:getProp():getState()
+		self.screen:getMaterial():setColor(Color(unpack(state.color or { 1, 1, 1, 1 })))
+	end
+
+	self.spawned = true
+end
+
+function TVMimic:getApplyTransformCallback()
+	return function()
+		love.graphics.transform(self.transform)
+	end
+end
+
+
+function TVMimic:calculateTransform(actorID)
+	local gameView = self:getGameView()
+	local game = gameView:getGame()
+	local actor = game:getStage():getActorByID(actorID)
+	local actorView = gameView:getActor(actor)
+
+	self.transform = actorView:getLocalBoneTransform("root")
+end
+
+function TVMimic:update(delta)
+	PropView.update(self, delta)
+
+	if self.video then
+		self.video:update()
+	end
+
+	self.video:makeSnapshot()
+
+	local state = self:getProp():getState()
+	if state.actorID then
+		self:calculateTransform(state.actorID)
+	end
+end
+
+return TVMimic
