6a72f9d142e35d5de38f83e5031b94f95faba368
Erin Maus <squeak@erinmaus.com>
2022-03-13 20:15:37 -0400

    Add magma jellyfish.

---
+++ itsyscape/Resources/Game/Items/X_MagmaJellyfishAttack/Logic.lua
@@ -0,0 +1,45 @@
+--------------------------------------------------------------------------------
+-- Resources/Game/Items/X_MagmaJellyfishAttack/Logic.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
+local Equipment = require "ItsyScape.Game.Equipment"
+local Utility = require "ItsyScape.Game.Utility"
+local MagicWeapon = require "ItsyScape.Game.MagicWeapon"
+
+local MagmaJellyfishAttack = Class(MagicWeapon)
+MagmaJellyfishAttack.AMMO = Equipment.AMMO_NONE
+
+function MagmaJellyfishAttack:perform(peep, target)
+	local position = Utility.Peep.getPosition(target)
+	local p = Utility.spawnPropAtPosition(
+		peep,
+		"MagmaJellyfishRock",
+		position.x,
+		position.y,
+		position.z,
+		0.0)
+
+	self:applyCooldown(peep)
+
+	return true
+end
+
+function MagmaJellyfishAttack:getAttackRange()
+	return 8
+end
+
+function MagmaJellyfishAttack:getProjectile()
+	return 'MagmaJellyfishRock'
+end
+
+function MagmaJellyfishAttack:getCooldown()
+	return 1.8
+end
+
+return MagmaJellyfishAttack
---
+++ itsyscape/Resources/Game/Skins/Jellyfish/MagmaJellyfish.lua
@@ -0,0 +1,15 @@
+{
+	model = "Resources/Game/Skins/Jellyfish/Jellyfish.lmesh",
+	texture = "Resources/Game/Skins/Jellyfish/MagmaJellyfish.png",
+	position = { 0, 8, 0 },
+	scale = { 2, 2, 2 },
+	fullLit = true,
+	lights = {
+		{
+			type = 'point',
+			color = { 1, 1, 1 },
+			position = { 0, -4, 0 },
+			attenuation = 12
+		}
+	}
+}
