f0aaf413b602dcf098605e982d6fda7f27cd5da6
Erin Maus <squeak@erinmaus.com>
2022-03-29 11:15:55 -0400

    Add ground decos to other tile sets.

--- itsyscape/Resources/Game/TileSets/CommonShip_Map/Ground.lua
+++ itsyscape/Resources/Game/TileSets/CommonShip_Map/Ground.lua
@@ -1,5 +1,5 @@
 --------------------------------------------------------------------------------
--- Resources/Game/TileSets/EmptyRuins/Ground.lua
+-- Resources/Game/TileSets/CommonShip_Ground/Ground.lua
 --
 -- This file is a part of ItsyScape.
 --
@@ -15,36 +15,17 @@ local GroundDecorations = require "ItsyScape.World.GroundDecorations"
 local BrickBlock = require "ItsyScape.World.GroundDecorations.BrickBlock"
 local GrassBlock = require "ItsyScape.World.GroundDecorations.GrassBlock"
 
-local EmptyRuinsGround = Class(GroundDecorations)
+local CommonShipGround = Class(GroundDecorations)
 
-function EmptyRuinsGround:new()
-	GroundDecorations.new(self, "EmptyRuins")
-
-	self:registerTile("grass", GrassBlock:Bind(self) {
-		COLOR = Color(0.3, 0.5, 0.4, 1.0),
-		FEATURE = "grass"
-	})
-
-	self:registerTile("purple-stone", BrickBlock:Bind(self) {
-		COLOR = Color(0.3, 0.3, 0.5, 1.0),
-		FEATURE = "brick",
-		SCALE = Vector.ONE,
-		NUM_FEATURES = 3
-	})
-
-	self:registerTile("green-stone", BrickBlock:Bind(self) {
-		COLOR = Color(0.3, 0.5, 0.3, 1.0),
-		FEATURE = "brick",
-		SCALE = Vector.ONE,
-		NUM_FEATURES = 3
-	})
+function CommonShipGround:new()
+	GroundDecorations.new(self, "CommonShip_Map")
 
 	self:registerTile("wood", BrickBlock:Bind(self) {
-		COLOR = Color(0.3, 0.2, 0.3, 1.0),
-		FEATURE = "brick",
-		SCALE = Vector(2, 1, 1),
+		COLOR = Color(0.5, 0.3, 0.2, 1.0),
+		FEATURE = "plank",
+		SCALE = Vector.ONE,
 		NUM_FEATURES = 2
 	})
 end
 
-return EmptyRuinsGround
+return CommonShipGround
