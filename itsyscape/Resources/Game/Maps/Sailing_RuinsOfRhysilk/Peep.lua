9de008f1796eab78eb35aac6ad2df6ce6b13a191
Erin Maus <squeak@erinmaus.com>
2022-03-31 20:08:47 -0400

    Add lightning.

--- itsyscape/Resources/Game/Maps/Sailing_RuinsOfRhysilk/Peep.lua
+++ itsyscape/Resources/Game/Maps/Sailing_RuinsOfRhysilk/Peep.lua
@@ -9,12 +9,18 @@
 --------------------------------------------------------------------------------
 local Class = require "ItsyScape.Common.Class"
 local Vector = require "ItsyScape.Common.Math.Vector"
+local CacheRef = require "ItsyScape.Game.CacheRef"
 local Utility = require "ItsyScape.Game.Utility"
+local Probe = require "ItsyScape.Peep.Probe"
 local Map = require "ItsyScape.Peep.Peeps.Map"
 local PositionBehavior = require "ItsyScape.Peep.Behaviors.PositionBehavior"
 local SailorsCommon = require "Resources.Game.Peeps.Sailors.Common"
 
 local Ruins = Class(Map)
+Ruins.MIN_LIGHTNING_PERIOD = 2
+Ruins.MAX_LIGHTNING_PERIOD = 4
+Ruins.LIGHTNING_TIME = 0.5
+Ruins.MAX_AMBIENCE = 2
 
 function Ruins:new(resource, name, ...)
 	Map.new(self, resource, name or 'RuinsOfRhysilk', ...)
@@ -30,13 +36,56 @@ function Ruins:onLoad(filename, args, layer)
 	local stage = self:getDirector():getGameInstance():getStage()
 	stage:forecast(layer, 'Sailing_RuinsOfRhysilk_HeavyRain', 'Rain', {
 		wind = { -15, 0, 0 },
-		heaviness = 0.5
+		heaviness = 1
 	})
 
 	local player = Utility.Peep.getPlayer(self)
 	local firstMate, pending = SailorsCommon.getActiveFirstMateResource(player)
 	if not pending then
 		Utility.spawnActorAtAnchor(self, firstMate, "Anchor_FirstMate", 0)
+	end
+
+	self.lightning = self:getDirector():probe(
+		self:getLayerName(),
+		Probe.namedMapObject("Light_Lightning"))[1]
+	self.lightningTime = 0
+	self:zap()
+end
+
+function Ruins:zap()
+	self.wait = math.random() * (self.MAX_LIGHTNING_PERIOD - self.MIN_LIGHTNING_PERIOD) + self.MIN_LIGHTNING_PERIOD
+	Log.info("CRACKLE-BOOM-ZAP!")
+end
+
+function Ruins:boom()
+	local actor = self:getDirector():getGameInstance():getPlayer():getActor()
+	if actor then
+		local animation = CacheRef(
+			"ItsyScape.Graphics.AnimationResource",
+			"Resources/Game/Animations/SFX_LightningStrike/Script.lua")
+		actor:playAnimation(
+			'x-haunted-mansion-lightning',
+			math.huge,
+			animation)
+	end
+end
+
+function Ruins:update(director, game)
+	local delta = game:getDelta()
+
+	if self.lightningTime > 0 then
+		self.lightningTime = self.lightningTime - delta
+
+		local mu = math.min(math.max(self.lightningTime / self.LIGHTNING_TIME, 0), 1)
+
+		self.lightning:setAmbience(self.MAX_AMBIENCE * mu)
+	else
+		self.wait = self.wait - delta
+		if self.wait <= 0 then
+			self:zap()
+			self:boom()
+			self.lightningTime = self.LIGHTNING_TIME
+		end
 	end
 end
 
