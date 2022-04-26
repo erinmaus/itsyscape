724f59a4d94ed7b8bd5926493c8a47287bc6aa09
Erin Maus <squeak@erinmaus.com>
2022-04-26 16:34:08 -0400

    Refactor 'DebugStats' into its own class.

--- itsyscape/ItsyScape/Graphics/Renderer.lua
+++ itsyscape/ItsyScape/Graphics/Renderer.lua
@@ -9,6 +9,7 @@
 --------------------------------------------------------------------------------
 local Class = require "ItsyScape.Common.Class"
 local Color = require "ItsyScape.Graphics.Color"
+local DebugStats = require "ItsyScape.Graphics.DebugStats"
 local DeferredRendererPass = require "ItsyScape.Graphics.DeferredRendererPass"
 local ForwardRendererPass = require "ItsyScape.Graphics.ForwardRendererPass"
 local MobileRendererPass = require "ItsyScape.Graphics.MobileRendererPass"
@@ -16,6 +17,14 @@ local MobileRendererPass = require "ItsyScape.Graphics.MobileRendererPass"
 -- Renderer type. Manages rendering resources and logic.
 local Renderer = Class()
 Renderer.DEFAULT_CLEAR_COLOR = Color(0.39, 0.58, 0.93, 1)
+
+Renderer.DebugStats = Class(DebugStats)
+
+function Renderer.DebugStats:process(node, renderer, delta)
+	node:beforeDraw(renderer, delta)
+	node:draw(renderer, delta)
+	node:afterDraw(renderer, delta)
+end
 
 function Renderer:new(isMobile)
 	self.cachedShaders = {}
@@ -36,6 +45,8 @@ function Renderer:new(isMobile)
 
 	self.cull = true
 	self.startTime = love.timer.getTime()
+
+	self.debugStats = Renderer.DebugStats()
 end
 
 function Renderer:getCullEnabled()
@@ -64,6 +75,10 @@ end
 
 function Renderer:setCamera(value)
 	self.camera = value or self.camera
+end
+
+function Renderer:getDebugStats()
+	return self.debugStats
 end
 
 function Renderer:clean()
@@ -194,67 +209,8 @@ function Renderer:releaseCachedShaders()
 	self.cachedShaders = {}
 end
 
-if _DEBUG then
-	function Renderer:renderNode(node, delta)
-		local debugStats = self.debugStats or {}
-
-		local nodeName = node:getDebugInfo().shortName
-		local stat = debugStats[nodeName] or { min = math.huge, max = -math.huge, currentTotal = 0, samples = 0 }
-
-		local before = love.timer.getTime()
-		node:beforeDraw(self, delta)
-		node:draw(self, delta)
-		node:afterDraw(self, delta)
-		local after = love.timer.getTime()
-		local duration = after - before
-
-		stat.min = math.min(stat.min, duration)
-		stat.max = math.max(stat.max, duration)
-		stat.currentTotal = stat.currentTotal + duration
-		stat.samples = stat.samples + 1
-
-		debugStats[nodeName] = stat
-		self.debugStats = debugStats
-	end
-
-	function Renderer:dumpStatsToCSV(filename)
-		local debugStats = {}
-		do
-			local unsortedDebugStats = self.debugStats or {}
-			for nodeName, stats in pairs(unsortedDebugStats) do
-				table.insert(debugStats, {
-					nodeName = nodeName,
-					stats = stats
-				})
-			end
-
-			table.sort(debugStats, function(a, b)
-				return a.nodeName < b.nodeName
-			end)
-		end
-
-		local file = io.open(filename, "w")
-
-		for i = 1, #debugStats do
-			local stats = debugStats[i].stats
-			local nodeName = debugStats[i].nodeName
-			local f = string.format(
-				"%s, %f, %f, %f, %f, %f\n",
-				nodeName, stats.min, stats.max, stats.currentTotal, stats.samples, stats.currentTotal / stats.samples)
-
-			file:write(f)
-		end
-	end
-else
-	function Renderer:renderNode(node, delta)
-		node:beforeDraw(self, delta)
-		node:draw(self, delta)
-		node:afterDraw(self, delta)
-	end
-
-	function Renderer:dumpStatsToCSV()
-		-- Nothing.
-	end
+function Renderer:renderNode(node, delta)
+	self.debugStats:measure(node, self, delta)
 end
 
 return Renderer
