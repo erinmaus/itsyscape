3755312e1c6ed154384ddcc3343225cf77601c5f
Erin Maus <squeak@erinmaus.com>
2022-09-01 10:31:55 -0400

    Abstract IPC queue to RPCService.

--- itsyscape/ItsyScape/Application.lua
+++ itsyscape/ItsyScape/Application.lua
@@ -14,6 +14,7 @@ local Ray = require "ItsyScape.Common.Math.Ray"
 local Probe = require "ItsyScape.Game.Probe"
 local GameDB = require "ItsyScape.GameDB.GameDB"
 local LocalGame = require "ItsyScape.Game.LocalModel.Game"
+local ChannelRPCService = require "ItsyScape.Game.RPC.ChannelRPCService"
 local LocalGameManager = require "ItsyScape.Game.LocalModel.LocalGameManager"
 local RemoteGameManager = require "ItsyScape.Game.RemoteModel.RemoteGameManager"
 local Color = require "ItsyScape.Graphics.Color"
@@ -97,7 +98,8 @@ function Application:new(multiThreaded)
 	if not self.multiThreaded then
 		self.localGame = LocalGame(self.gameDB)
 	else
-		self.remoteGameManager = RemoteGameManager(self.outputChannel, self.inputChannel, self.gameDB)
+		local rpcService = ChannelRPCService(self.outputChannel, self.inputChannel)
+		self.remoteGameManager = RemoteGameManager(rpcService, self.gameDB)
 		self.gameThread = love.thread.newThread("ItsyScape/Game/LocalModel/Threads/Game.lua")
 		self.gameThread:start({
 			_DEBUG = _DEBUG
---
+++ itsyscape/ItsyScape/Game/RPC/RPCService.lua
@@ -0,0 +1,26 @@
+--------------------------------------------------------------------------------
+-- ItsyScape/Game/RPC/RPCService.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
+
+local RPCService = Class()
+
+function RPCService:new()
+	-- Nothing.
+end
+
+function RPCService:send(channel, e)
+	Class.ABSTRACT()
+end
+
+function RPCService:receive()
+	return Class.ABSTRACT()
+end
+
+return RPCService
