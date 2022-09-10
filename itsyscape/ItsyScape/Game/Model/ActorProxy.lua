607ce19ab97b9391a487c0cac3798bcfc876e1b7
Erin Maus <squeak@erinmaus.com>
2022-09-10 11:30:00 -0400

    Handle default values better.

--- itsyscape/ItsyScape/Game/Model/ActorProxy.lua
+++ itsyscape/ItsyScape/Game/Model/ActorProxy.lua
@@ -7,6 +7,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --------------------------------------------------------------------------------
+local Vector = require "ItsyScape.Common.Math.Vector"
 local Proxy = require "ItsyScape.Game.RPC.Proxy"
 local Event = require "ItsyScape.Game.RPC.Event"
 local Property = require "ItsyScape.Game.RPC.Property"
@@ -39,11 +40,11 @@ ActorProxy.unsetSkin:link(
 	Event.Argument("priority"),
 	Event.Argument("skin"))
 
-ActorProxy.getID = Property()
-ActorProxy.getPeepID = Property()
-ActorProxy.getName = Property()
-ActorProxy.getDescription = Property()
-ActorProxy.getDirection = Property()
+ActorProxy.getID = Property(-1)
+ActorProxy.getPeepID = Property("null")
+ActorProxy.getName = Property("Null")
+ActorProxy.getDescription = Property("This actor hasn't fully loaded yet.")
+ActorProxy.getDirection = Property(Vector.UNIT_X)
 
 ActorProxy.teleport = Event.ServerToClientRPC(Event.Argument("position"))
 ActorProxy.teleport:link("onTeleport", Event.Argument("position"))
@@ -54,14 +55,14 @@ ActorProxy.direction = Event.ServerToClientRPC(
 	Event.Argument("rotation"))
 ActorProxy.direction:link("onDirectionChanged", Event.Argument("direction"), Event.Argument("rotation"))
 
-ActorProxy.getPosition = Property()
-ActorProxy.getScale = Property()
-ActorProxy.getCurrentHitpoints = Property()
-ActorProxy.getMaximumHitpoints = Property()
-ActorProxy.getTile = Property()
-ActorProxy.getBounds = Property()
-ActorProxy.getActions = Property.Actions()
-ActorProxy.getBody = Property()
+ActorProxy.getPosition = Property(Vector.ZERO)
+ActorProxy.getScale = Property(Vector.ONE)
+ActorProxy.getCurrentHitpoints = Property(1)
+ActorProxy.getMaximumHitpoints = Property(1)
+ActorProxy.getTile = Property(0, 0, 0)
+ActorProxy.getBounds = Property(Vector.ZERO, Vector.ZERO)
+ActorProxy.getActions = Property.Actions({})
+ActorProxy.getBody = Property(false)
 
 ActorProxy.ANIMATIONS = "animations"
 ActorProxy.playAnimation = Event.Set(
