ff1df458b5c0b405fc55f74a9bc86e80fcc7efa6
Erin Maus <squeak@erinmaus.com>
2022-10-01 16:38:44 -0400

    Init NBunny.

--- itsyscape/ItsyScape/Graphics/Threads/Resource.lua
+++ itsyscape/ItsyScape/Graphics/Threads/Resource.lua
@@ -7,6 +7,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --------------------------------------------------------------------------------
+require "bootstrap"
 require "love.timer"
 
 local input = love.thread.getChannel('ItsyScape.Resource.File::input')
