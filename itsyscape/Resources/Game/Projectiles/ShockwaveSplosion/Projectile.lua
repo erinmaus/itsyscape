204ce45af6fdf1e042cbc1285ce90ec1e917b9d0
Erin Maus <squeak@erinmaus.com>
2023-03-02 10:09:44 -0500

    Add Judgment to Theodyssius's arsenal.

--- itsyscape/Resources/Game/Projectiles/ShockwaveSplosion/Projectile.lua
+++ itsyscape/Resources/Game/Projectiles/ShockwaveSplosion/Projectile.lua
@@ -101,7 +101,6 @@ function Splosion:update(elapsed)
 	if self.spawnPosition then
 		local root = self:getRoot()
 		root:getTransform():setLocalTranslation(self.spawnPosition)
-		root:getTransform():setPreviousTransform(self.spawnPosition)
 	end
 end
 
