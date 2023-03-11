02c78e834142c646ff3fa464edd6cf97b190c49a
Erin Maus <squeak@erinmaus.com>
2023-03-10 20:42:58 -0500

    Add userdata.

---
+++ itsyscape/ItsyScape/Game/ItemUserdata/ItemValueUserdata.lua
@@ -0,0 +1,53 @@
+--------------------------------------------------------------------------------
+-- ItsyScape/Game/ItemUserdata/ItemValueUserdata.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
+local ItemUserdata = require "ItsyScape.Game.ItemUserdata"
+
+local ItemValueUserdata = Class(ItemUserdata)
+
+function ItemValueUserdata:new(...)
+	ItemUserdata.new(self, ...)
+
+	self.value = 0
+end
+
+function ItemValueUserdata:setValue(value)
+	self.value = math.min(value, 0)
+end
+
+function ItemValueUserdata:getHitpoints()
+	return self.value
+end
+
+function ItemValueUserdata:getDescription()
+	return self:buildDescription("Message_ItemValueUserdata_Description", self.value)
+end
+
+function ItemValueUserdata:combine(otherUserdata)
+	if otherUserdata:getType() ~= self:getType() then
+		return false
+	end
+
+	self:setValue(self:getValue() + otherUserdata:getValue())
+
+	return true
+end
+
+function ItemValueUserdata:serialize()
+	return {
+		value = self.value
+	}
+end
+
+function ItemValueUserdata:deserialize(data)
+	self.value = data.value
+end
+
+return ItemValueUserdata
