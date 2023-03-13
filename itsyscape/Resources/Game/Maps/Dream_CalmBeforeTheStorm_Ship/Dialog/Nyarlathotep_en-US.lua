3dcceb2b8d66f89cc8f8cdef53699044f26947c1
Erin Maus <squeak@erinmaus.com>
2023-03-13 18:36:58 -0400

    Tweak dialogue in second dream.

--- itsyscape/Resources/Game/Maps/Dream_CalmBeforeTheStorm_Ship/Dialog/Nyarlathotep_en-US.lua
+++ itsyscape/Resources/Game/Maps/Dream_CalmBeforeTheStorm_Ship/Dialog/Nyarlathotep_en-US.lua
@@ -3,22 +3,37 @@ speaker "Nyarlathotep"
 PLAYER_NAME = _TARGET:getName()
 
 message "Nightmares... They're frightening things."
-message "Sometimes we dream them. Sometimes we live them."
+message {
+	"Sometimes we dream them.",
+	"Sometimes we live them."
+}
 
 if _TARGET:getState():has("KeyItem", "CalmBeforeTheStorm_KursedByCthulhu") then
 	message {
-		"Seems to be worse for you, though, huh? You didn't just drown, there's more than that: a horror, a kurse, wrought into your soul.",
-		"Maybe you should've listened to %person{Hans} when he told you to enter the portal."
+		"Seems to be worse for you, though, right?",
+		"You didn't just drown!",
+		"No, there's more than that..."
 	}
 
-	message "That's right, %person{${PLAYER_NAME}}. I know everything."
+	message "A horror, a kurse, wrought into your very soul!"
+
+	message {
+		"Maybe you should've listened to %person{Hans}",
+		"when he told you to enter the portal."
+	}
+
+	message {
+		"That's right, %person{${PLAYER_NAME}}.",
+		"I know everything."
+	}
 
 	message "Watch yourself."
 end
 
 message "%empty{The Empty King} wants me to keep an eye on you."
 message {
-	"You're special. You can't die. That's unique. Only few can claim such a gift.",
+	"You're special. You can't die. That's unique.",
+	"Only few can claim such a gift.",
 	"But that gift comes with a price."
 }
 
