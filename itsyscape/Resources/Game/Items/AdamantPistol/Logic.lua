60a8b553dae5d28643d23c9f55c1e15dd583a875
Erin Maus <squeak@erinmaus.com>
2023-03-22 13:57:41 -0400

    Add adamant guns.

---
+++ itsyscape/Resources/Game/Items/AdamantPistol/Logic.lua
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- Resources/Game/Items/AdamantPistol/Logic.lua
+--
+-- This file is a part of ItsyScape.
+--
+-- This Source Code Form is subject to the terms of the Mozilla Public
+-- License, v. 2.0. If a copy of the MPL was not distributed with this
+-- file, You can obtain one at http://mozilla.org/MPL/2.0/.
+--------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
+local Pistol = require "Resources.Game.Items.Common.Pistol"
+
+local AdamantPistol = Class(Pistol)
+
+return AdamantPistol
