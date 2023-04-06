5dba3b1e5965ba6378230bf570c68544e1764bd2
Erin Maus <squeak@erinmaus.com>
2023-04-06 09:24:47 -0400

    Update earth strike.

--- itsyscape/Resources/Game/Projectiles/EarthStrike/Projectile.lua
+++ itsyscape/Resources/Game/Projectiles/EarthStrike/Projectile.lua
@@ -15,21 +15,75 @@ local CacheRef = require "ItsyScape.Game.CacheRef"
 local Actor = require "ItsyScape.Game.Model.Actor"
 local Projectile = require "ItsyScape.Graphics.Projectile"
 local Color = require "ItsyScape.Graphics.Color"
-local QuadSceneNode = require "ItsyScape.Graphics.QuadSceneNode"
+local ParticleSceneNode = require "ItsyScape.Graphics.ParticleSceneNode"
 local PointLightSceneNode = require "ItsyScape.Graphics.PointLightSceneNode"
 local TextureResource = require "ItsyScape.Graphics.TextureResource"
+local QuadSceneNode = require "ItsyScape.Graphics.QuadSceneNode"
 
 local EarthStrike = Class(Projectile)
 EarthStrike.SPEED = 5
+
+EarthStrike.PARTICLE_SYSTEM = {
+	numParticles = 40,
+	texture = "Resources/Game/Projectiles/EarthStrike/Particle.png",
+	columns = 1,
+
+	emitters = {
+		{
+			type = "RadialEmitter",
+			radius = { 0.25 }
+		},
+		{
+			type = "DirectionalEmitter",
+			direction = { 0, 0, -1 },
+			speed = { 2.5, 3.5 },
+		},
+		{
+			type = "RandomColorEmitter",
+			colors = {
+				{ Color(0.67, 0.78, 0.21):get() },
+				{ Color(0.67, 0.78, 0.21):get() },
+				{ Color(0.67, 0.78, 0.21):get() },
+				{ Color(0.67, 0.78, 0.21):get() }
+			}
+		},
+		{
+			type = "RandomLifetimeEmitter",
+			lifetime = { 1.0, 1.5 }
+		},
+		{
+			type = "RandomScaleEmitter",
+			scale = { 0.3, 0.4 }
+		},
+		{
+			type = "RandomRotationEmitter",
+			rotation = { 0, 360 },
+			velocity = { 30, 60 },
+			acceleration = { -40, -20 }
+		}
+	},
+
+	paths = {
+		{
+			type = "FadeInOutPath",
+			fadeInPercent = { 0.1 },
+			fadeOutPercent = { 0.8 },
+			tween = { 'sineEaseOut' }
+		}
+	},
+
+	emissionStrategy = {
+		type = "RandomDelayEmissionStrategy",
+		count = { 4, 6 },
+		delay = { 0.125 },
+		duration = { math.huge }
+	}
+}
 
 function EarthStrike:attach()
 	Projectile.attach(self)
 
 	self.duration = math.huge
-end
-
-function EarthStrike:getTextureFilename()
-	return Class.ABSTRACT()
 end
 
 function EarthStrike:load()
@@ -41,12 +95,15 @@ function EarthStrike:load()
 	self.quad = QuadSceneNode()
 	self.quad:setParent(root)
 	self.quad:setIsBillboarded(false)
+	self.quad:getTransform():setLocalScale(Vector(0.5))
+
+	self.particleSystem = ParticleSceneNode()
+	self.particleSystem:setParent(root)
+	self.particleSystem:initParticleSystemFromDef(EarthStrike.PARTICLE_SYSTEM, resources)
 
 	self.light = PointLightSceneNode()
-	self.light:setParent(root)
+	self.light:setParent(self.quad)
 	self.light:setColor(Color(0.67, 0.78, 0.21))
-
-	self.quad:getTransform():setLocalScale(Vector(0.5, 1.0, 0.5))
 
 	resources:queue(
 		TextureResource,
@@ -72,8 +129,7 @@ end
 function EarthStrike:update(elapsed)
 	Projectile.update(self, elapsed)
 
-	if self.spawnPosition then
-		local hitPosition = self:getTargetPosition(self:getDestination()) + Vector(0, 1, 0)
+	if self.spawnPosition and self.hitPosition then
 		local root = self:getRoot()
 		local delta = self:getDelta()
 		local mu = Tween.sineEaseOut(delta)
@@ -85,13 +141,19 @@ function EarthStrike:update(elapsed)
 		end
 
 		local xRotation = Quaternion.fromAxisAngle(Vector.UNIT_X, -math.pi / 2)
-		local lookRotation = Quaternion.lookAt(self.spawnPosition, hitPosition)
+		local lookRotation = Quaternion.lookAt(self.spawnPosition, self.hitPosition)
 		local rotation = lookRotation * xRotation
 
-		root:getTransform():setLocalTranslation(position)
-		root:getTransform():setLocalRotation(rotation)
+		self.quad:getTransform():setLocalTranslation(position)
+		self.quad:getTransform():setLocalRotation(rotation)
 
 		self.quad:getMaterial():setColor(Color(1, 1, 1, alpha))
+		self.particleSystem:getMaterial():setColor(Color(1, 1, 1, alpha))
+
+		local particleSystem = self.particleSystem:getParticleSystem()
+		if particleSystem then
+			particleSystem:updateEmittersLocalPosition(position)
+		end
 
 		self.light:setAttenuation((1 - alpha) * 3 + 2)
 	end
