52f2dd9d993024019fefd4c41cf7d0256e57859d
Erin Maus <squeak@erinmaus.com>
2023-04-06 09:13:03 -0400

    Update air strike.

--- itsyscape/Resources/Game/Projectiles/AirStrike/Projectile.lua
+++ itsyscape/Resources/Game/Projectiles/AirStrike/Projectile.lua
@@ -8,29 +8,22 @@
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --------------------------------------------------------------------------------
 local Class = require "ItsyScape.Common.Class"
-local Tween = require "ItsyScape.Common.Math.Tween"
 local Vector = require "ItsyScape.Common.Math.Vector"
 local Quaternion = require "ItsyScape.Common.Math.Quaternion"
 local CacheRef = require "ItsyScape.Game.CacheRef"
 local Actor = require "ItsyScape.Game.Model.Actor"
-local Projectile = require "ItsyScape.Graphics.Projectile"
 local Color = require "ItsyScape.Graphics.Color"
-local QuadSceneNode = require "ItsyScape.Graphics.QuadSceneNode"
-local PointLightSceneNode = require "ItsyScape.Graphics.PointLightSceneNode"
-local TextureResource = require "ItsyScape.Graphics.TextureResource"
+local Projectile = require "ItsyScape.Graphics.Projectile"
+local LightBeamSceneNode = require "ItsyScape.Graphics.LightBeamSceneNode"
 
 local AirStrike = Class(Projectile)
-AirStrike.SPEED = 5
 
-function AirStrike:attach()
-	Projectile.attach(self)
+AirStrike.SEGMENT_LENGTH  = 1 / 25
+AirStrike.RADIUS = 0.5
 
-	self.duration = math.huge
-end
+AirStrike.SPEED = 8
 
-function AirStrike:getTextureFilename()
-	return Class.ABSTRACT()
-end
+AirStrike.ALPHA_MULTIPLIER = 1.75
 
 function AirStrike:load()
 	Projectile.load(self)
@@ -38,31 +31,62 @@ function AirStrike:load()
 	local resources = self:getResources()
 	local root = self:getRoot()
 
-	self.quad = QuadSceneNode()
-	self.quad:setParent(root)
-	self.quad:setIsBillboarded(false)
+	self.airWave = LightBeamSceneNode()
+	self.airWave:setParent(root)
+	self.airWave:setBeamSize(0.25)
+	self.airWave:getMaterial():setIsFullLit(true)
+	self.airWave:getMaterial():setColor(Color(1))
 
-	self.light = PointLightSceneNode()
-	self.light:setParent(root)
+	self.fullPath = {}
+	self.currentPath = {}
+	self.previousNumSegments = 1
+end
 
-	self.quad:getTransform():setLocalScale(Vector(0.5, 0.5, 0.5))
+function AirStrike:generatePath(spawn, hit)
+	local crowDistance = (spawn - hit):getLength()
+	local totalDistance = 2 * math.pi * AirStrike.RADIUS * (crowDistance / AirStrike.RADIUS)
 
-	resources:queue(
-		TextureResource,
-		"Resources/Game/Projectiles/AirStrike/Texture.png",
-		function(texture)
-			self.quad:getMaterial():setTextures(texture)
-		end)
+	local currentDistance = 0
+	while currentDistance < totalDistance do
+		local delta = currentDistance / crowDistance
+
+		local position = Vector(
+			math.cos(delta * math.pi * 2) * (AirStrike.RADIUS),
+			math.sin(delta * math.pi * 2) * (AirStrike.RADIUS),
+			-currentDistance / totalDistance * crowDistance)
+		table.insert(self.fullPath, position)
+
+		currentDistance = currentDistance + AirStrike.SEGMENT_LENGTH
+	end
+end
+
+function AirStrike:updatePath()
+	local numSegments = math.min(math.floor(#self.fullPath * self:getDelta()) + 1, #self.fullPath)
+
+	for i = self.previousNumSegments + 1, numSegments do
+		local a = self.fullPath[i - 1]
+		local b = self.fullPath[i]
+
+		table.insert(self.currentPath, {
+			a = { a:get() },
+			b = { b:get() }
+		})
+		self.previousNumSegments = self.previousNumSegments + 1
+	end
+
+	self.airWave:buildSeamless(self.currentPath)
 end
 
 function AirStrike:getDuration()
-	return self.duration
+	return self.duration or math.huge
 end
 
 function AirStrike:tick()
-	if not self.spawnPosition or not self.hitPosition then
-		self.spawnPosition = self:getTargetPosition(self:getSource()) + Vector(0, 1, 0)
-		self.hitPosition = self:getTargetPosition(self:getDestination()) + Vector(0, 1, 0)
+	if not self.spawnPosition then
+		self.hitPosition = self:getTargetPosition(self:getDestination())
+		self.spawnPosition = self:getTargetPosition(self:getSource())
+
+		self:generatePath(self.spawnPosition, self.hitPosition)
 
 		self.duration = math.max((self.spawnPosition - self.hitPosition):getLength() / self.SPEED, 0.5)
 	end
@@ -71,27 +95,16 @@ end
 function AirStrike:update(elapsed)
 	Projectile.update(self, elapsed)
 
-	if self.spawnPosition then
-		local root = self:getRoot()
+	if self.spawnPosition and self.hitPosition then
+		self:updatePath()
+
 		local delta = self:getDelta()
-		local mu = Tween.sineEaseOut(delta)
-		local position = self.spawnPosition:lerp(self.hitPosition, mu)
+		local alpha = math.abs(math.sin(delta * math.pi)) * AirStrike.ALPHA_MULTIPLIER
+		alpha = math.min(alpha, 1)
 
-		local alpha = 1
-		if delta > 0.5 then
-			alpha = 1 - (delta - 0.5) / 0.5
-		end
-
-		local xRotation = Quaternion.fromAxisAngle(Vector.UNIT_X, math.pi / 4)
-		local zRotation = Quaternion.fromAxisAngle(Vector.UNIT_Z, self:getTime() * math.pi * 4)
-		local rotation = xRotation * zRotation
-
-		root:getTransform():setLocalTranslation(position)
-		root:getTransform():setLocalRotation(rotation)
-
-		self.quad:getMaterial():setColor(Color(1, 1, 1, alpha))
-
-		self.light:setAttenuation(alpha * 2 + 2)
+		self.airWave:getTransform():setLocalRotation(Quaternion.lookAt(self.spawnPosition, self.hitPosition))
+		self.airWave:getTransform():setLocalTranslation(self.spawnPosition)
+		self.airWave:getMaterial():setColor(Color(1, 1, 1, alpha))
 	end
 end
 
