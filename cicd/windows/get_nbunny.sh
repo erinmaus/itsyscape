ad450edbcefb1f8dcbb1c17a132900c16908682f
Erin Maus <squeak@erinmaus.com>
2023-04-10 13:02:45 -0400

    Build Windows binary.

---
+++ cicd/windows/get_nbunny.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -xe
+
+cp -r ../../utilities ./build/nbunny
+
+cd build/nbunny
+
+sed -i 's/platforms { "x86", "x64", "ARM64" }//g' premake5.lua
+
+../../premake5 --DEPS="$(pwd)/.." gmake
+
+CPPFLAGS="-I$(pwd)/../megasource/libs/love/src/modules -I$(pwd)/../megasource/libs/love/src/modules -I$(pwd)/../megasource/libs/love/src" LDFLAGS="-L$(pwd)/../megasource-build/love -llove" make config=release nbunny
+
+cp bin/nbunny.dll ../../staging/ext/nbunny.dll
