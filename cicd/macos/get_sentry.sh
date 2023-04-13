87c4d16664b37c4f66a6e1d44370dc8de9294d76
Erin Maus <squeak@erinmaus.com>
2023-04-13 14:20:25 -0400

    Fix not including Sentry on macOS.

--- cicd/macos/get_sentry.sh
+++ cicd/macos/get_sentry.sh
@@ -9,3 +9,5 @@ unzip -qo 0.6.1.zip
 cmake -Bsentry-native-0.6.1-build -Hsentry-native-0.6.1 -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64" -DSENTRY_BACKEND=inproc -DCMAKE_BUILD_TYPE=Release
 cmake --build sentry-native-0.6.1-build --parallel
 cmake --install sentry-native-0.6.1-build --prefix .
+
+cp ./lib/libsentry.dylib ../staging/ext
