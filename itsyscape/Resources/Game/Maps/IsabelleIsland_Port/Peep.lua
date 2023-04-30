664855b8179821a90fa1f96a40fe8941e7a40ff8
Erin Maus <squeak@erinmaus.com>
2023-04-30 08:45:55 -0400

    Remove sailor that takes you to Rumbridge.

--- itsyscape/Resources/Game/Maps/IsabelleIsland_Port/Peep.lua
+++ itsyscape/Resources/Game/Maps/IsabelleIsland_Port/Peep.lua
@@ -15,42 +15,6 @@ local Port = Class(Map)
 
 function Port:new(resource, name, ...)
 	Map.new(self, resource, name or 'IsabelleIsland_Port', ...)
-
-	self.spawnedSailor = false
-end
-
-function Port:spawnSailor()
-	local actor = Utility.spawnMapObjectAtAnchor(
-		self,
-		"Sailor",
-		"Anchor_Sailor",
-		0)
-	local peep = actor:getPeep()
-	local tripAnimation = peep:getResource("animation-resurrect", "ItsyScape.Graphics.AnimationResource")
-	if tripAnimation then
-		actor:playAnimation('x-fall', math.huge, tripAnimation)
-	end
-end
-
-function Port:update(...)
-	Map.update(self, ...)
-
-	local player = Utility.Peep.getPlayer(self)
-	if not player then
-		return
-	end
-
-	local playerState = player:getState()
-	local talkedToJenkins = playerState:has("KeyItem", "CalmBeforeTheStorm_TalkedToJenkins")
-	local hasCompletedQuest = playerState:has("Quest", "CalmBeforeTheStorm")
-
-	local shouldSpawnSailor = talkedToJenkins and not hasCompletedQuest
-	shouldSpawnSailor = _DEBUG or shouldSpawnSailor
-
-	if shouldSpawnSailor and not self.spawnedSailor then
-		self:spawnSailor()
-		self.spawnedSailor = true
-	end
 end
 
 return Port
