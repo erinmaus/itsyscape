f0b23462ee171b5662249f2d66a65a8452a4429a
Erin Maus <squeak@erinmaus.com>
2023-05-09 15:05:18 -0400

    Add spider death animation.

---
+++ itsyscape/Resources/Game/Animations/Spider_Die/Script.lua
@@ -0,0 +1,9 @@
+Animation "Spider Die" {
+	Target {
+		PlayAnimation "Resources/Game/Animations/Spider_Die/Die.lanim",
+
+		PlayAnimation "Resources/Game/Animations/Spider_Die/Idle.lanim" {
+			repeatAnimation = true
+		}
+	}
+}
