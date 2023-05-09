c441b39f0d06266c546e14e3ef9587af63e4b81a
Erin Maus <squeak@erinmaus.com>
2023-05-09 15:30:32 -0400

    Add melee attack animation.

---
+++ itsyscape/Resources/Game/Animations/Spider_Attack_Melee/Script.lua
@@ -0,0 +1,14 @@
+Animation "Spider Attack" {
+	Target {
+		PlayAnimation "Resources/Game/Animations/Spider_Attack_Melee/Animation.lanim" {
+			bones = {
+				"legA1.R",
+				"legA2.R",
+				"legA3.R",
+				"legA1.L",
+				"legA2.L",
+				"legA3.L",
+			}
+		}
+	}
+}
