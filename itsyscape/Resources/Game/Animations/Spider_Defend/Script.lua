62f22c02fef084ca27d398f51c9927993a011dd8
Erin Maus <squeak@erinmaus.com>
2023-05-27 10:52:03 -0400

    Update sounds for spiders and sewers.

--- itsyscape/Resources/Game/Animations/Spider_Defend/Script.lua
+++ itsyscape/Resources/Game/Animations/Spider_Defend/Script.lua
@@ -1,5 +1,6 @@
 Animation "Spider Defend" {
 	Target {
+		PlaySound "Resources/Game/Animations/Spider_Defend/Sound.wav",
 		PlayAnimation "Resources/Game/Animations/Spider_Defend/Animation.lanim"
 	}
 }
