ec3a093b3a0bc5a8da287351cf1e11d23e0e23cc
Erin Maus <squeak@erinmaus.com>
2023-06-16 10:26:32 -0400

    Add animations to ancient karadon.

---
+++ itsyscape/Resources/Game/Animations/AncientKaradon_Die/Script.lua
@@ -0,0 +1,8 @@
+Animation "Ancient Karadon Die" {
+	Target {
+		PlayAnimation "Resources/Game/Animations/AncientKaradon_Die/Animation.lanim" {
+			keep = true,
+			repeatAnimation = true
+		}
+	}
+}
