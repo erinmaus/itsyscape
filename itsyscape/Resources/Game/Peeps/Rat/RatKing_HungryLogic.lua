e9c26e5bc6aa83ac93f8150cdb22cf2715c0a18d
Erin Maus <squeak@erinmaus.com>
2023-06-25 10:25:44 -0400

    More work on Rat King fight.

--- itsyscape/Resources/Game/Peeps/Rat/RatKing_HungryLogic.lua
+++ itsyscape/Resources/Game/Peeps/Rat/RatKing_HungryLogic.lua
@@ -14,106 +14,140 @@ local Mashina = require "ItsyScape.Mashina"
 local Probe = require "ItsyScape.Peep.Probe"
 
 local TARGET = B.Reference("RatKing_HungryLogic", "TARGET")
-local ATTACK_POKE = B.Reference("RatKing_HungryLogic", "ATTACK_POKE")
-local ATTACK_POKE_DAMAGE = B.Reference("RatKing_HungryLogic", "ATTACK_POKE_DAMAGE")
 
-local function ratRobe(p)
+local function ratProbe(p)
+	local gameDB = p:getDirector():getGameDB()
+
 	local resource = Utility.Peep.getResource(p)
 	if not resource then
 		return false
 	end
 
-	local tag = self:getDirector():getGameDB():getRecord({
+	local tag = gameDB:getRecord("ResourceTag", {
 		Value = "Rat",
 		Resource = resource
 	})
 
-	return tag ~= nil
+	if not tag then
+		return false
+	end
+
+	local mapObject = Utility.Peep.getMapObject(p)
+	if not mapObject then
+		return false
+	end
+
+	local isInGroup = gameDB:getRecord("MapObjectGroup", {
+		MapObjectGroup = "RatKing_Court",
+		Map = Utility.Peep.getMapResource(p),
+		MapObject = mapObject
+	})
+
+	if not isInGroup then
+		return false
+	end
+
+	local status = p:getBehavior("CombatStatus")
+	return status and not status.dead and status.currentHitpoints > 0
 end
 
-local Tree = BTreeBuilder.Node() {
-	Mashina.Step {
-		Mashina.Success {
-			Mashina.Step {
-				Mashina.Repeat {
-					Mashina.Try {
-						Mashina.Peep.FindNearbyCombatTarget {
-							filter = ratProbe,
-							distance = 10,
-							include_npcs = true,
-							[TARGET] = B.Output.result
-						},
-
-						-- This one should eventually fail to fall back to
-						-- the first try sequence above.
-						Mashina.Failure {
-							Mashina.Step {
-								Mashina.Peep.PokeSelf {
-									event = "summon"
-								},
-
-								Mashina.Peep.Talk {
-									message = "You think you've outsmarted me by slaying my court...?"
-								},
-
-								Mashina.Peep.Timeout {
-									duration = 1
-								}
-							}
-						}
-					}
-				},
-
-				Mashina.Repeat {
-					Mashina.Try {
-						Mashina.Sequence {
-							Mashina.Peep.IsCombatTarget {
-								target = TARGET
-							},
-
-							Mashina.Peep.EngageCombatTarget {
-								peep = TARGET
-							}
-						},
-
-						Mashina.Sequence {
-							Mashina.Peep.IsCombatTarget {
-								target = TARGET
-							},
-
-							Mashina.Invert {
-								Mashina.Peep.IsDead
-							}
-						}
-					}
-				},
-
-				Mashina.Peep.Talk {
-					message = "Yum!"
-				},
-
-				Mashina.Peep.PokeSelf {
-					event = "eat",
-					poke = function(_, state)
-						return {
-							target = state[TARGET]
-						}
-					end
-				}
-			}
-		},
-
+local SummonOrTargetSequence = Mashina.ParallelTry {
+	Mashina.Sequence {
 		Mashina.Peep.FindNearbyCombatTarget {
+			filter = ratProbe,
+			distance = 12,
+			include_npcs = true,
 			[TARGET] = B.Output.result
 		},
 
-		Mashina.Peep.EngageCombatTarget {
-			peep = TARGET
+		Mashina.Peep.Talk {
+			message = "ENGAGED"
+		}
+	},
+
+	Mashina.Step {
+		Mashina.Peep.PokeSelf {
+			event = "summon"
 		},
 
-		Mashina.Peep.SetState {
-			state = "attack"
+		Mashina.Peep.Talk {
+			message = "You think you've outsmarted me by slaying my court...?"
+		},
+
+		Mashina.Peep.Timeout {
+			duration = 1
+		},
+
+		Mashina.Peep.FindNearbyCombatTarget {
+			filter = ratProbe,
+			distance = 12,
+			include_npcs = true,
+			[TARGET] = B.Output.result
 		}
+	}
+}
+
+local FightSequence = Mashina.Repeat {
+	Mashina.Try {
+		Mashina.Sequence {
+			Mashina.Invert {
+				Mashina.Peep.IsCombatTarget {
+					target = TARGET
+				}
+			},
+
+			Mashina.Peep.EngageCombatTarget {
+				peep = TARGET
+			}
+		},
+
+		Mashina.Sequence {
+			Mashina.Peep.IsCombatTarget {
+				target = TARGET
+			},
+
+			Mashina.Invert {
+				Mashina.Peep.IsDead
+			}
+		}
+	}
+}
+
+local EatSequence = Mashina.Sequence {
+	Mashina.Peep.Talk {
+		message = "Yum!"
+	},
+
+	Mashina.Peep.PokeSelf {
+		event = "eat",
+		poke = function(_, state)
+			return {
+				target = state[TARGET]
+			}
+		end
+	}
+}
+
+local AttackPlayerSequence = Mashina.Sequence {
+	Mashina.Peep.FindNearbyCombatTarget {
+		[TARGET] = B.Output.result
+	},
+
+	Mashina.Peep.EngageCombatTarget {
+		peep = TARGET
+	},
+
+	Mashina.Peep.SetState {
+		state = "attack"
+	}
+}
+
+local Tree = BTreeBuilder.Node() {
+	Mashina.Step {
+		SummonOrTargetSequence,
+		FightSequence,
+		EatSequence,
+		AttackPlayerSequence,
 	}
 }
 
