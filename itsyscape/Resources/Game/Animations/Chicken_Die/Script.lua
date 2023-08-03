f006901fffa8f5f8411f32595be5861681016aed
Erin Maus <squeak@erinmaus.com>
2023-08-03 09:13:05 -0400

    Add death sound to chicken.

--- itsyscape/Resources/Game/Animations/Chicken_Die/Script.lua
+++ itsyscape/Resources/Game/Animations/Chicken_Die/Script.lua
@@ -1,5 +1,6 @@
 Animation "Chicken Death" {
 	Target {
+		PlaySound "Resources/Game/Animations/Chicken_Die/Sound.wav",
 		PlayAnimation "Resources/Game/Animations/Chicken_Die/Animation.lanim" {
 			repeatAnimation = true,
 			keep = true
