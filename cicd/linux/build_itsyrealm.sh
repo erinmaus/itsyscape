18dd59e95ff3b21d355347a2dabfba7614d3d774
Erin Maus <squeak@erinmaus.com>
2023-08-23 18:01:04 -0400

    Fix LOVE file gen.

--- cicd/linux/build_itsyrealm.sh
+++ cicd/linux/build_itsyrealm.sh
@@ -2,11 +2,14 @@
 
 set -xe
 
-cd itsyscape && git pull && cd itsyscape
-cp -r ../../bmashina/lmashina/lua/B ./B
+rm -rf itsyscape-love
+cp -r ../../itsyscape itsyscape-love
+cd itsyscape-love
+
+cp -r ../bmashina/lmashina/lua/B ./B
 
 echo $ITSYREALM_VERSION > version.meta
-zip -9 -qr ../../itsyrealm.love .
+zip -9 -qr ../itsyrealm.love .
 
-cd ../..
+cd ..
 cp ./itsyrealm.love ./installdir/bin/itsyrealm.love
