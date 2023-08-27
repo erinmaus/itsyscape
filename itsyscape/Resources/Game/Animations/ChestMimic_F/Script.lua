87265094f062f80d3d7215779cd837eb69190eff
Erin Maus <squeak@erinmaus.com>
2023-08-27 18:53:16 -0400

    Add chest mimic support.

---
+++ itsyscape/Resources/Game/Animations/ChestMimic_F/Script.lua
@@ -0,0 +1,14 @@
+Animation "Chest Mimic Talk (F)" {
+	Target {
+		PlayAnimation "Resources/Game/Animations/ChestMimic_F/Animation.lanim" {
+			repeatAnimation = true,
+			keep = true,
+			bones = {
+				"tongue.b",
+				"tongue.m",
+				"tongue.f",
+				"head"
+			}
+		}
+	}
+}
