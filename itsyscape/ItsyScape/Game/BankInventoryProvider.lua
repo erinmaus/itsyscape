7e04b0c7ff88055e25d7f40f7d77d15bece3bd6b
Erin Maus <squeak@erinmaus.com>
2023-11-11 10:38:23 -0500

    [IR-165] Rebuild item z and item key from index in player storage.

--- itsyscape/ItsyScape/Game/BankInventoryProvider.lua
+++ itsyscape/ItsyScape/Game/BankInventoryProvider.lua
@@ -280,6 +280,8 @@ function BankInventoryProvider:load(...)
 	if storage then
 		for key, section in storage:iterateSections() do
 			local item = broker:itemFromStorage(self, section)
+
+			broker:setItemKey(item, key)
 			broker:setItemZ(item, broker:getItemKey(item))
 		end
 	end
