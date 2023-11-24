8c3387f9d0f0de1500ae1b7ed0be3ae841528243
Erin Maus <squeak@erinmaus.com>
2023-11-24 06:39:45 -0500

    Improve CICD publish step for Steam.

--- cicd/publish/upload_steam.sh
+++ cicd/publish/upload_steam.sh
@@ -7,4 +7,15 @@ brew install --cask steamcmd
 mkdir -p "$HOME/Library/Application Support/Steam/config"
 echo "${STEAM_CONFIG_VDF}" > "$HOME/Library/Application Support/Steam/config/config.vdf"
 
-steamcmd +login ${STEAM_CICD_USERNAME} +run_app_build $(pwd)/$1 +quit
+ITSYREALM_VDF="$(pwd)/$1"
+upload_steam () {
+	steamcmd +login ${STEAM_CICD_USERNAME} +run_app_build "$ITSYREALM_VDF" +quit
+}
+
+upload_steam
+while [ $? -ne 0 ]; do
+	echo "Failed to upload to Steam. Trying again in 5 seconds..."
+	sleep 5
+
+	upload_steam
+done
