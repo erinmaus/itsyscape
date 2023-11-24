5339ba3c4727ce9a5b35f3df7a2794414b5820b7
Erin Maus <squeak@erinmaus.com>
2023-11-23 20:21:28 -0500

    [IR-248] Nerf fury power.

--- itsyscape/Resources/Game/Effects/Power_Fury/Effect.lua
+++ itsyscape/Resources/Game/Effects/Power_Fury/Effect.lua
@@ -15,9 +15,8 @@ local CombatEffect = require "ItsyScape.Peep.Effects.CombatEffect"
 
 -- Prevents 50%-100% of damage from the next attack.
 local Fury = Class(CombatEffect)
-Fury.DURATION = math.huge
-Fury.MIN_FADE_DURATION = 10
-Fury.MAX_FADE_DURATION = 20
+Fury.MIN_FADE_DURATION = 20
+Fury.MAX_FADE_DURATION = 30
 Fury.MIN_DAMAGE_MULTIPLIER_STEP = 0.05
 Fury.MAX_DAMAGE_MULTIPLIER_STEP = 0.10
 Fury.MAX_DAMAGE_MULTIPLIER = 1
@@ -27,17 +26,40 @@ Fury.MAX_LEVEL = 60
 function Fury:new(activator)
 	CombatEffect.new(self)
 
-	local level = activator:getState():count(
+	self.currentDamageMultiplier = 0
+end
+
+function Fury:getDamageMultiplierStep()
+	local level = self:getPeep():getState():count(
 		"Skill",
 		"Defense",
 		{ ['skill-as-level'] = true })
 
-	local delta = (level - Fury.MIN_LEVEL) / (Fury.MAX_LEVEL - Fury.MIN_LEVEL)
-	delta = math.min(math.max(delta, 0), 1)
+	local step = Utility.Combat.calcBoost(
+		level,
+		Fury.MIN_LEVEL, Fury.MAX_LEVEL,
+		Fury.MIN_DAMAGE_MULTIPLIER_STEP, Fury.MAX_DAMAGE_MULTIPLIER_STEP)
+	return step
+end
 
-	self.fadeDuration = delta * (Fury.MAX_FADE_DURATION - Fury.MIN_FADE_DURATION) + Fury.MIN_FADE_DURATION
-	self.damageMultiplierStep = delta * (Fury.MAX_DAMAGE_MULTIPLIER_STEP - Fury.MIN_DAMAGE_MULTIPLIER_STEP) + Fury.MIN_DAMAGE_MULTIPLIER_STEP
-	self.currentDamageMultiplier = 0
+function Fury:boostDuration()
+	local level = self:getPeep():getState():count(
+		"Skill",
+		"Defense",
+		{ ['skill-as-level'] = true })
+
+	local duration = Utility.Combat.calcBoost(
+		level,
+		Fury.MIN_LEVEL, Fury.MAX_LEVEL,
+		Fury.MIN_FADE_DURATION, Fury.MAX_FADE_DURATION)
+	self.fadeDuration = math.min((self.fadeDuration or 0) + duration, duration)
+
+	Log.info("Fury duration for peep '%s' is now %.2f seconds.", self:getPeep():getName(), self.fadeDuration)
+end
+
+function Fury:enchant(peep)
+	CombatEffect.enchant(self, peep)
+	self:boostDuration()
 end
 
 function Fury:getDescription()
@@ -53,25 +75,27 @@ function Fury:applySelfToDamage(roll)
 	roll:setMinHit(math.floor(roll:getMinHit() * (1 + self.currentDamageMultiplier)))
 end
 
-function Fury:applyTargetToAttack(roll)
+function Fury:receiveDamage(roll)
 	local oldDamageMultiplier = self.currentDamageMultiplier
-	self.currentDamageMultiplier = math.min(self.currentDamageMultiplier + self.damageMultiplierStep, Fury.MAX_DAMAGE_MULTIPLIER)
+	self.currentDamageMultiplier = math.min(self.currentDamageMultiplier + self:getDamageMultiplierStep(), Fury.MAX_DAMAGE_MULTIPLIER)
 
 	if self.currentDamageMultiplier > oldDamageMultiplier then
-		Log.info("Damage increased to %d%%.", self.currentDamageMultiplier * 100)
+		Log.info("Damage for peep '%s' increased to %d%%.", self:getPeep():getName(), self.currentDamageMultiplier * 100)
 	end
+
+	self:boostDuration()
 end
 
 function Fury:update(delta)
-	CombatEffect.update(self, delta)
-
 	local equippedShield = Utility.Peep.getEquippedShield(self:getPeep(), true)
 	if not equippedShield then
-		self.fadeDuration = self.fadeDuration - delta
-		self:setDuration(self.fadeDuration)
+		self.fadeDuration = 0
 	else
-		self:setDuration(math.huge)
+		self.fadeDuration = (self.fadeDuration or 0) - delta
 	end
+	self:setDuration(self.fadeDuration)
+
+	CombatEffect.update(self, delta)
 end
 
 return Fury
