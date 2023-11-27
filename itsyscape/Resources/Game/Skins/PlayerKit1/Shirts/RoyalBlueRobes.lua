aaa69c4c60e1254460653377ed34264283ddb09a
Erin Maus <squeak@erinmaus.com>
2023-11-27 16:55:42 -0500

    [IR-36] More work on King.

--- itsyscape/Resources/Game/Skins/PlayerKit1/Shirts/RoyalBlueRobes.lua
+++ itsyscape/Resources/Game/Skins/PlayerKit1/Shirts/RoyalBlueRobes.lua
@@ -1,5 +1,4 @@
 {
 	model = "Resources/Game/Skins/Common/PlayerKit1/Dress.lmesh",
-	texture = "Resources/Game/Skins/PlayerKit1/Shirts/RoyalBlueRobes.png",
-	position = { -0.125, -0.06125, 0 },
+	texture = "Resources/Game/Skins/PlayerKit1/Shirts/RoyalBlueRobes.png"
 }
