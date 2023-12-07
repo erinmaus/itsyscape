fec5b209e05f88048037e96c093982a0546ca7cf
Erin Maus <squeak@erinmaus.com>
2023-12-07 18:30:19 -0500

    Add support for patch notes.

--- cicd/macos/zip_itsyrealm.sh
+++ cicd/macos/zip_itsyrealm.sh
@@ -6,5 +6,7 @@ cd ../../itsyscape
 echo $ITSYREALM_VERSION > version.meta
 
 cp -r ../cicd/macos/staging/ext/B ./B
+cp -r ../cicd/macos/staging/ext/devi ./devi
 zip -9 -qr ../cicd/macos/staging/ItsyRealm.app/Contents/Resources/itsyrealm.love .
 rm -r ./B
+rm -r ./devi
