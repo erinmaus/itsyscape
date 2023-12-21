0bcebaafc25ab1804b4f82ba557e9a648cab904b
Erin Maus <squeak@erinmaus.com>
2023-12-21 18:56:01 -0500

    Blend behemoth rise animation with idle animation.

--- itsyscape/Resources/Game/Animations/Behemoth_Idle/Script.lua
+++ itsyscape/Resources/Game/Animations/Behemoth_Idle/Script.lua
@@ -1,4 +1,9 @@
 Animation "Behemoth Idle" {
+	Blend {
+		from = "Behemoth Stun",
+		duration = 0.5
+	},
+
 	Target {
 		PlayAnimation "Resources/Game/Animations/Behemoth_Idle/Animation.lanim" {
 			repeatAnimation = true
