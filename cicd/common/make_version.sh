0a3c162627f12f5ae2ce6aebb960aee7daff3f8b
Erin Maus <squeak@erinmaus.com>
2024-01-05 19:52:00 -0500

    Allow overriding version number.

--- cicd/common/make_version.sh
+++ cicd/common/make_version.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-latest_version=$(git describe --tags | sed -n 's/\([0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*\).*$/\1/p')
-commit_version=$(git rev-parse --short=8 HEAD)
+if [ ! -z "$ITSYREALM_VERSION_OVERRIDE" ]; then
+    latest_version="$ITSYREALM_VERSION_OVERRIDE"
+else
+    latest_version=$(git describe --tags | sed -n 's/\([0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*\).*$/\1/p')
+fi
+
+commit_version=$(git rev-parse --short=8 $latest_version)
 build_version=$(git rev-list --count ${latest_version}..HEAD)
 
 if [ "$1" == "simple" ]; then
