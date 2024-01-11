1b4eb2029abbe54f237191420d5950ba36873a8c
Erin Maus <squeak@erinmaus.com>
2024-01-11 11:27:25 -0500

    Make explosive barrel rock.

--- itsyscape/Resources/Game/Peeps/EmptyRuins/ExplosiveBarrel.lua
+++ itsyscape/Resources/Game/Peeps/EmptyRuins/ExplosiveBarrel.lua
@@ -12,13 +12,13 @@ local Quaternion = require "ItsyScape.Common.Math.Quaternion"
 local Vector = require "ItsyScape.Common.Math.Vector"
 local Utility = require "ItsyScape.Game.Utility"
 local Probe = require "ItsyScape.Peep.Probe"
-local StaticProp = require "Resources.Game.Peeps.Props.StaticProp"
+local BlockingProp = require "Resources.Game.Peeps.Props.BlockingProp"
 
-local ExplosiveBarrel = Class(StaticProp)
+local ExplosiveBarrel = Class(BlockingProp)
 ExplosiveBarrel.ROCK_INTERVAL = 0.5
 
 function ExplosiveBarrel:ready(...)
-	StaticProp.ready(self, ...)
+	BlockingProp.ready(self, ...)
 	self:roll()
 end
 
@@ -26,29 +26,29 @@ function ExplosiveBarrel:rock(delta)
 	local start = self.startRotation or Quaternion.IDENTITY
 	local target = self.targetRotation or Quaternion.IDENTITY
 
-	Utility.Peep.setRotation(self, start:slerp(target, delta))
+	Utility.Peep.setRotation(self, start:slerp(target, delta):getNormal())
 end
 
 function ExplosiveBarrel:roll()
 	self.startRotation = Utility.Peep.getRotation(self)
 
-	local xAngle = math.rad(love.math.random(-45, 45))
-	local zAngle = math.rad(love.math.random(-45, 45))
+	local xAngle = math.rad(love.math.random(-15, 15))
+	local zAngle = math.rad(love.math.random(-15, 15))
 	local xRotation = Quaternion.fromAxisAngle(Vector.UNIT_X, xAngle)
-	local zRotation = Quaternion.fromAxisAngle(Vector.UNIT_X, zAngle)
+	local zRotation = Quaternion.fromAxisAngle(Vector.UNIT_Z, zAngle)
 
 	self.targetRotation = zRotation * xRotation
 	self.currentRockTime = self.ROCK_INTERVAL
 end
 
 function ExplosiveBarrel:update(director, game)
-	StaticProp.update(self, director, game)
+	BlockingProp.update(self, director, game)
 
 	local delta = game:getDelta()
 
 	self.currentRockTime = (self.currentRockTime or 0) - delta
 	if self.currentRockTime > 0 then
-		local mu = math.min(math.max(self.currentRockTime / ExplosiveBarrel.ROCK_INTERVAL, 1), 0)
+		local mu = 1 - math.min(math.max(self.currentRockTime / ExplosiveBarrel.ROCK_INTERVAL, 0), 1)
 		self:rock(mu)
 	else
 		self:roll()
