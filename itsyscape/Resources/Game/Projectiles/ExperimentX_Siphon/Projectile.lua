b85a31aa7707356b5cd1b541306c1c560de9cdb4
Erin Maus <squeak@erinmaus.com>
2024-01-26 09:55:31 -0500

    Fix ActorView.getBoneWorldPosition.

--- itsyscape/Resources/Game/Projectiles/ExperimentX_Siphon/Projectile.lua
+++ itsyscape/Resources/Game/Projectiles/ExperimentX_Siphon/Projectile.lua
@@ -127,7 +127,7 @@ function Siphon:tick()
 
 	for _, adventurer in pairs(self.adventurers) do
 		if not adventurer.spawnPosition then
-			adventurer.spawnPosition = sourceView:getBoneWorldPosition(adventurer.boneName, Vector.ZERO, Quaternion.Y_180)
+			adventurer.spawnPosition = sourceView:getBoneWorldPosition(adventurer.boneName)
 			isPending = true
 		end
 	end
