0e80a28a3cdbc2b92b737d0143b51cd590d80985
Erin Maus <squeak@erinmaus.com>
2024-02-17 10:04:12 -0500

    Focus peep during dialog in cutscene.

--- itsyscape/Resources/Game/Maps/IsabelleIsland_FarOcean2/Dialog/Jenkins_Pirate_en-US.lua
+++ itsyscape/Resources/Game/Maps/IsabelleIsland_FarOcean2/Dialog/Jenkins_Pirate_en-US.lua
@@ -1,7 +1,21 @@
 PLAYER_NAME = _TARGET:getName()
 
+local function focus(speakerName)
+	local speakerPeep = _SPEAKERS[speakerName]
+	local actor = speakerPeep:getBehavior("ActorReference")
+	actor = actor and actor.actor
+
+	if actor then
+		local player = Utility.Peep.getPlayerModel(_TARGET)
+		player:pokeCamera("targetActor", actor:getID())
+		player:pokeCamera("zoom", 35)
+	end
+
+	speaker(speakerName)
+end
+
 if Utility.Quest.isNextStep("PreTutorial", "PreTutorial_Start", _TARGET) then
-	speaker "CapnRaven"
+	focus "CapnRaven"
 
 	message { 
 		"Avast, %person{Jenkins}!",
@@ -9,40 +23,33 @@ if Utility.Quest.isNextStep("PreTutorial", "PreTutorial_Start", _TARGET) then
 		"Give us yer %item{loot} and ye can sail another day."
 	}
 
-	speaker "Jenkins"
+	focus "Jenkins"
 
 	message {
 		"How could ye do this?",
 		"We use'd t'be mates, %person{Cap'n Raven}!"
 	}
 
-	speaker "CapnRaven"
+	focus "CapnRaven"
 
 	message "That t'was before ye threw off yer hat."
 	message "Now give us yer valuables and out of the little respect I still have fer ye, I'll let ye live."
 
-	speaker "Orlando"
+	focus "Orlando"
 	message "No! You won't have any of my lil sis's loot!"
 
-	speaker "Rosalind"
+	focus "Rosalind"
 	message "You'll have to get through us first!"
 
-	speaker "CapnRaven"
+	focus "CapnRaven"
 	message {
 		"A young witch and a cowardly warrior?",
 		"Har har har! Sea bass be scarier then ye!"
 	}
 
-	speaker "Rosalind"
+	focus "Rosalind"
 	message "%person{Cap'n Raven}, you'll swallow your words!"
 
-	speaker "CapnRaven"
+	focus "CapnRaven"
 	message "We be seein' about that, landlubber!"
-
-	speaker "Jenkins"
-	message {
-		"%person{${PLAYER_NAME}}, man the cannons!",
-		"%person{Orlando} and %person{Rosalind}, kill any pirates!",
-		"Rest of ye, don't get in the way!"
-	}
 end
