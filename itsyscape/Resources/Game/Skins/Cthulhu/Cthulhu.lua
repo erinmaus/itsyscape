cb2daeb2d495de19659890bf99feae2d34b00ebe
Erin Maus <squeak@erinmaus.com>
2024-03-02 17:34:14 -0500

    Give Cthulhu glowing eyes.

--- itsyscape/Resources/Game/Skins/Cthulhu/Cthulhu.lua
+++ itsyscape/Resources/Game/Skins/Cthulhu/Cthulhu.lua
@@ -1,5 +1,21 @@
 {
 	model = "Resources/Game/Skins/Cthulhu/Cthulhu.lmesh",
 	texture = "Resources/Game/Skins/Cthulhu/Cthulhu.png",
-	position = { 0, -4, 0 }
+	position = { 0, -4, 0 },
+	lights = {
+		{
+			type = "point",
+			color = { 1, 0.5, 0.5 },
+			position = { -2, 3, 1 },
+			attenuation = 8,
+			attach = "head"
+		},
+		{
+			type = "point",
+			color = { 1, 0.5, 0.5 },
+			position = { 2, 3, 1 },
+			attenuation = 8,
+			attach = "head"
+		}
+	}
 }
