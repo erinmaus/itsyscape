5f6f23c561a572dd44fa3608aa514f49b518c935
Erin Maus <squeak@erinmaus.com>
2024-03-22 01:03:17 -0400

    Try combining?

---
+++ itsyscape/Resources/Renderers/PostProcess/Combine.frag.glsl
@@ -0,0 +1,16 @@
+uniform sampler2D scape_Other;
+
+vec4 effect(vec4 color, Image texture, vec2 textureCoordinate, vec2 screenCoordinates)
+{
+	vec3 self = Texel(texture, textureCoordinate).rgb;
+	vec3 other = Texel(scape_Other, textureCoordinate).rgb;
+
+	if (self.b < other.b)
+	{
+		return vec4(self, 1.0);
+	}
+	else
+	{
+		return vec4(other, 1.0);
+	}
+}
