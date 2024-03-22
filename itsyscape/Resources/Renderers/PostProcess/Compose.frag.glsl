441eaa0e89ce6d63d156d4ba65b9006cca058abb
Erin Maus <squeak@erinmaus.com>
2024-03-22 10:51:18 -0400

    More progress.

--- itsyscape/Resources/Renderers/PostProcess/Compose.frag.glsl
+++ itsyscape/Resources/Renderers/PostProcess/Compose.frag.glsl
@@ -6,21 +6,45 @@ uniform sampler2D scape_DiffuseTexture;
 vec4 effect(vec4 color, Image texture, vec2 textureCoordinate, vec2 screenCoordinates)
 {
 	vec4 sample = Texel(texture, textureCoordinate);
+	vec2 position = sample.xy;
+	float distance = length(position - textureCoordinate);
 
-	float distance = length(sample.rg - textureCoordinate);
-	float maxDistance = length(scape_TexelSize * scape_MaxDistance);
+	float outline = Texel(scape_OutlineTexture, textureCoordinate).r;
 
-	vec4 diffuse = vec4(0.0);
-	if (distance >= maxDistance)
+	if (outline < 1.0)
 	{
-		//diffuse = vec4(vec3(0.0), Texel(scape_OutlineTexture, textureCoordinate).a);
-		//diffuse = diffuse * vec4(1.0, 0.0, 0.0, 1.0);
-	}
-	else
-	{
-		diffuse = Texel(scape_DiffuseTexture, sample.rg);
-		//diffuse = diffuse * vec4(1.0, 1.0, 0.0, 1.0);
+		return vec4(Texel(scape_DiffuseTexture, sample.xy).rgb, sample.a);
 	}
 
-	return vec4(vec3(sample.r, 0.0, 0.0), diffuse.a * sample.a);
+	return Texel(scape_DiffuseTexture, textureCoordinate);
+
+	// if (sample.b > 0.0)
+	// {
+	// 	return vec4(vec3(sample.rrr), 1.0);
+	// }
+	// else
+	// {
+	// 	return vec4(1.0);
+	// }
+
+	//return vec4(Texel(scape_DiffuseTexture, sample.xy).rgb, sample.a);
+
+	//return vec4(normalize(vec3(sample.x, 0.0, sample.y)) * vec3(0.5) + vec3(0.5), 1.0);
+
+	// float distance = length(sample.rg - textureCoordinate);
+	// float maxDistance = length(scape_TexelSize * scape_MaxDistance);
+
+	// vec4 diffuse = vec4(0.0);
+	// if (distance >= maxDistance)
+	// {
+	// 	//diffuse = vec4(vec3(0.0), Texel(scape_OutlineTexture, textureCoordinate).a);
+	// 	//diffuse = diffuse * vec4(1.0, 0.0, 0.0, 1.0);
+	// }
+	// else
+	// {
+	// 	diffuse = Texel(scape_DiffuseTexture, sample.rg);
+	// 	//diffuse = diffuse * vec4(1.0, 1.0, 0.0, 1.0);
+	// }
+
+	// return vec4(vec3(sample.r, 0.0, 0.0), diffuse.a * sample.a);
 }
--- itsyscape/Resources/Renderers/PostProcess/InitDistance.frag.glsl
+++ itsyscape/Resources/Renderers/PostProcess/InitDistance.frag.glsl
@@ -1,12 +1,12 @@
 vec4 effect(vec4 color, Image texture, vec2 textureCoordinate, vec2 screenCoordinates)
 {
 	vec4 sample = Texel(texture, textureCoordinate);
-	if (sample.r == 1.0)
+	if (sample.r == 1.0 && sample.a > 0.0)
 	{
-		return vec4(textureCoordinate.st, 0.0, sample.a);
+		return vec4(textureCoordinate, 1.0, sample.a);
 	}
 	else
 	{
-		return vec4(-1.0, -1.0, 0.0, sample.a);
+		return vec4(0.0, 0.0, 0.0, sample.a);
 	}
 }
