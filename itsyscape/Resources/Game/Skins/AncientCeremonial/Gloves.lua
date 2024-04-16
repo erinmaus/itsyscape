d969a6494bce704352d220fa0efd9a0922fe3902
Erin Maus <squeak@erinmaus.com>
2024-04-16 09:34:15 -0400

    Ancient ceremonial.

--- itsyscape/Resources/Game/Skins/AncientCeremonial/Gloves.lua
+++ itsyscape/Resources/Game/Skins/AncientCeremonial/Gloves.lua
@@ -1,4 +1,4 @@
 {
-	model = "Resources/Game/Skins/AncientCeremonial/Gloves.lmesh",
+	model = "Resources/Game/Skins/Common/Equipment/Gloves.lmesh",
 	texture = "Resources/Game/Skins/AncientCeremonial/AncientCeremonial.png"
 }
