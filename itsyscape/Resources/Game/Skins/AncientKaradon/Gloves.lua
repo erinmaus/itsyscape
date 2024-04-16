4558b91a7056a8c054f15146b15e09198c739574
Erin Maus <squeak@erinmaus.com>
2024-04-16 11:28:07 -0400

    Ancient karadon.

--- itsyscape/Resources/Game/Skins/AncientKaradon/Gloves.lua
+++ itsyscape/Resources/Game/Skins/AncientKaradon/Gloves.lua
@@ -1,4 +1,4 @@
 {
-	model = "Resources/Game/Skins/AncientKaradon/Gloves.lmesh",
+	model = "Resources/Game/Skins/Common/Equipment/Gloves.lmesh",
 	texture = "Resources/Game/Skins/Common/LeatherArmor/AncientKaradon.png"
 }
