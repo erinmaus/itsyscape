8aab863ac132a9249cfbc05e0b7450c88a37e953
Erin Maus <squeak@erinmaus.com>
2024-04-17 13:58:34 -0400

    Blue cotton.

--- itsyscape/Resources/Game/Skins/BlueCotton/Slippers.lua
+++ itsyscape/Resources/Game/Skins/BlueCotton/Slippers.lua
@@ -1,4 +1,4 @@
 {
-	model = "Resources/Game/Skins/Common/MagicArmor/Slippers.lmesh",
+	model = "Resources/Game/Skins/Common/Equipment/Boots.lmesh",
 	texture = "Resources/Game/Skins/BlueCotton/Texture.png"
 }
