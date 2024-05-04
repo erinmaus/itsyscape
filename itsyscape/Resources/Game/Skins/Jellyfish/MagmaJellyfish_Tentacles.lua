2000bd528a973d3bf7422438ee776e64e6ca71f3
Erin Maus <squeak@erinmaus.com>
2024-05-03 20:17:33 -0400

    wip particle pass

---
+++ itsyscape/Resources/Game/Skins/Jellyfish/MagmaJellyfish_Tentacles.lua
@@ -0,0 +1,6 @@
+{
+	model = "Resources/Game/Skins/Jellyfish/Jellyfish_Tentacles.lmesh",
+	texture = "Resources/Game/Skins/Jellyfish/MagmaJellyfish.png",
+	position = { 0, 8, 0 },
+	scale = { 2, 2, 2 }
+}
--- itsyscape/Resources/Renderers/PostProcess/CustomOutline.frag.glsl
+++ itsyscape/Resources/Renderers/PostProcess/CustomOutline.frag.glsl
@@ -1,19 +1,40 @@
+uniform float scape_Near;
+uniform float scape_Far;
 uniform vec2 scape_TexelSize;
 uniform sampler2D scape_DepthTexture;
 
 #define SCAPE_BLACK_DISCARD_THRESHOLD 128.0 / 255.0
 
+float linearDepth(float depthSample)
+{
+	depthSample = 2.0 * depthSample - 1.0;
+	float zLinear = 2.0 * scape_Near * scape_Far / (scape_Far + scape_Near - depthSample * (scape_Far - scape_Near));
+	return zLinear;
+}
+
+float restoreDepth(float zLinear)
+{
+	float zOverW = ((scape_Far + scape_Near) - ((2.0 * scape_Near * scape_Far) / zLinear)) / (scape_Far - scape_Near);
+	float z = (zOverW + 1.0) / 2.0;
+
+	return z;
+}
+
 vec4 effect(vec4 color, Image texture, vec2 textureCoordinate, vec2 screenCoordinates)
 {
 	vec4 outlineSample = Texel(texture, textureCoordinate);
-	float depth = Texel(scape_DepthTexture, textureCoordinate).r;
+	float depth = linearDepth(Texel(scape_DepthTexture, textureCoordinate).r);
 
 	if (outlineSample.r >= SCAPE_BLACK_DISCARD_THRESHOLD || outlineSample.g >= SCAPE_BLACK_DISCARD_THRESHOLD || outlineSample.b >= SCAPE_BLACK_DISCARD_THRESHOLD)
 	{
 		discard;
 	}
 
-	gl_FragDepth = depth - 0.000005;
+//<<<<<<< Updated upstream
+//	gl_FragDepth = depth - 0.000005;
+//=======
+	gl_FragDepth = restoreDepth(depth - 0.025);
+//>>>>>>> Stashed changes
 
 	return vec4(outlineSample.rgb, 1.0);
 }
