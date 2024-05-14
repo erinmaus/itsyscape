ba7b6cfeb421bbcf834024b0ff727467aaadc9c7
Erin Maus <squeak@erinmaus.com>
2024-05-14 12:08:26 -0400

    Add hair.

---
+++ itsyscape/Resources/Game/Skins/PlayerKit2/Hair/ForwardSwirl.lua
@@ -0,0 +1,23 @@
+{
+	model = "Resources/Game/Skins/Common/PlayerKit1/Hair.lmesh",
+	pathTexture = "Resources/Game/Skins/PlayerKit2/Hair/ForwardSwirl.lvg",
+	colors = {
+		{
+			name = "Hair",
+
+			"hair"
+		},
+		{
+			name = "Hair Shadow",
+			parent = "Hair",
+			hueOffset = 10,
+			lightnessOffset = -20,
+			saturationOffset = 10,
+
+			"shadow1",
+			"shadow2"
+		}
+	},
+	isBlocking = false,
+	isOccluded = true
+}
---
+++ itsyscape/Resources/Game/Skins/PlayerKit2/Hair/Robot.lua
@@ -0,0 +1,27 @@
+{
+	model = "Resources/Game/Skins/Common/PlayerKit1/Hair.lmesh",
+	pathTexture = "Resources/Game/Skins/PlayerKit2/Hair/Robot.lvg",
+	colors = {
+		{
+			name = "Hair",
+
+			"hair"
+		},
+		{
+			name = "Hair Highlight",
+
+			"highlight"
+		},
+		{
+			name = "Hair Shadow",
+			parent = "Hair",
+			hueOffset = -5,
+			lightnessOffset = -15,
+			saturationOffset = 5,
+
+			"shadow"
+		}
+	},
+	isBlocking = false,
+	isOccluded = true
+}
