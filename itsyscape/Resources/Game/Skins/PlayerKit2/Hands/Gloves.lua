aa6a01deb9964b43e25b4f688e52d6fda20037c4
Erin Maus <squeak@erinmaus.com>
2024-05-14 16:40:00 -0400

    Add hands.

---
+++ itsyscape/Resources/Game/Skins/PlayerKit2/Hands/Gloves.lua
@@ -0,0 +1,23 @@
+{
+	model = "Resources/Game/Skins/Common/PlayerKit1/Hands.lmesh",
+	pathTexture = "Resources/Game/Skins/PlayerKit2/Hands/Gloves.lvg",
+	colors = {
+		{
+			name = "Glove",
+
+			"glove1",
+			"glove2"
+		},
+		{
+			name = "Glove Shadow",
+			parent = "Glove",
+
+			hueOffset = -10,
+			lightnessOffset = -75,
+			saturationOffset = -10,
+
+			"shadow1",
+			"shadow2"
+		}
+	}
+}
---
+++ itsyscape/Resources/Game/Skins/PlayerKit2/Hands/StripedMedallionGloves.lua
@@ -0,0 +1,20 @@
+{
+	model = "Resources/Game/Skins/Common/PlayerKit1/Hands.lmesh",
+	pathTexture = "Resources/Game/Skins/PlayerKit2/Hands/StripedMedallionGloves.lvg",
+	colors = {
+		{
+			name = "Glove",
+
+			"hand1",
+			"hand2"
+		},
+		{
+			name = "Stripe",
+
+			"stripe1",
+			"stripe2",
+			"medallion1",
+			"medallion2"
+		}
+	}
+}
