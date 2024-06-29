ee0da460389d1b54414b01c791751b70df3736a5
Erin Maus <squeak@erinmaus.com>
2024-06-29 19:24:55 -0400

    macOS fixes.

--- itsyscape/Resources/Shaders/Skybox.frag.glsl
+++ itsyscape/Resources/Shaders/Skybox.frag.glsl
@@ -1,12 +1,10 @@
 uniform vec4 scape_TopClearColor;
 uniform vec4 scape_BottomClearColor;
 
-varying vec3 frag_NDCPosition;
-
 vec4 performEffect(vec4 color, vec2 textureCoordinate)
 {
 	float delta = 1.0 - textureCoordinate.t;
-	delta = pow(4.0, delta) / 4.0;
+	delta = pow(2.5, delta) / 2.5;
 
 	return mix(scape_TopClearColor, scape_BottomClearColor, delta);
 }
