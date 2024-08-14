12f2e84d7b554ae661c59ee8bde0b41a7b95881d
Erin Maus <squeak@erinmaus.com>
2024-08-14 18:23:13 -0400

    Improve outlines?

--- itsyscape/ItsyScape/Graphics/TextureResource.lua
+++ itsyscape/ItsyScape/Graphics/TextureResource.lua
@@ -15,6 +15,34 @@ local NTextureResourceInstance = require "nbunny.optimaus.textureresourceinstanc
 
 local TextureResource = Resource(NTextureResource)
 
+if love.graphics then
+	TextureResource.OUTLINE_MIPMAP_SHADER = love.graphics.newShader([[
+		uniform vec2 scape_MipmapBlockSize;
+		uniform vec2 scape_MipmapTexelScale;
+
+		vec4 effect(vec4 color, Image image, vec2 textureCoordinate, vec2 screenCoordinates)
+		{
+			vec4 currentSample = Texel(image, textureCoordinate);
+			float currentSampleLength = length(currentSample);
+			for (float x = 0; x <= scape_MipmapBlockSize.x; x += scape_MipmapTexelScale.x)
+			{
+				for (float y = 0; y <= scape_MipmapBlockSize.y; y += scape_MipmapTexelScale.y)
+				{
+					vec4 subSample = Texel(image, textureCoordinate + vec2(x, y));
+					float subSampleLength = length(subSample);
+					if (subSample.a > 0.5 && (subSampleLength < currentSampleLength || subSample.a > currentSample.a))
+					{
+						currentSample = subSample;
+						currentSampleLength = subSampleLength;
+					}
+				}
+			}
+
+			return vec4(currentSample.r, currentSample.g, currentSample.b, step(0.5, currentSample.a));
+		}
+	]])
+end
+
 TextureResource.PASSES = {
 	["Deferred"] = RendererPass.PASS_DEFERRED,
 	["Forward"] = RendererPass.PASS_FORWARD,
@@ -59,6 +87,62 @@ function TextureResource:release()
 	self:getHandle():setTexture()
 end
 
+function TextureResource:_generateOutlineImage(image)
+	local width = image:getWidth()
+	local height = image:getHeight()
+
+	local previousWidth = width
+	local previousHeight = height
+	local previousImage = image
+
+	love.graphics.push("all")
+	
+	love.graphics.setBlendMode("replace", "premultiplied")
+	love.graphics.setShader(self.OUTLINE_MIPMAP_SHADER)
+	local mipmaps = {}
+	repeat
+		local canvas = love.graphics.newCanvas(width, height)
+		local blockWidth = previousWidth / width
+		local blockHeight = previousHeight / height
+		local texelScaleX = 1.0 / previousWidth
+		local texelScaleY = 1.0 / previousHeight
+		self.OUTLINE_MIPMAP_SHADER:send("scape_MipmapTexelScale", { texelScaleX, texelScaleY })
+		self.OUTLINE_MIPMAP_SHADER:send("scape_MipmapBlockSize", { blockWidth * texelScaleX, blockHeight * texelScaleY })
+		print(">>> width, height", previousWidth, previousHeight)
+		print(">>> blockSize w, h", blockWidth, blockHeight)
+
+		love.graphics.clear(1, 1, 1, 0)
+		love.graphics.setCanvas(canvas)
+		previousImage:setFilter("nearest", "nearest")
+		previousImage:setWrap("clamp")
+		love.graphics.draw(previousImage, 0, 0, 0, width / previousWidth, height / previousHeight)
+		love.graphics.setCanvas()
+
+		table.insert(mipmaps, canvas)
+
+		previousWidth = width
+		previousHeight = height
+		previousImage = canvas
+
+		width = math.max(math.floor(width / 2), 1)
+		height = math.max(math.floor(height / 2), 1)
+	until previousWidth == 1 and previousHeight == 1
+
+	local result = love.graphics.newCanvas(image:getWidth(), image:getHeight(), { mipmaps = "manual" })
+	for level, mipmap in ipairs(mipmaps) do
+		love.graphics.setCanvas(result, level)
+		love.graphics.clear(1, 1, 1, 0)
+		love.graphics.draw(mipmap)
+	end
+
+	love.graphics.pop()
+	result:setMipmapFilter("linear")
+	result:setFilter("linear", "linear")
+	result:setWrap("repeat")
+
+	return result
+end
+
 function TextureResource:loadFromFile(filename, resourceManager)
 	local image = love.graphics.newImage(filename) 
 	image:setFilter('nearest', 'nearest')
@@ -74,6 +158,10 @@ function TextureResource:loadFromFile(filename, resourceManager)
 			local perPassImage = love.graphics.newImage(perPassTextureFilename)
 			perPassImage:setFilter("linear", "linear")
 			perPassImage:setWrap("repeat")
+
+			if passID == RendererPass.PASS_OUTLINE then
+				perPassImage = self:_generateOutlineImage(perPassImage)
+			end
 
 			self:getHandle():setPerPassTexture(passID, perPassImage)
 		end
