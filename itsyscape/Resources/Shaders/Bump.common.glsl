a3547bc75cb337ca8a9598cf863fd9013aabe88c
Erin Maus <squeak@erinmaus.com>
2024-08-19 16:36:39 -0400

    Get bump mapping working.

--- itsyscape/Resources/Shaders/Bump.common.glsl
+++ itsyscape/Resources/Shaders/Bump.common.glsl
@@ -1,4 +1,4 @@
-void calculateBumpNormal(Image heightmapTexture, vec2 textureCoordinate, vec2 texelSize, out vec3 normal, out float height)
+void calculateBumpNormal(Image heightmapTexture, vec2 textureCoordinate, vec2 texelSize, float scale, out vec3 normal, out float height)
 {
     float center = texture(heightmapTexture, textureCoordinate).r;
     float left = Texel(heightmapTexture, textureCoordinate + vec2(-1.0, 0.0) * texelSize).r;
@@ -6,8 +6,8 @@ void calculateBumpNormal(Image heightmapTexture, vec2 textureCoordinate, vec2 te
     float top = Texel(heightmapTexture, textureCoordinate + vec2(0.0, -1.0) * texelSize).r;
     float bottom = Texel(heightmapTexture, textureCoordinate + vec2(0.0, 1.0) * texelSize).r;
 
-    vec3 a = normalize(vec3(vec2(2.0, 0.0), right - left));
-    vec3 b = normalize(vec3(vec2(0.0, 2.0), bottom - top));
+    vec3 a = normalize(vec3(2.0, (left - right) * scale, 0.0));
+    vec3 b = normalize(vec3(0.0, (top - bottom) * scale, 2.0));
 
     normal = normalize(cross(a, b));
     height = center;
