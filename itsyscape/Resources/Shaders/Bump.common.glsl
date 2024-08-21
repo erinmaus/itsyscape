90097403ebb01c83b5ec25534dfba0d7bfbc9b6d
Erin Maus <squeak@erinmaus.com>
2024-08-20 20:00:32 -0400

    Some tweaks to bump mapping.

--- itsyscape/Resources/Shaders/Bump.common.glsl
+++ itsyscape/Resources/Shaders/Bump.common.glsl
@@ -12,10 +12,9 @@ void calculateBumpNormal(Image heightmapTexture, vec2 textureCoordinate, vec2 te
     float bottomRight = Texel(heightmapTexture, textureCoordinate + vec2(1.0, 1.0) * texelSize).r;
     float center = Texel(heightmapTexture, textureCoordinate).r;
 
-    normal = vec3(
-        (topRight + 2.0 * right + bottomRight) - (topLeft + 2.0 * left + bottomLeft),
-        (bottomLeft + 2.0 * bottom + bottomRight) - (topLeft + 2.0 * top + topRight),
-        scale);
-    normal = normalize(normal);
+    float x = (topRight + 2.0 * right + bottomRight) - (topLeft + 2.0 * left + bottomLeft);
+    float y = (bottomLeft + 2.0 * bottom + bottomRight) - (topLeft + 2.0 * top + topRight);
+
+    normal = normalize(vec3(x, -y, scale));
     height = center;
 }
