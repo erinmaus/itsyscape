2c9f8f03b24b17fe56d38b64f5c6c4e15cb750c3
Erin Maus <squeak@erinmaus.com>
2024-12-05 14:45:58 -0500

    Improve masking.

--- itsyscape/Resources/Renderers/PostProcess/BumpShip.vert.glsl
+++ itsyscape/Resources/Renderers/PostProcess/BumpShip.vert.glsl
@@ -1,12 +1,6 @@
 #include "Resources/Shaders/RendererPass.common.glsl"
 
-attribute vec2 VertexTexture;
-
-varying vec2 frag_Texture;
-
 vec4 position(mat4 modelViewProjection, vec4 vertexPosition)
 {
-	frag_Texture = VertexTexture;
-
 	return getWorldViewProjection() * vertexPosition;
 }
--- itsyscape/Resources/Renderers/PostProcess/BumpWater.frag.glsl
+++ itsyscape/Resources/Renderers/PostProcess/BumpWater.frag.glsl
@@ -1,5 +1,4 @@
 void effect()
 {
 	love_Canvases[0] = vec4(0.0);
-	love_Canvases[1] = vec4(0.0);
 }
