d3ef542f198b7014b303070730e70f2c199220ef
Erin Maus <squeak@erinmaus.com>
2024-12-10 19:51:55 -0500

    Tweak outline algo.

--- itsyscape/Resources/Renderers/PostProcess/DepthOutline.frag.glsl
+++ itsyscape/Resources/Renderers/PostProcess/DepthOutline.frag.glsl
@@ -13,8 +13,18 @@ vec4 effect(vec4 color, Image image, vec2 textureCoordinate, vec2 screenCoordina
 	vec4 outlineSample = Texel(scape_OutlineTexture, textureCoordinate);
 	float outlineThreshold = (outlineSample.z - 0.5) * 2.0;
 	vec3 outlineColor = vec3(outlineSample.y);
-	float depthEdge = getDepthEdge(image, textureCoordinate, scape_TexelSize, step(0.0, outlineThreshold));
-	float normalEdge = getNormalEdge(scape_NormalTexture, textureCoordinate, scape_TexelSize);
+
+	float depthEdge = 0.0;
+	float normalEdge = 0.0;
+	for (float x = -1.0; x <= 1.0; x += 1.0)
+	{
+		for (float y = -1.0; y <= 1.0; y += 1.0)
+		{
+			depthEdge = max(depthEdge, getDepthEdge(image, textureCoordinate, scape_TexelSize, step(0.0, outlineThreshold)));
+			normalEdge = max(normalEdge, getNormalEdge(scape_NormalTexture, textureCoordinate, scape_TexelSize));
+		}
+	}
+
 	float depthEdgeStep = max(scape_DepthStep + abs(outlineThreshold), 0.0);
 	float normalEdgeStep = max(scape_NormalStep, 0.0);
 
