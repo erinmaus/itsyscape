aa458bd2871b733d47871fca8896c7a9f38f1812
Erin Maus <squeak@erinmaus.com>
2025-05-04 08:42:37 -0400

    Fix repackaging.

--- cicd/publish/repackage.sh
+++ cicd/publish/repackage.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"
+
 set -xe
 
 cp ./itsyrealm_linux/itsyrealm.love ./itsyrealm_linux/LICENSE.txt ./itsyrealm_macos/ItsyRealm.app/Contents/Resources/
-cp ./itsyrealm_linux/itsyrealm.love ./itsyrealm_linux/LICENSE.txt ./itsyrealm_windows/itsyrealm.love
+cp ./itsyrealm_linux/itsyrealm.love ./itsyrealm_linux/LICENSE.txt ./itsyrealm_windows/
 rm ./itsyrealm_linux/itsyrealm.love # It's in the AppImage
 
-mkdir -p cicd/macos/build/love2d/platform/xcode
-cp itsyrealm_macos cicd/macos/build/love2d/platform/xcode/love-macosxRelease.entitlements
+mkdir -p ../macos/build/love2d/platform/xcode
+cp itsyrealm_macos/build/love2d/platform/xcode/love-macosxRelease.entitlements ../macos/build/love2d/platform/xcode/love-macosxRelease.entitlements
 
-cd cicd/macos
-./cicd/macos/codesign.sh ../../itsyrealm_macos/ItsyRealm.app
+cd ../macos
+./codesign.sh ../publish/itsyrealm_macos/ItsyRealm.app
