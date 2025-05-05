ca474f80c1da834d2ca6370d719d54aa5c970a55
Erin Maus <squeak@erinmaus.com>
2025-05-05 06:28:18 -0400

    Don't use AppImage in Linux build.

--- cicd/publish/repackage.sh
+++ cicd/publish/repackage.sh
@@ -6,7 +6,7 @@ set -xe
 
 cp ./itsyrealm_linux/itsyrealm.love ./itsyrealm_linux/LICENSE.txt ./itsyrealm_macos/ItsyRealm.app/Contents/Resources/
 cp ./itsyrealm_linux/itsyrealm.love ./itsyrealm_linux/LICENSE.txt ./itsyrealm_windows/
-rm ./itsyrealm_linux/itsyrealm.love # It's in the AppImage
+cp ./itsyrealm_linux/LICENSE.txt ./itsyrealm_linux/installdir/LICENSE.txt
 
 mkdir -p ../macos/build/love2d/platform/xcode
 cp itsyrealm_macos/build/love2d/platform/xcode/love-macosxRelease.entitlements ../macos/build/love2d/platform/xcode/love-macosxRelease.entitlements
