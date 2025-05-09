fa594d99b226ef767f6dd6731468cbc0f7efed5d
Erin Maus <squeak@erinmaus.com>
2025-05-09 10:52:27 -0400

    Fix crash.

--- itsyscape/Resources/Game/Peeps/Tutorial/Common.lua
+++ itsyscape/Resources/Game/Peeps/Tutorial/Common.lua
@@ -7,6 +7,7 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --------------------------------------------------------------------------------
+local Class = require "ItsyScape.Common.Class"
 local Utility = require "ItsyScape.Game.Utility"
 local Equipment = require "ItsyScape.Game.Equipment"
 local Weapon = require "ItsyScape.Game.Weapon"
