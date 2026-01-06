9952eb2d7f37e3c6b28318a16a49c94df8074f1b
Erin Maus <squeak@erinmaus.com>
2026-01-05 21:10:27 -0500

    Improve Cthulhu animations.

--- itsyscape/Resources/Game/Animations/Cthulhu_Defend/Script.lua
+++ itsyscape/Resources/Game/Animations/Cthulhu_Defend/Script.lua
@@ -1,4 +1,4 @@
-Animation "Cthulhu Attack" {
+Animation "Cthulhu Defend" {
 	Blend {
 		from = "Cthulhu Risen",
 		duration = 0.25
@@ -6,6 +6,6 @@ Animation "Cthulhu Attack" {
 
 	Target {
 		PlaySound "Resources/Game/Animations/Cthulhu_Defend/Roar.wav",
-		PlayAnimation "Resources/Game/Animations/Cthulhu_Attack/Animation.lanim"
+		PlayAnimation "Resources/Game/Animations/Cthulhu_Defend/Animation.lanim"
 	}
 }
--- itsyscape/Resources/Game/Animations/Cthulhu_Idle/Script.lua
+++ itsyscape/Resources/Game/Animations/Cthulhu_Idle/Script.lua
@@ -1,4 +1,14 @@
 Animation "Cthulhu Risen" {
+	Blend {
+		from = "Cthulhu Attack",
+		duration = 0.25
+	},
+
+	Blend {
+		from = "Cthulhu Defend",
+		duration = 0.25
+	},
+
 	Target {
 		PlayAnimation "Resources/Game/Animations/Cthulhu_Idle/Animation.lanim" {
 			repeatAnimation = true
