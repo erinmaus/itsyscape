c2411c93c0f952db9c4c4280edb57f2c3b660b1a
Erin Maus <squeak@erinmaus.com>
2026-03-01 11:09:08 -0500

    Add impact to dragon stun.

--- itsyscape/Resources/Game/Animations/Dragon_Stunned/Script.lua
+++ itsyscape/Resources/Game/Animations/Dragon_Stunned/Script.lua
@@ -2,7 +2,20 @@ Animation "Dragon Stunned" {
 	fadesIn = true,
 	fadesOut = true,
 
+	Channel {
+		Wait(30 / 24),
+		PlaySound "Resources/Game/Animations/Dragon_Stunned/Impact.wav",
+
+		Shake {
+			duration = 1.5,
+			interval = 0.05,
+			minOffset = 0.25,
+			maxOffset = 0.5
+		}
+	},
+
 	Target {
+		PlaySound "Resources/Game/Animations/Dragon_Stunned/Roar.wav",
 		PlayAnimation "Resources/Game/Animations/Dragon_Stunned/Begin.lanim",
 		PlayAnimation "Resources/Game/Animations/Dragon_Stunned/Idle.lanim" {
 			repeatAnimation = true
